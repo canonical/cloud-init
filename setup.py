1f33a10388e1ac4079d17e9b836dfb4ab7730fec
Soren Hansen <soren@canonical.com>
2009-08-10 21:34:47 +0200

    Move ec2 init script from debian/ out into top-level. Install init-script using distutils. Add -o to dh_installinit call to let it find the init script.

--- setup.py
+++ setup.py
@@ -36,5 +36,6 @@ setup(name='EC2-init',
                'ec2-set-hostname.py',
                'ec2-wait-for-meta-data-service.py'],
       data_files=[('/etc/ec2-init', ['debian/ec2-config.cfg']),
-                  ('/etc/ec2-init/templates', glob('templates/*'))],
+                  ('/etc/ec2-init/templates', glob('templates/*')),
+                  ('/etc/init.d', ['ec2-init'])],
       )
