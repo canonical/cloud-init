a1f0138606b165a8301172669a7c6584780878a8
Scott Moser <smoser@nelson>
2010-01-07 21:14:00 -0500

    add setting of default locale and apt-sources

--- ec2-init.py
+++ ec2-init.py
@@ -34,7 +34,11 @@ def main():
         raise
 
     # set the defaults (like what ec2-set-defaults.py did)
-    # TODO: cloud.set_defaults()
+    try:
+        generate_sources_list(cloud.get_mirror())
+        apply_locale(cloud.get_locale())
+    except:
+        warn("failed to set defaults")
 
     # set the ssh keys up
     cloud.apply_credentials()
@@ -43,6 +47,24 @@ def main():
     cloud.initctl_emit()
 
     sys.exit(0)
+
+def render_to_file(template, outfile, searchList):
+    t = Template(file='/etc/ec2-init/templates/%s.tmpl' % template, searchList=[searchList])
+    f = open(outfile, 'w')
+    f.write(t.respond())
+    f.close()
+    
+def apply_locale(locale):
+    subprocess.Popen(['locale-gen', locale]).communicate()
+    subprocess.Popen(['update-locale', locale]).communicate()
+
+    render_to_file('default-locale', '/etc/default/locale', { 'locale' : locale })
+
+def generate_sources_list(mirror):
+    stdout, stderr = subprocess.Popen(['lsb_release', '-cs'], stdout=subprocess.PIPE).communicate()
+    codename = stdout.strip()
+
+    render_to_file('sources.list', '/etc/apt/sources.list', { 'mirror' : mirror, 'codename' : codename })
 
 if __name__ == '__main__':
     main()
