9591a7177e1ea6fe64cab50204bdf93bdfac64af
Scott Moser <smoser@ubuntu.com>
2010-02-17 09:40:30 -0500

    0.5.4-0ubuntu1 (patches unapplied)
    Imported using git-ubuntu import.

--- cloud-init.py
+++ cloud-init.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python
+#
+#    Copyright (C) 2009-2010 Canonical Ltd.
+#
+#    Author: Scott Moser <scott.moser@canonical.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License version 3, as
+#    published by the Free Software Foundation.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import subprocess
 import sys
@@ -10,7 +26,7 @@ def warn(str):
     sys.stderr.write(str)
 
 def main():
-    cloud = cloudinit.EC2Init()
+    cloud = cloudinit.CloudInit()
 
     try:
         cloud.get_data_source()
@@ -18,10 +34,6 @@ def main():
         print e
         sys.stderr.write("Failed to get instance data")
         sys.exit(1)
-
-    hostname = cloud.get_hostname()
-    subprocess.Popen(['hostname', hostname]).communicate()
-    #print "user data is:" + cloud.get_user_data()
 
     # store the metadata
     cloud.update_cache()
@@ -33,6 +45,15 @@ def main():
     except:
         warn("consuming user data failed!\n")
         raise
+
+    try:
+        hostname = cloud.get_hostname()
+        cloud.sem_and_run("set_hostname", "once-per-instance",
+            set_hostname, [ hostname ], False)
+    except:
+        warn("failed to set hostname\n")
+
+    #print "user data is:" + cloud.get_user_data()
 
     # set the defaults (like what ec2-set-defaults.py did)
     try:
@@ -55,6 +76,12 @@ def apply_locale(locale):
 
     util.render_to_file('default-locale', '/etc/default/locale', \
         { 'locale' : locale })
+
+def set_hostname(hostname):
+    subprocess.Popen(['hostname', hostname]).communicate()
+    f=open("/etc/hostname","wb")
+    f.write("%s\n" % hostname)
+    f.close()
 
 if __name__ == '__main__':
     main()
