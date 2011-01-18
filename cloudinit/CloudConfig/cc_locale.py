cdfcc677b809ffd57cb80172a90ff7f853d697ef
Scott Moser <smoser@ubuntu.com>
2011-01-18 21:24:29 +0000

    move setting of default locale out of cloud-init, into cloud-config
    Now, instead of setting a default value in cloud-init based only on the
    DataSource, this supports using 'locale' in the cloud-config.

---
+++ cloudinit/CloudConfig/cc_locale.py
@@ -0,0 +1,43 @@
+# vi: ts=4 expandtab
+#
+#    Copyright (C) 2011 Canonical Ltd.
+#
+#    Author: Scott Moser <scott.moser@canonical.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License version 3, as
+#    published by the Free Software Foundation.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import cloudinit.util as util
+import subprocess
+import traceback
+
+def apply_locale(locale):
+    subprocess.Popen(['locale-gen', locale]).communicate()
+    subprocess.Popen(['update-locale', locale]).communicate()
+
+    util.render_to_file('default-locale', '/etc/default/locale', \
+        { 'locale' : locale })
+
+def handle(name,cfg,cloud,log,args):
+    if len(args) != 0:
+        value = args[0]
+    else:
+        locale = util.get_cfg_option_str(cfg,"locale",cloud.get_locale())
+
+    if not locale: return
+
+    log.debug("setting locale to %s" % locale)
+
+    try:
+        apply_locale(locale)
+    except Exception, e:
+        log.debug(traceback.format_exc(e))
+        raise Exception("failed to apply locale %s" % locale)
