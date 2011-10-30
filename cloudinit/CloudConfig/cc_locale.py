e49f67f4d7c61dee67e2d136ad15396bacb1ce9c
Scott Moser <smoser@ubuntu.com>
2011-10-30 17:23:39 -0400

    Make locale file location configurable
    fedora's analog to /etc/default/locale is /etc/sysconfig/i18n .
    
    This makes locale_configfile configurable and chooses between
      /usr/sbin/locale-gen (ubuntu/debian) and 
      /usr/sbin/update-localeo (fedora)
    based on availability to generate locales.
    
    taken from
    git://pkgs.fedoraproject.org/cloud-init.git
    commit 87f33190f43d2b26cced4597e7298835024466c2
    Author: Garrett Holmstrom <gholms@fedoraproject.org>
    Patch4: cloud-init-0.6.2-localefile.patch

--- cloudinit/CloudConfig/cc_locale.py
+++ cloudinit/CloudConfig/cc_locale.py
@@ -16,15 +16,17 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import cloudinit.util as util
+import os.path
 import subprocess
 import traceback
 
-def apply_locale(locale):
-    subprocess.Popen(['locale-gen', locale]).communicate()
-    subprocess.Popen(['update-locale', locale]).communicate()
+def apply_locale(locale, cfgfile):
+    if os.path.exists('/usr/sbin/locale-gen'):
+        subprocess.Popen(['locale-gen', locale]).communicate()
+    if os.path.exists('/usr/sbin/update-locale'):
+        subprocess.Popen(['update-locale', locale]).communicate()
 
-    util.render_to_file('default-locale', '/etc/default/locale', \
-        { 'locale' : locale })
+    util.render_to_file('default-locale', cfgfile, { 'locale' : locale })
 
 def handle(name,cfg,cloud,log,args):
     if len(args) != 0:
@@ -32,12 +34,15 @@ def handle(name,cfg,cloud,log,args):
     else:
         locale = util.get_cfg_option_str(cfg,"locale",cloud.get_locale())
 
+    locale_cfgfile = util.get_cfg_option_str(cfg, "locale_configfile",
+                                             "/etc/default/locale")
+
     if not locale: return
 
     log.debug("setting locale to %s" % locale)
 
     try:
-        apply_locale(locale)
+        apply_locale(locale, locale_cfgfile)
     except Exception as e:
         log.debug(traceback.format_exc(e))
         raise Exception("failed to apply locale %s" % locale)
