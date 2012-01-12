17f493a9e8cdadd192df5c3ca75ca36b1c7b0fd6
Mike Milner <mike.milner@canonical.com>
2012-01-12 19:28:09 +0100

    Add ability to rebuild CA certificate file.

--- tests/unittests/test_handler_ca_certs.py
+++ tests/unittests/test_handler_ca_certs.py
@@ -1,7 +1,28 @@
 from unittest import TestCase
 from mocker import MockerTestCase
 
-from cloudinit.CloudConfig.cc_ca_certs import handle, write_file
+from cloudinit.CloudConfig.cc_ca_certs import handle, write_file, update_ca_certs
+
+class TestNoConfig(MockerTestCase):
+    def setUp(self):
+        super(TestNoConfig, self).setUp()
+        self.name = "ca-certs"
+        self.cloud_init = None
+        self.log = None
+        self.args = []
+
+    def test_no_config(self):
+        """
+        Test that nothing is done if no ca-certs configuration is provided.
+        """
+        config = {"unknown-key": "value"}
+
+        self.mocker.replace(write_file, passthrough=False)
+        self.mocker.replace(update_ca_certs, passthrough=False)
+        self.mocker.replay()
+
+        handle(self.name, config, self.cloud_init, self.log, self.args)
+
 
 class TestAddCaCerts(MockerTestCase):
     def setUp(self):
@@ -11,14 +32,10 @@ class TestAddCaCerts(MockerTestCase):
         self.log = None
         self.args = []
 
-    def test_no_config(self):
-        """Test that no certificate are written if not provided."""
-        config = {"unknown-key": "value"}
-
-        mock = self.mocker.replace(write_file, passthrough=False)
-        self.mocker.replay()
-
-        handle(self.name, config, self.cloud_init, self.log, self.args)
+        # The config option is present for all these tests so
+        # update_ca_certs should always be called.
+        mock = self.mocker.replace(update_ca_certs, passthrough=False)
+        mock()
 
     def test_no_trusted_list(self):
         """Test that no certificate are written if not provided."""
@@ -62,3 +79,13 @@ class TestAddCaCerts(MockerTestCase):
         self.mocker.replay()
 
         handle(self.name, config, self.cloud_init, self.log, self.args)
+
+class TestUpdateCaCerts(MockerTestCase):
+    def test_commands(self):
+        mock_check_call = self.mocker.replace("subprocess.check_call",
+                                              passthrough=False)
+        mock_check_call(["dpkg-reconfigure", "ca-certificates"])
+        mock_check_call(["update-ca-certificates"])
+        self.mocker.replay()
+
+        update_ca_certs()
