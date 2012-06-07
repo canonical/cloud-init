7adde5f221ab3e8d79d3cf053b374c541a75fbf1
Joshua Harlow <harlowja@yahoo-inc.com>
2012-06-07 13:48:30 -0700

    Start moving code from there to here.

--- cloudinit/handling.py
+++ cloudinit/handling.py
@@ -7,8 +7,81 @@ from cloudinit.constants import (PER_INSTANCE, PER_ALWAYS)
 LOG = logging.getLogger(__name__)
 
 
+def handler_register(mod, part_handlers, data, frequency=per_instance):
+    if not hasattr(mod, "handler_version"):
+        setattr(mod, "handler_version", 1)
+
+    for mtype in mod.list_types():
+        part_handlers[mtype] = mod
+
+    handler_call_begin(mod, data, frequency)
+    return(mod)
+
+
+def handler_call_begin(mod, data, frequency):
+    handler_handle_part(mod, data, "__begin__", None, None, frequency)
+
+
+def handler_call_end(mod, data, frequency):
+    handler_handle_part(mod, data, "__end__", None, None, frequency)
+
+
+def handler_handle_part(mod, data, ctype, filename, payload, frequency):
+    # only add the handler if the module should run
+    modfreq = getattr(mod, "frequency", per_instance)
+    if not (modfreq == per_always or
+            (frequency == per_instance and modfreq == per_instance)):
+        return
+    try:
+        if mod.handler_version == 1:
+            mod.handle_part(data, ctype, filename, payload)
+        else:
+            mod.handle_part(data, ctype, filename, payload, frequency)
+    except:
+        util.logexc(log)
+        traceback.print_exc(file=sys.stderr)
+
+
+def partwalker_handle_handler(pdata, _ctype, _filename, payload):
+    curcount = pdata['handlercount']
+    modname = 'part-handler-%03d' % curcount
+    frequency = pdata['frequency']
+
+    modfname = modname + ".py"
+    util.write_file("%s/%s" % (pdata['handlerdir'], modfname), payload, 0600)
+
+    try:
+        mod = __import__(modname)
+        handler_register(mod, pdata['handlers'], pdata['data'], frequency)
+        pdata['handlercount'] = curcount + 1
+    except:
+        util.logexc(log)
+        traceback.print_exc(file=sys.stderr)
+
+
+def partwalker_callback(pdata, ctype, filename, payload):
+    # data here is the part_handlers array and then the data to pass through
+    if ctype == "text/part-handler":
+        if 'handlercount' not in pdata:
+            pdata['handlercount'] = 0
+        partwalker_handle_handler(pdata, ctype, filename, payload)
+        return
+    if ctype not in pdata['handlers']:
+        if ctype == "text/x-not-multipart":
+            # Extract the first line or 24 bytes for displaying in the log
+            start = payload.split("\n", 1)[0][:24]
+            if start < payload:
+                details = "starting '%s...'" % start.encode("string-escape")
+            else:
+                details = repr(payload)
+            log.warning("Unhandled non-multipart userdata %s", details)
+        return
+    handler_handle_part(pdata['handlers'][ctype], pdata['data'],
+        ctype, filename, payload, pdata['frequency'])
+
+
 class InternalPartHandler:
-    freq = PER_INSTANCE
+    freq = per_instance
     mtypes = []
     handler_version = 1
     handler = None
@@ -27,75 +100,3 @@ class InternalPartHandler:
 
     def handle_part(self, data, ctype, filename, payload, frequency):
         return(self.handler(data, ctype, filename, payload, frequency))
-
-
-def handler_register(mod, part_handlers, data, frequency=PER_INSTANCE):
-    if not hasattr(mod, "handler_version"):
-        setattr(mod, "handler_version", 1)
-
-    for mtype in mod.list_types():
-        part_handlers[mtype] = mod
-
-    handler_call_begin(mod, data, frequency)
-    return mod
-
-
-def handler_call_begin(mod, data, frequency):
-    handler_handle_part(mod, data, "__begin__", None, None, frequency)
-
-
-def handler_call_end(mod, data, frequency):
-    handler_handle_part(mod, data, "__end__", None, None, frequency)
-
-
-def handler_handle_part(mod, data, ctype, filename, payload, frequency):
-    # only add the handler if the module should run
-    modfreq = getattr(mod, "frequency", PER_INSTANCE)
-    if not (modfreq == PER_ALWAYS or
-            (frequency == PER_INSTANCE and modfreq == PER_INSTANCE)):
-        return
-    try:
-        if mod.handler_version == 1:
-            mod.handle_part(data, ctype, filename, payload)
-        else:
-            mod.handle_part(data, ctype, filename, payload, frequency)
-    except:
-        util.logexc(log)
-        traceback.print_exc(file=sys.stderr)
-
-
-def partwalker_handle_handler(pdata, _ctype, _filename, payload):
-    curcount = pdata['handlercount']
-    modname = 'part-handler-%03d' % curcount
-    frequency = pdata['frequency']
-
-    modfname = modname + ".py"
-    util.write_file(os.path.join(pdata['handlerdir'], modfname), payload, 0600)
-
-    try:
-        mod = importer.import_module(modname)
-        handler_register(mod, pdata['handlers'], pdata['data'], frequency)
-        pdata['handlercount'] = curcount + 1
-    except:
-        LOG.exception("Could not import module %s", modname)
-
-
-def partwalker_callback(pdata, ctype, filename, payload):
-    # data here is the part_handlers array and then the data to pass through
-    if ctype == "text/part-handler":
-        if 'handlercount' not in pdata:
-            pdata['handlercount'] = 0
-        partwalker_handle_handler(pdata, ctype, filename, payload)
-        return
-    if ctype not in pdata['handlers']:
-        if ctype == "text/x-not-multipart":
-            # Extract the first line or 24 bytes for displaying in the log
-            start = payload.split("\n", 1)[0][:24]
-            if start < payload:
-                details = "starting '%s...'" % start.encode("string-escape")
-            else:
-                details = repr(payload)
-            LOG.warning("Unhandled non-multipart userdata %s", details)
-        return
-    handler_handle_part(pdata['handlers'][ctype], pdata['data'],
-        ctype, filename, payload, pdata['frequency'])
