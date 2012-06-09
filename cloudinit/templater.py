46e451abd8e5945a9b944dfd794a2556a3830811
Joshua Harlow <harlowja@yahoo-inc.com>
2012-06-09 12:36:18 -0700

    Remove unused imports and reduce lines needed + add copy right.

--- cloudinit/templater.py
+++ cloudinit/templater.py
@@ -1,18 +1,32 @@
-import os
+# vi: ts=4 expandtab
+#
+#    Copyright (C) 2012 Canonical Ltd.
+#    Copyright (C) 2012 Hewlett-Packard Development Company, L.P.
+#    Copyright (C) 2012 Yahoo! Inc.
+#
+#    Author: Scott Moser <scott.moser@canonical.com>
+#    Author: Juerg Haefliger <juerg.haefliger@hp.com>
+#    Author: Joshua Harlow <harlowja@yahoo-inc.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License version 3, as
+#    published by the Free Software Foundation.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from Cheetah.Template import Template
 
-from cloudinit import settings
 from cloudinit import util
 
 
-def render_to_file(template, outfile, searchList):
-    fn = template
-    (base, ext) = os.path.splitext(fn)
-    if ext != ".tmpl":
-        fn = "%s.tmpl" % (fn)
-    fn = os.path.join(settings.TEMPLATE_DIR, fn)
-    contents = Template(file=fn, searchList=[searchList]).respond()
+def render_to_file(template_fn, outfile, searchList):
+    contents = Template(file=template_fn, searchList=[searchList]).respond()
     util.write_file(outfile, contents)
 
 
