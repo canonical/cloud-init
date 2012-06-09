5ee615c0542a664a7dd19bbfadcc7eeeaff0d6e1
Joshua Harlow <harlowja@yahoo-inc.com>
2012-06-09 12:34:52 -0700

    Remove pathmap and make this a part of the CloudPath object, add copyright and format built in config

--- cloudinit/settings.py
+++ cloudinit/settings.py
@@ -1,40 +1,51 @@
-import os
-
-VAR_LIB_DIR = '/var/lib/cloud'
-CUR_INSTANCE_LINK = os.path.join(VAR_LIB_DIR, "instance")
-BOOT_FINISHED = os.path.join(CUR_INSTANCE_LINK, "boot-finished")
-SEED_DIR = os.path.join(VAR_LIB_DIR, "seed")
+# vi: ts=4 expandtab
+#
+#    Copyright (C) 2012 Canonical Ltd.
+#    Copyright (C) 2012 Hewlett-Packard Development Company, L.P.
+#    Copyright (C) 2012 Yahoo! Inc.
+#
+#    Author: Scott Moser <scott.moser@canonical.com>
+#    Author: Juerg Haefliger <juerg.haefliger@hp.com>
+#    Author: Joshua Harlow <harlowja@yahoo-inc.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License version 3, as
+#    published by the Free Software Foundation.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 CFG_ENV_NAME = "CLOUD_CFG"
 CLOUD_CONFIG = '/etc/cloud/cloud.cfg'
 OLD_CLOUD_CONFIG = '/etc/ec2-init/ec2-config.cfg'
 
 CFG_BUILTIN = {
-    'datasource_list': ['NoCloud',
-                        'ConfigDrive',
-                        'OVF',
-                        'MAAS',
-                        'Ec2',
-                        'CloudStack'],
+    'datasource_list': [
+        'NoCloud',
+        'ConfigDrive',
+        'OVF',
+        'MAAS',
+        'Ec2',
+        'CloudStack'
+    ],
     'def_log_file': '/var/log/cloud-init.log',
     'log_cfgs': [],
-    'syslog_fix_perms': 'syslog:adm'
-}
-
-PATH_MAP = {
-   "handlers": "handlers",
-   "scripts": "scripts",
-   "sem": "sem",
-   "boothooks": "boothooks",
-   "userdata_raw": "user-data.txt",
-   "userdata": "user-data.txt.i",
-   "obj_pkl": "obj.pkl",
-   "cloud_config": "cloud-config.txt",
-   "data": "data",
+    'syslog_fix_perms': 'syslog:adm',
+    'system_info': {
+        'paths': {
+            'cloud_dir': '/var/lib/cloud',
+            'templates_dir': '/etc/cloud/templates/',
+        }, 
+        'distro': 'ubuntu',
+    },
 }
 
 PER_INSTANCE = "once-per-instance"
 PER_ALWAYS = "always"
 PER_ONCE = "once"
 
-TEMPLATE_DIR = '/etc/cloud/templates/'
