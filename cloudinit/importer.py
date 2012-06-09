1b4f105bcc4f37644e896a8e090382ce049dac91
Joshua Harlow <harlowja@yahoo-inc.com>
2012-06-09 12:33:20 -0700

    Add copyright + index hash directly instead of get.

--- cloudinit/importer.py
+++ cloudinit/importer.py
@@ -1,4 +1,24 @@
-# vim: tabstop=4 shiftwidth=4 softtabstop=4
+# vi: ts=4 expandtab
+#
+#    Copyright (C) 2012 Canonical Ltd.
+#    Copyright (C) 2012 Hewlett-Packard Development Company, L.P.
+#    Copyright (C) 2012 Yahoo! Inc.
+#
+#    Author: Scott Moser <scott.moser@canonical.com>
+#    Author: Juerg Haefliger <juerg.haefliger@hp.com>
+#    Author: Joshua Harlow <harlowja@yahoo-inc.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License version 3, as
+#    published by the Free Software Foundation.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
 
@@ -6,6 +26,6 @@ import sys
 def import_module(module_name):
     try:
         __import__(module_name)
-        return sys.modules.get(module_name, None)
+        return sys.modules[module_name]
     except ImportError as err:
         raise RuntimeError('Could not load module %s: %s' % (module_name, err))
