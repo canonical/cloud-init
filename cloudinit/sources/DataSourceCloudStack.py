09273ee69b69418f683b5f98d1f01243c3c5638b
Joshua Harlow <harlowja@yahoo-inc.com>
2012-06-11 17:16:19 -0700

    Fix this up to use the new datasource class hierachy, as well as other new objects/logging added...

--- cloudinit/sources/DataSourceCloudStack.py
+++ cloudinit/sources/DataSourceCloudStack.py
@@ -18,62 +18,68 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import cloudinit.DataSource as DataSource
-
-from cloudinit import seeddir as base_seeddir
-from cloudinit import log
-import cloudinit.util as util
 from socket import inet_ntoa
-import time
-import boto.utils as boto_utils
 from struct import pack
 
+import os
+import time
 
-class DataSourceCloudStack(DataSource.DataSource):
-    api_ver = 'latest'
-    seeddir = base_seeddir + '/cs'
-    metadata_address = None
+import boto.utils as boto_utils
 
-    def __init__(self, sys_cfg=None):
-        DataSource.DataSource.__init__(self, sys_cfg)
+from cloudinit import log as logging
+from cloudinit import sources
+from cloudinit import util
+
+LOG = logging.getLogger(__name__)
+
+
+class DataSourceCloudStack(sources.DataSource):
+    def __init__(self, sys_cfg, distro, paths):
+        sources.DataSource.__init__(self, sys_cfg, distro, paths)
+        self.seed_dir = os.path.join(paths.seed_dir, 'cs')
         # Cloudstack has its metadata/userdata URLs located at
         # http://<default-gateway-ip>/latest/
-        self.metadata_address = "http://%s/" % self.get_default_gateway()
+        self.api_ver = 'latest'
+        gw_addr = self.get_default_gateway()
+        if not gw_addr:
+            raise RuntimeError("No default gateway found!")
+        self.metadata_address = "http://%s/" % (gw_addr)
 
     def get_default_gateway(self):
         """ Returns the default gateway ip address in the dotted format
         """
-        with open("/proc/net/route", "r") as f:
-            for line in f.readlines():
-                items = line.split("\t")
-                if items[1] == "00000000":
-                    # found the default route, get the gateway
-                    gw = inet_ntoa(pack("<L", int(items[2], 16)))
-                    log.debug("found default route, gateway is %s" % gw)
-                    return gw
+        lines = util.load_file("/proc/net/route").splitlines()
+        for line in lines:
+            items = line.split("\t")
+            if items[1] == "00000000":
+                # Found the default route, get the gateway
+                gw = inet_ntoa(pack("<L", int(items[2], 16)))
+                LOG.debug("Found default route, gateway is %s", gw)
+                return gw
+        return None
 
     def __str__(self):
-        return "DataSourceCloudStack"
+        return util.obj_name(self)
 
     def get_data(self):
-        seedret = {}
-        if util.read_optional_seed(seedret, base=self.seeddir + "/"):
-            self.userdata_raw = seedret['user-data']
-            self.metadata = seedret['meta-data']
-            log.debug("using seeded cs data in %s" % self.seeddir)
+        seed_ret = {}
+        if util.read_optional_seed(seed_ret, base=(self.seed_dir + "/")):
+            self.userdata_raw = seed_ret['user-data']
+            self.metadata = seed_ret['meta-data']
+            LOG.info("Using seeded cloudstack data from: %s", self.seed_dir)
             return True
-
         try:
             start = time.time()
             self.userdata_raw = boto_utils.get_instance_userdata(self.api_ver,
                 None, self.metadata_address)
             self.metadata = boto_utils.get_instance_metadata(self.api_ver,
                 self.metadata_address)
-            log.debug("crawl of metadata service took %ds" %
-                (time.time() - start))
+            LOG.debug("Crawl of metadata service took %ds",
+                      (time.time() - start))
             return True
         except Exception as e:
-            log.exception(e)
+            LOG.exception(('Failed fetching from metadata '
+                           'service %s due to: %s'), self.metadata_address, e)
             return False
 
     def get_instance_id(self):
@@ -82,11 +88,13 @@ class DataSourceCloudStack(DataSource.DataSource):
     def get_availability_zone(self):
         return self.metadata['availability-zone']
 
+
+# Used to match classes to dependencies
 datasources = [
-  (DataSourceCloudStack, (DataSource.DEP_FILESYSTEM, DataSource.DEP_NETWORK)),
+  (DataSourceCloudStack, (sources.DEP_FILESYSTEM, sources.DEP_NETWORK)),
 ]
 
 
-# return a list of data sources that match this set of dependencies
+# Return a list of data sources that match this set of dependencies
 def get_datasource_list(depends):
-    return DataSource.list_from_depends(depends, datasources)
+    return sources.list_from_depends(depends, datasources)
