54435c2557c9b2881cf81cfd16dab751e4fd7b10
Joshua Harlow <harlowja@yahoo-inc.com>
2012-06-11 20:12:36 -0700

    Fix copy right.

--- cloudinit/netinfo.py
+++ cloudinit/netinfo.py
@@ -1,11 +1,12 @@
-#!/usr/bin/python
 # vi: ts=4 expandtab
 #
 #    Copyright (C) 2012 Canonical Ltd.
 #    Copyright (C) 2012 Hewlett-Packard Development Company, L.P.
+#    Copyright (C) 2012 Yahoo! Inc.
 #
 #    Author: Scott Moser <scott.moser@canonical.com>
 #    Author: Juerg Haefliger <juerg.haefliger@hp.com>
+#    Author: Joshua Harlow <harlowja@yahoo-inc.com>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License version 3, as
