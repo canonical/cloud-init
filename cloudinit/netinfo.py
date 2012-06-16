e2eb9bbc7efd79df5c2a0b381063d54bdc2e42d1
Joshua Harlow <harlowja@yahoo-inc.com>
2012-06-16 09:41:13 -0700

    Unused import removed

--- cloudinit/netinfo.py
+++ cloudinit/netinfo.py
@@ -20,8 +20,6 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
-
 import cloudinit.util as util
 
 from prettytable import PrettyTable
