84e97a885fd407290b28aacff0af1f7afd56540d
Joshua Harlow <harlowja@yahoo-inc.com>
2012-06-20 16:36:02 -0700

    Move the configparser helper to here since it being in a cfg.py is confusing when there is also a directory named 'config'

--- cloudinit/helpers.py
+++ cloudinit/helpers.py
@@ -23,7 +23,10 @@
 from time import time
 
 import contextlib
+import io
 import os
+
+from ConfigParser import (NoSectionError, NoOptionError, RawConfigParser)
 
 from cloudinit.settings import (PER_INSTANCE, PER_ALWAYS, PER_ONCE)
 
@@ -298,3 +301,63 @@ class Paths(object):
             return None
         else:
             return ipath
+
+
+# This config parser will not throw when sections don't exist
+# and you are setting values on those sections which is useful
+# when writing to new options that may not have corresponding
+# sections. Also it can default other values when doing gets
+# so that if those sections/options do not exist you will
+# get a default instead of an error. Another useful case where
+# you can avoid catching exceptions that you typically don't
+# care about...
+
+class DefaultingConfigParser(RawConfigParser):
+    DEF_INT = 0
+    DEF_FLOAT = 0.0
+    DEF_BOOLEAN = False
+    DEF_BASE = None
+
+    def get(self, section, option):
+        value = self.DEF_BASE
+        try:
+            value = RawConfigParser.get(self, section, option)
+        except NoSectionError:
+            pass
+        except NoOptionError:
+            pass
+        return value
+
+    def set(self, section, option, value):
+        if not self.has_section(section) and section.lower() != 'default':
+            self.add_section(section)
+        RawConfigParser.set(self, section, option, value)
+
+    def remove_option(self, section, option):
+        if self.has_option(section, option):
+            RawConfigParser.remove_option(self, section, option)
+
+    def getboolean(self, section, option):
+        if not self.has_option(section, option):
+            return self.DEF_BOOLEAN
+        return RawConfigParser.getboolean(self, section, option)
+
+    def getfloat(self, section, option):
+        if not self.has_option(section, option):
+            return self.DEF_FLOAT
+        return RawConfigParser.getfloat(self, section, option)
+
+    def getint(self, section, option):
+        if not self.has_option(section, option):
+            return self.DEF_INT
+        return RawConfigParser.getint(self, section, option)
+
+    def stringify(self, header=None):
+        contents = ''
+        with io.BytesIO() as outputstream:
+            self.write(outputstream)
+            outputstream.flush()
+            contents = outputstream.getvalue()
+            if header:
+                contents = "\n".join([header, contents])
+        return contents
