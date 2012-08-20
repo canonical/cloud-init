8aa59086146062fc767349be086fe0c35bf9c477
Ben Howard <ben.howard@canonical.com>
2012-08-20 15:39:39 -0600

    Dropped hidden command; replaced with logstring. Also changed useradd command to use log options over short

--- cloudinit/util.py
+++ cloudinit/util.py
@@ -1329,17 +1329,17 @@ def delete_dir_contents(dirname):
             del_file(node_fullpath)
 
 
-def subp(args, data=None, rcs=None, env=None, capture=True, shell=False, hidden=False):
+def subp(args, data=None, rcs=None, env=None, capture=True, shell=False, logstring=False):
     if rcs is None:
         rcs = [0]
     try:
 
-        if not hidden:
+        if not logstring:
             LOG.debug(("Running command %s with allowed return codes %s"
                       " (shell=%s, capture=%s)"), args, rcs, shell, capture)
         else:
-            LOG.debug(("Running hidden command to protect sensative output "
-                        " Calling function: %s" ), hidden)
+            LOG.debug(("Running hidden command to protect sensitive input/output "
+                        " logstring: %s" ), logstring)
 
         if not capture:
             stdout = None
