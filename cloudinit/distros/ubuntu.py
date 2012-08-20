336ddbe13bdfc729495f5bfb8cc89b4360916157
Ben Howard <ben.howard@canonical.com>
2012-08-20 14:52:31 -0600

    Added "userless" mode to cloud-init for handling the creation of the users and the default user on Ubuntu.
    cloudinit/config/cc_users_groups.py: new cloud-config module for creating
        users and groups on instance initialization.
        - Creates users and group
        - Sets "user" directive used in ssh_import_id
    
    cloudinit/config/cc_ssh_import_id.py: module will rely upon users_groups
        for setting the default user. Removed assumption of 'ubuntu' user.
    
    cloudinit/distros/__init__.py: Added new abstract methods for getting
        and creating the default user.
    
    cloudinit/distros/ubuntu.py: Defined abstract methods for getting and
        and creating the default 'ubuntu' user on Ubuntu instances.
    
    cloudinit/util.py: Added ability to hide command run through util.subp to
        prevent the commands from showing in the logs. Used by user_groups
        cloud-config module.
    
    config/cloud.cfg: Removed "user: ubuntu" directive and replaced with new
        user-less syntax.
    
    doc/examples/cloud-config.txt: Documented the creation of users and groups.

--- cloudinit/distros/ubuntu.py
+++ cloudinit/distros/ubuntu.py
@@ -7,6 +7,7 @@
 #    Author: Scott Moser <scott.moser@canonical.com>
 #    Author: Juerg Haefliger <juerg.haefliger@hp.com>
 #    Author: Joshua Harlow <harlowja@yahoo-inc.com>
+#    Author: Ben Howard <ben.howard@canonical.com>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License version 3, as
@@ -20,12 +21,70 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from cloudinit import distros
 from cloudinit.distros import debian
-
+from cloudinit import helpers
 from cloudinit import log as logging
+from cloudinit import util
+from cloudinit.settings import PER_INSTANCE
+
+import pwd
 
 LOG = logging.getLogger(__name__)
 
 
 class Distro(debian.Distro):
-    pass
+
+    distro_name = 'ubuntu'
+    __default_user_name__ = 'ubuntu-test'
+
+    def __init__(self, name, cfg, paths):
+        distros.Distro.__init__(self, name, cfg, paths)
+        # This will be used to restrict certain
+        # calls from repeatly happening (when they
+        # should only happen say once per instance...)
+        self._runner = helpers.Runners(paths)
+
+    def get_default_username(self):
+        return self.__default_user_name__
+
+    def add_default_user(self):
+        # Adds the ubuntu user using the rules:
+        #  - Password is 'ubuntu', but is locked
+        #  - nopasswd sudo access
+
+
+        if self.__default_user_name__ in [x[0] for x in pwd.getpwall()]:
+            LOG.warn("'%s' user already exists, not creating it." % \
+                    self.__default_user_name__)
+            return
+
+        try:
+            util.subp(['adduser',
+                        '--shell', '/bin/bash',
+                        '--home', '/home/%s' % self.__default_user_name__,
+                        '--disabled-password',
+                        '--gecos', 'Ubuntu',
+			self.__default_user_name__,
+                        ])
+
+            pass_string = '%(u)s:%(u)s' % {'u': self.__default_user_name__}
+            util.subp(['chpasswd'], pass_string)
+            util.subp(['passwd', '-l', self.__default_user_name__])
+
+            ubuntu_sudoers="""
+# Added by cloud-init
+# %(user)s user is default user in cloud-images.
+# It needs passwordless sudo functionality.
+%(user)s ALL=(ALL) NOPASSWD:ALL
+""" % { 'user': self.__default_user_name__ }
+
+            util.write_file('/etc/sudoers.d/90-cloud-init-ubuntu',
+                            ubuntu_sudoers,
+                            mode=0440)
+
+            LOG.info("Added default 'ubuntu' user with passwordless sudo")
+
+        except Exception as e:
+            util.logexc(LOG, "Failed to create %s user\n%s" %
+                        (self.__default_user_name__, e))
