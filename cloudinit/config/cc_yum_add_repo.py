b23df6b38b4822b1c2f1c536725a2bcbdfc56775
Joshua Harlow <harlowja@yahoo-inc.com>
2012-10-16 15:19:14 -0700

    Fix copyright.

--- cloudinit/config/cc_yum_add_repo.py
+++ cloudinit/config/cc_yum_add_repo.py
@@ -1,10 +1,8 @@
 # vi: ts=4 expandtab
 #
-#    Copyright (C) 2011 Canonical Ltd.
-#    Copyright (C) 2012 Hewlett-Packard Development Company, L.P.
+#    Copyright (C) 2012 Yahoo! Inc.
 #
-#    Author: Scott Moser <scott.moser@canonical.com>
-#    Author: Juerg Haefliger <juerg.haefliger@hp.com>
+#    Author: Joshua Harlow <harlowja@yahoo-inc.com>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License version 3, as
@@ -16,7 +14,7 @@
 #    GNU General Public License for more details.
 #
 #    You should have received a copy of the GNU General Public License
-#    along with this 
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
 
