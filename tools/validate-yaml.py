d8e82fb7bc9da6259b158804ae3d8343050c67aa
Joshua Harlow <harlowja@gmail.com>
2012-11-08 08:55:04 -0800

    Add non-zero exit code when bad yamls are found instead of returning zero in that case.

--- tools/validate-yaml.py
+++ tools/validate-yaml.py
@@ -9,6 +9,7 @@ import yaml
 
 
 if __name__ == "__main__":
+    bads = 0
     for fn in sys.argv[1:]:
         sys.stdout.write("%s" % (fn))
         try:
@@ -18,3 +19,8 @@ if __name__ == "__main__":
             sys.stdout.write(" - ok\n")
         except Exception, e:
             sys.stdout.write(" - bad (%s)\n" % (e))
+            bads += 1
+    if bads > 0:
+        sys.exit(1)
+    else:
+        sys.exit(0)
