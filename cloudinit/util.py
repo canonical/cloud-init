4efa9ddb09df20cd52af5a696e7436f75c673952
Scott Moser <smoser@ubuntu.com>
2012-11-10 22:24:01 -0500

    revert unrelated whitespace changes that creeped in.

--- cloudinit/util.py
+++ cloudinit/util.py
@@ -1193,7 +1193,8 @@ def yaml_dumps(obj):
                     indent=4,
                     explicit_start=True,
                     explicit_end=True,
-                    default_flow_style=False)
+                    default_flow_style=False,
+                    )
     return formatted
 
 
