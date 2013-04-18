b2db3e0b934177c98d7afe4f9515d6dd9e009ea6
Joshua Harlow <harlowja@yahoo-inc.com>
2013-04-18 14:15:37 -0700

    Remove str merging for now.

--- cloudinit/mergers/__init__.py
+++ cloudinit/mergers/__init__.py
@@ -25,7 +25,7 @@ from cloudinit import type_utils
 NAME_MTCH = re.compile(r"(^[a-zA-Z_][A-Za-z0-9_]*)\((.*?)\)$")
 
 LOG = logging.getLogger(__name__)
-DEF_MERGE_TYPE = "list()+dict()+str()"
+DEF_MERGE_TYPE = "list()+dict()"
 MERGER_PREFIX = 'm_'
 MERGER_ATTR = 'Merger'
 
