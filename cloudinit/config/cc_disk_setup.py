d81f7c2a16000d791e7ba51f2b9042ddaa034abb
Eric Nordlund <nrdlngr@gmail.com>
2013-10-01 14:19:43 -0700

    Reordered several imports in order for test suite to pass.

--- cloudinit/config/cc_disk_setup.py
+++ cloudinit/config/cc_disk_setup.py
@@ -16,8 +16,8 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-from cloudinit import util
 from cloudinit.settings import PER_INSTANCE
+from cloudinit import util
 import logging
 import shlex
 
