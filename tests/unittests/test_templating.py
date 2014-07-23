7545bb234e01b2810ab354c6df93f0e84d184c4c
Dimitri John Ledkov <dimitri.ledkov@canonical.com>
2014-07-23 17:13:03 +0100

    more import statements

--- tests/unittests/test_templating.py
+++ tests/unittests/test_templating.py
@@ -16,7 +16,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from tests.unittests import helpers as test_helpers
+from . import helpers as test_helpers
 import textwrap
 
 from cloudinit import templater
