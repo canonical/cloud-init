21c6e0d8f5784991de1e1821b04eab0ea48b0295
Scott Moser <smoser@ubuntu.com>
2014-07-29 09:52:02 -0400

    do not put comments in /etc/timezone
    comments in /etc/timezone are not expected, and can cause problems
    if another tool tries to read it.

--- cloudinit/distros/__init__.py
+++ cloudinit/distros/__init__.py
@@ -856,3 +856,12 @@ def fetch(name):
     mod = importer.import_module(locs[0])
     cls = getattr(mod, 'Distro')
     return cls
+
+
+def set_etc_timezone(tz, tz_file=None, tz_conf="/etc/timezone",
+                     tz_local="/etc/localtime"):
+    util.write_file(tz_conf, str(tz).rstrip() + "\n")
+    # This ensures that the correct tz will be used for the system
+    if tz_local and tz_file:
+        util.copy(tz_file, self.tz_local_fn)
+    return
