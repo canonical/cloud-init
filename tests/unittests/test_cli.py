2dc69f02e30cf6c89a9f0f036e797c78d8e00b84
Scott Moser <smoser@ubuntu.com>
2015-05-15 16:57:35 -0400

    temporarily disasble test if no bin/cloud-init
    the tests of bin/cloud-init would fail in a package build environment.
    so, temporariliy skip them in that environment.

--- tests/unittests/test_cli.py
+++ tests/unittests/test_cli.py
@@ -1,6 +1,6 @@
 import imp
+import os
 import sys
-
 import six
 
 from . import helpers as test_helpers
@@ -9,6 +9,9 @@ try:
     from unittest import mock
 except ImportError:
     import mock
+
+
+BIN_CLOUDINIT = "bin/cloud-init"
 
 
 class TestCLI(test_helpers.FilesystemMockingTestCase):
@@ -25,16 +28,18 @@ class TestCLI(test_helpers.FilesystemMockingTestCase):
         self.patched_funcs.enter_context(
             mock.patch.object(sys, 'argv', ['cloud-init']))
         cli = imp.load_module(
-            'cli', open('bin/cloud-init'), '', ('', 'r', imp.PY_SOURCE))
+            'cli', open(BIN_CLOUDINIT), '', ('', 'r', imp.PY_SOURCE))
         try:
             return cli.main()
         except:
             pass
 
+    @test_helpers.skipIf(not os.path.isfile(BIN_CLOUDINIT), "no bin/cloudinit")
     def test_no_arguments_shows_usage(self):
         self._call_main()
         self.assertIn('usage: cloud-init', self.stderr.getvalue())
 
+    @test_helpers.skipIf(not os.path.isfile(BIN_CLOUDINIT), "no bin/cloudinit")
     def test_no_arguments_exits_2(self):
         exit_code = self._call_main()
         if self.sys_exit.call_count:
@@ -42,6 +47,7 @@ class TestCLI(test_helpers.FilesystemMockingTestCase):
         else:
             self.assertEqual(2, exit_code)
 
+    @test_helpers.skipIf(not os.path.isfile(BIN_CLOUDINIT), "no bin/cloudinit")
     def test_no_arguments_shows_error_message(self):
         self._call_main()
         self.assertIn('cloud-init: error: too few arguments',
