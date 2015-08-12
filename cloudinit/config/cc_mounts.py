827b7b903abc07d5fb04591bbae5587e6dc44993
Scott Moser <smoser@ubuntu.com>
2015-08-12 12:51:39 -0400

    swap: create swap with fallocate if possible
    fallocate is much faster than 'dd' for creating and initializing a
    swap file.
    
    LP: #1482994

--- cloudinit/config/cc_mounts.py
+++ cloudinit/config/cc_mounts.py
@@ -206,7 +206,8 @@ def setup_swapfile(fname, size=None, maxsize=None):
         util.log_time(LOG.debug, msg, func=util.subp,
             args=[['sh', '-c',
                    ('rm -f "$1" && umask 0066 && '
-                    'dd if=/dev/zero "of=$1" bs=1M "count=$2" && '
+                    '{ fallocate -l "${2}M" "$1" || '
+                    '  dd if=/dev/zero "of=$1" bs=1M "count=$2"; } && '
                     'mkswap "$1" || { r=$?; rm -f "$1"; exit $r; }'),
                    'setup_swap', fname, mbsize]])
 
