200e811268b7c17e814cfb6b7ef3f603c0590abc
Sergii Golovatiuk <sgolovatiuk@mirantis.com>
2016-07-14 16:58:56 +0200

    Change StringIO to BytesIO in cc_mcollective.py
    * StringIO from six doesn't act as 'binary stream' in Python 3.
    This patch changes StringIO to BytesIO to have code compatible with
    Python 3 and Python 2.
    * Add try/except for IOError in case when server.cfg doesn't exists. This is
    necessary for unit tests or cases when server.cfg is not included to package
    * Add UnitTest for cc_mcollective.py
    
    LP: #1597699

---
+++ tests/unittests/test_handler/test_handler_mcollective.py
@@ -0,0 +1,60 @@
+from cloudinit.config import cc_mcollective
+from cloudinit import util
+
+from .. import helpers
+
+import configobj
+import logging
+import shutil
+from six import BytesIO
+import tempfile
+
+LOG = logging.getLogger(__name__)
+
+
+class TestConfig(helpers.FilesystemMockingTestCase):
+    def setUp(self):
+        super(TestConfig, self).setUp()
+        self.tmp = tempfile.mkdtemp()
+        self.addCleanup(shutil.rmtree, self.tmp)
+
+    def test_basic_config(self):
+        cfg = {
+            'mcollective': {
+                'conf': {
+                    'loglevel': 'debug',
+                    'connector': 'rabbitmq',
+                    'logfile': '/var/log/mcollective.log',
+                    'ttl': '4294957',
+                    'collectives': 'mcollective',
+                    'main_collective': 'mcollective',
+                    'securityprovider': 'psk',
+                    'daemonize': '1',
+                    'factsource': 'yaml',
+                    'direct_addressing': '1',
+                    'plugin.psk': 'unset',
+                    'libdir': '/usr/share/mcollective/plugins',
+                    'identity': '1',
+                },
+            },
+        }
+        self.patchUtils(self.tmp)
+        cc_mcollective.configure(cfg['mcollective']['conf'])
+        contents = util.load_file("/etc/mcollective/server.cfg", decode=False)
+        contents = configobj.ConfigObj(BytesIO(contents))
+        expected = {
+                'loglevel': 'debug',
+                'connector': 'rabbitmq',
+                'logfile': '/var/log/mcollective.log',
+                'ttl': '4294957',
+                'collectives': 'mcollective',
+                'main_collective': 'mcollective',
+                'securityprovider': 'psk',
+                'daemonize': '1',
+                'factsource': 'yaml',
+                'direct_addressing': '1',
+                'plugin.psk': 'unset',
+                'libdir': '/usr/share/mcollective/plugins',
+                'identity': '1',
+                }
+        self.assertEqual(expected, dict(contents))
