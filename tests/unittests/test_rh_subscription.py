bc4d9c5964f486cd2e6c5b29808428cb2aee6f37
Scott Moser <smoser@brickies.net>
2016-11-22 14:06:09 -0500

    flake8: fix trailing white space

--- tests/unittests/test_rh_subscription.py
+++ tests/unittests/test_rh_subscription.py
@@ -83,7 +83,7 @@ class GoodTests(TestCase):
         '''
         call_lists = []
         call_lists.append(['attach', '--pool=pool1', '--pool=pool3'])
-        call_lists.append(['repos', '--disable=repo5', '--enable=repo2', 
+        call_lists.append(['repos', '--disable=repo5', '--enable=repo2',
                            '--enable=repo3'])
         call_lists.append(['attach', '--auto', '--servicelevel=self-support'])
         self.SM.log_success = mock.MagicMock()
