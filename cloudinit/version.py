7140b42117422de6d4567b0284fbbcc12cecd759
Chad Smith <chad.smith@canonical.com>
2018-06-20 11:33:36 -0600

    18.3-0ubuntu1 (patches unapplied)
    Imported using git-ubuntu import.

--- cloudinit/version.py
+++ cloudinit/version.py
@@ -4,7 +4,7 @@
 #
 # This file is part of cloud-init. See LICENSE file for license information.
 
-__VERSION__ = "18.2"
+__VERSION__ = "18.3"
 _PACKAGED_VERSION = '@@PACKAGED_VERSION@@'
 
 FEATURES = [
