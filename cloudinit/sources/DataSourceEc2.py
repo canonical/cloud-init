17d86be13a820ed6a9bd56e07a2620f76be9fc3c
Ryan Harper <ryan.harper@canonical.com>
2019-11-21 12:24:21 -0600

    Update cloudinit/sources/DataSourceEc2.py
    Co-Authored-By: Chad Smith <blackboxsw@gmail.com>

--- cloudinit/sources/DataSourceEc2.py
+++ cloudinit/sources/DataSourceEc2.py
@@ -440,6 +440,7 @@ class DataSourceEc2(sources.DataSource):
         return response.contents
 
     def _skip_or_refresh_stale_aws_token_cb(self, msg, exception):
+        """Callback will not retry on SKIP_USERDATA_CODES or if no token is available"""
         retry = ec2.skip_retry_on_codes(
             ec2.SKIP_USERDATA_CODES, msg, exception)
         if not retry:
