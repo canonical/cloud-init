5819a290df0794327ecaf90a2d885b2d6b25e036
Eduardo Otubo <otubo@redhat.com>
2019-12-05 17:22:30 +0100

    Removing strip()

--- cloudinit/ssh_util.py
+++ cloudinit/ssh_util.py
@@ -220,7 +220,7 @@ def render_authorizedkeysfile_paths(value, homedir, username):
     macros = (("%h", homedir), ("%u", username), ("%%", "%"))
     if not value:
         value = "%h/.ssh/authorized_keys"
-    paths = value.strip().split()
+    paths = value.split()
     rendered = []
     for path in paths:
         for macro, field in macros:
