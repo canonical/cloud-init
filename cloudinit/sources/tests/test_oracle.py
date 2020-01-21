bb71a9d08d25193836eda91c328760305285574e
Daniel Watkins <oddbloke@ubuntu.com>
2020-01-21 18:02:42 -0500

    Drop most of the remaining use of six (#179)

--- cloudinit/sources/tests/test_oracle.py
+++ cloudinit/sources/tests/test_oracle.py
@@ -13,7 +13,6 @@ import httpretty
 import json
 import mock
 import os
-import six
 import uuid
 
 DS_PATH = "cloudinit.sources.DataSourceOracle"
@@ -334,7 +333,7 @@ class TestReadMetaData(test_helpers.HttprettyTestCase):
         for k, v in data.items():
             httpretty.register_uri(
                 httpretty.GET, self.mdurl + MD_VER + "/" + k,
-                v if not isinstance(v, six.text_type) else v.encode('utf-8'))
+                v if not isinstance(v, str) else v.encode('utf-8'))
 
     def test_broken_no_sys_uuid(self, m_read_system_uuid):
         """Datasource requires ability to read system_uuid and true return."""
