cdd1eb1392b0c48383159be67aa125953254462a
Daniel Watkins <oddbloke@ubuntu.com>
2020-01-21 17:41:06 -0500

    tests/unittests/test_log: sort imports

--- tests/unittests/test_log.py
+++ tests/unittests/test_log.py
@@ -2,13 +2,14 @@
 
 """Tests for cloudinit.log """
 
-from cloudinit.analyze.dump import CLOUD_INIT_ASCTIME_FMT
-from cloudinit import log as ci_logging
-from cloudinit.tests.helpers import CiTestCase
 import datetime
 import io
 import logging
 import time
+
+from cloudinit import log as ci_logging
+from cloudinit.analyze.dump import CLOUD_INIT_ASCTIME_FMT
+from cloudinit.tests.helpers import CiTestCase
 
 
 class TestCloudInitLogger(CiTestCase):
