8f0c909798a9712162a8d91be4e2d7741b7203bf
Daniel Watkins <oddbloke@ubuntu.com>
2020-05-04 14:21:25 -0400

    conftest: add docs and tests regarding CiTestCase's subp functionality

--- cloudinit/tests/test_conftest.py
+++ cloudinit/tests/test_conftest.py
@@ -1,9 +1,11 @@
 import pytest
 
 from cloudinit import util
+from cloudinit.tests.helpers import CiTestCase
 
 
 class TestDisableSubpUsage:
+    """Test that the disable_subp_usage fixture behaves as expected."""
 
     def test_using_subp_raises_assertion_error(self):
         with pytest.raises(AssertionError):
@@ -16,3 +18,23 @@ class TestDisableSubpUsage:
     @pytest.mark.parametrize('disable_subp_usage', [False], indirect=True)
     def test_subp_usage_can_be_reenabled(self):
         util.subp(['whoami'])
+
+
+class TestDisableSubpUsageInTestSubclass(CiTestCase):
+    """Test that disable_subp_usage doesn't impact CiTestCase's subp logic."""
+
+    def test_using_subp_raises_assertion_error(self):
+        with pytest.raises(Exception):
+            util.subp(["some", "args"])
+
+    def test_typeerrors_on_incorrect_usage(self):
+        with pytest.raises(IndexError):
+            util.subp()
+
+    def test_subp_usage_can_be_reenabled(self):
+        _old_allowed_subp = self.allow_subp
+        self.allowed_subp = True
+        try:
+            util.subp(['whoami'])
+        finally:
+            self.allowed_subp = _old_allowed_subp
