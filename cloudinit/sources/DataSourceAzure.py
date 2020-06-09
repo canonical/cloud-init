b9e35b940ea13e46cf0875674d9353f7d91f3c0f
Johnson Shi <johsh@microsoft.com>
2020-06-09 02:51:55 +0000

    Refactor Azure report ready code.
    This PR refactors Azure report ready code to
    include more robust tests and telemetry.

--- cloudinit/sources/DataSourceAzure.py
+++ cloudinit/sources/DataSourceAzure.py
@@ -426,17 +426,19 @@ class DataSourceAzure(sources.DataSource):
                     ret = load_azure_ds_dir(cdev)
 
             except NonAzureDataSource:
-                report_diagnostic_event(
-                    "Did not find Azure data source in %s" % cdev)
+                msg = "Did not find Azure data source in %s" % cdev
+                LOG.debug(msg)
+                report_diagnostic_event(msg)
                 continue
             except BrokenAzureDataSource as exc:
                 msg = 'BrokenAzureDataSource: %s' % exc
+                LOG.error(msg)
                 report_diagnostic_event(msg)
                 raise sources.InvalidMetaDataException(msg)
             except util.MountFailedError:
                 msg = '%s was not mountable' % cdev
-                report_diagnostic_event(msg)
                 LOG.warning(msg)
+                report_diagnostic_event(msg)
                 continue
 
             perform_reprovision = reprovision or self._should_reprovision(ret)
@@ -464,6 +466,7 @@ class DataSourceAzure(sources.DataSource):
 
         if not found:
             msg = 'No Azure metadata found'
+            LOG.error(msg)
             report_diagnostic_event(msg)
             raise sources.InvalidMetaDataException(msg)
 
@@ -488,8 +491,9 @@ class DataSourceAzure(sources.DataSource):
                             azure_ds_reporter) as lease:
                         self._report_ready(lease=lease)
                 except Exception as e:
-                    report_diagnostic_event(
-                        "exception while reporting ready: %s" % e)
+                    msg = "exception while reporting ready: %s" % e
+                    LOG.error(msg)
+                    report_diagnostic_event(msg)
                     raise
         return crawled_data
 
@@ -617,10 +621,11 @@ class DataSourceAzure(sources.DataSource):
                 else:
                     # If we get an exception while trying to call IMDS, we call
                     # DHCP and setup the ephemeral network to acquire a new IP.
-                    report_diagnostic_event("poll IMDS with %s failed. "
-                                            "Exception: %s and code: %s" %
-                                            (msg, exception.cause,
-                                             exception.code))
+                    evt_msg = ("poll IMDS with %s failed. "
+                               "Exception: %s and code: %s" %
+                               (msg, exception.cause, exception.code))
+                    LOG.warning(evt_msg)
+                    report_diagnostic_event(evt_msg)
                     return False
 
                 LOG.debug("poll IMDS failed with an unexpected exception: %s",
@@ -644,8 +649,8 @@ class DataSourceAzure(sources.DataSource):
                     try:
                         nl_sock = netlink.create_bound_netlink_socket()
                     except netlink.NetlinkCreateSocketError as e:
-                        report_diagnostic_event(e)
                         LOG.warning(e)
+                        report_diagnostic_event(e)
                         self._ephemeral_dhcp_ctx.clean_network()
                         break
 
@@ -665,8 +670,8 @@ class DataSourceAzure(sources.DataSource):
                             netlink.wait_for_media_disconnect_connect(
                                 nl_sock, lease['interface'])
                         except AssertionError as error:
-                            report_diagnostic_event(error)
                             LOG.error(error)
+                            report_diagnostic_event(error)
                             break
 
                     vnet_switched = True
@@ -692,10 +697,12 @@ class DataSourceAzure(sources.DataSource):
                     nl_sock.close()
 
         if vnet_switched:
-            report_diagnostic_event("attempted dhcp %d times after reuse" %
-                                    dhcp_attempts)
-            report_diagnostic_event("polled imds %d times after reuse" %
-                                    self.imds_poll_counter)
+            msg = "attempted dhcp %d times after reuse" % dhcp_attempts
+            LOG.debug(msg)
+            report_diagnostic_event(msg)
+            msg = "polled imds %d times after reuse" % self.imds_poll_counter
+            LOG.debug(msg)
+            report_diagnostic_event(msg)
 
         return return_val
 
@@ -768,12 +775,12 @@ class DataSourceAzure(sources.DataSource):
         try:
             fabric_data = metadata_func()
         except Exception as e:
+            LOG.error(
+                "Error communicating with Azure fabric; You may experience "
+                "connectivity issues.", exc_info=True)
             report_diagnostic_event(
                 "Error communicating with Azure fabric; You may experience "
                 "connectivity issues: %s" % e)
-            LOG.warning(
-                "Error communicating with Azure fabric; You may experience "
-                "connectivity issues.", exc_info=True)
             return False
 
         util.del_file(REPORTED_READY_MARKER_FILE)
@@ -1132,6 +1139,7 @@ def read_azure_ovf(contents):
         dom = minidom.parseString(contents)
     except Exception as e:
         error_str = "Invalid ovf-env.xml: %s" % e
+        LOG.error(error_str)
         report_diagnostic_event(error_str)
         raise BrokenAzureDataSource(error_str)
 
@@ -1414,7 +1422,9 @@ def get_metadata_from_imds(fallback_nic, retries):
                     azure_ds_reporter, fallback_nic):
                 return util.log_time(**kwargs)
         except Exception as e:
-            report_diagnostic_event("exception while getting metadata: %s" % e)
+            msg = "exception while getting metadata: %s" % e
+            LOG.error(msg)
+            report_diagnostic_event(msg)
             raise
 
 
@@ -1429,15 +1439,15 @@ def _get_metadata_from_imds(retries):
             retries=retries, exception_cb=retry_on_url_exc)
     except Exception as e:
         msg = 'Ignoring IMDS instance metadata: %s' % e
+        LOG.error(msg)
         report_diagnostic_event(msg)
-        LOG.debug(msg)
         return {}
     try:
         return util.load_json(str(response))
     except json.decoder.JSONDecodeError as e:
-        report_diagnostic_event('non-json imds response' % e)
-        LOG.warning(
+        LOG.error(
             'Ignoring non-json IMDS instance metadata: %s', str(response))
+        report_diagnostic_event('non-json imds response' % e)
     return {}
 
 
