c9df3d06f061abdefd1af8d165530be07af977a6
Daniel Watkins <oddbloke@ubuntu.com>
2020-07-03 13:53:43 -0400

    cloudinit: remove global disable of pylint W0105 and fix errors
    This includes a fix to a test that had a string concatenation issue, and
    so was only testing a prefix of what was intended.

--- cloudinit/net/eni.py
+++ cloudinit/net/eni.py
@@ -483,10 +483,8 @@ class Renderer(renderer.Renderer):
         if searchdomains:
             lo['subnets'][0]["dns_search"] = (" ".join(searchdomains))
 
-        ''' Apply a sort order to ensure that we write out
-            the physical interfaces first; this is critical for
-            bonding
-        '''
+        # Apply a sort order to ensure that we write out the physical
+        # interfaces first; this is critical for bonding
         order = {
             'loopback': 0,
             'physical': 1,
