c068f992c3905d07732dafa6eb61c1ae3aa65916
James Falcon <TheRealFalcon@users.noreply.github.com>
2020-08-07 10:19:09 -0500

    Recognize LABEL_FATBOOT labels (#513)
    Update DataSourceNoCloud and ds-identify to recognize LABEL_FATBOOT labels from blkid.
    Also updated associated tests.
    
    LP: #1841466

--- cloudinit/util.py
+++ cloudinit/util.py
@@ -1112,6 +1112,7 @@ def close_stdin():
 
 def find_devs_with_freebsd(criteria=None, oformat='device',
                            tag=None, no_cache=False, path=None):
+    devlist = []
     if not criteria:
         return glob.glob("/dev/msdosfs/*") + glob.glob("/dev/iso9660/*")
     if criteria.startswith("LABEL="):
