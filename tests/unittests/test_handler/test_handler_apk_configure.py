22dd3a0aa2f69d4bf7ef1bdc68d110b9c6595f5c
Dermot Bradley <dermot_bradley@yahoo.com>
2020-08-14 19:04:05 +0100

    Fixed test_handler_apk_configure.py - all its testcases are passing now.

--- tests/unittests/test_handler/test_handler_apk_configure.py
+++ tests/unittests/test_handler/test_handler_apk_configure.py
@@ -6,20 +6,18 @@ Test creation of repositories file
 
 import logging
 import os
-import unittest
 
 from cloudinit import cloud
 from cloudinit import util
 
 from cloudinit.config import cc_apk_configure
-from cloudinit.tests.helpers import TestCase
+from cloudinit.tests.helpers import FilesystemMockingTestCase
 
 
 REPO_FILE = "/etc/apk/repositories"
 DEFAULT_MIRROR_URL = "https://alpine.global.ssl.fastly.net/alpine"
 
-EXPECTED_COMMENT_HEADER = """
-#
+EXPECTED_COMMENT_HEADER = """#
 # Created by cloud-init
 #
 # This file is written on first boot of an instance
@@ -28,38 +26,34 @@ EXPECTED_COMMENT_HEADER = """
 """
 
 EXPECTED_ALPINE_312_TESTING_COMMENT = """
-
 #
 # Testing - using this with a non-Edge installation will likely cause problems!
 #
 """
 
 EXPECTED_LOCAL_REPO_COMMENT_HEADER = """
-
 #
 # Local repo
 #
 """
 
 
-class TestNoConfig(unittest.TestCase):
+class TestNoConfig(FilesystemMockingTestCase):
     def setUp(self):
         super(TestNoConfig, self).setUp()
+        self.new_root = self.tmp_dir()
+        util.ensure_dir(os.path.join(self.new_root, 'etc/apk'))
         self.name = "apk-configure"
         self.cloud_init = None
         self.log = logging.getLogger("TestNoConfig")
         self.args = []
-
-    def _makedir(self):
-        if not os.path.exists('/etc/apk'):
-            os.makedirs('/etc/apk')
 
     def test_no_config(self):
         """
         Test that nothing is done if no apk-configure
         configuration is provided.
         """
-        self._makedir()
+        self.new_root = self.reRoot(root=self.new_root)
         config = util.get_builtin_cfg()
 
         cc_apk_configure.handle(self.name, config, self.cloud_init,
@@ -68,25 +62,23 @@ class TestNoConfig(unittest.TestCase):
         self.assertRaises(IOError, util.load_file, REPO_FILE)
 
 
-class TestConfig(TestCase):
+class TestConfig(FilesystemMockingTestCase):
     def setUp(self):
         super(TestConfig, self).setUp()
+        self.new_root = self.tmp_dir()
+        util.ensure_dir(os.path.join(self.new_root, 'etc/apk'))
         self.name = "apk-configure"
         self.paths = None
         self.cloud = cloud.Cloud(None, self.paths, None, None, None)
         self.log = logging.getLogger("TestNoConfig")
         self.args = []
 
-    def _makedir(self):
-        if not os.path.exists('/etc/apk'):
-            os.makedirs('/etc/apk')
-
     def test_no_repo_settings(self):
         """
         Test that nothing is written if the 'alpine-repo' key
         is not present.
         """
-        self._makedir()
+        self.new_root = self.reRoot(root=self.new_root)
         config = {"apk_repos": {}}
 
         cc_apk_configure.handle(self.name, config, self.cloud, self.log,
@@ -98,8 +90,8 @@ class TestConfig(TestCase):
         """
         Test that nothing is written if 'alpine_repo' list is empty.
         """
-        self._makedir()
-        config = {"apk_repos": {"alpine_repos": []}}
+        self.new_root = self.reRoot(root=self.new_root)
+        config = {"apk_repos": {"alpine_repo": []}}
 
         cc_apk_configure.handle(self.name, config, self.cloud, self.log,
                                 self.args)
@@ -110,11 +102,11 @@ class TestConfig(TestCase):
         """
         Test when only details of main repo is written to file.
         """
-        self._makedir()
+        self.new_root = self.reRoot(root=self.new_root)
         alpine_version = 'v3.12'
         config = {
             "apk_repos": {
-                "alpine_repos": {
+                "alpine_repo": {
                     "version": alpine_version
                 }
             }
@@ -132,11 +124,11 @@ class TestConfig(TestCase):
         Test when only details of main and community repos are
         written to file.
         """
-        self._makedir()
+        self.new_root = self.reRoot(root=self.new_root)
         alpine_version = 'edge'
         config = {
             "apk_repos": {
-                "alpine_repos": {
+                "alpine_repo": {
                     "version": alpine_version,
                     "community_enabled": True
                 }
@@ -158,11 +150,11 @@ class TestConfig(TestCase):
         Test when details of main, community and testing repos
         are written to file.
         """
-        self._makedir()
+        self.new_root = self.reRoot(root=self.new_root)
         alpine_version = 'v3.12'
         config = {
             "apk_repos": {
-                "alpine_repos": {
+                "alpine_repo": {
                     "version": alpine_version,
                     "community_enabled": True,
                     "testing_enabled": True
@@ -176,8 +168,7 @@ class TestConfig(TestCase):
         main_repo = DEFAULT_MIRROR_URL + '/' + alpine_version + '/main\n'
         community_repo = (DEFAULT_MIRROR_URL + '/' + alpine_version +
                           '/community\n')
-        testing_repo = (DEFAULT_MIRROR_URL + '/' + alpine_version +
-                        '/testing\n')
+        testing_repo = DEFAULT_MIRROR_URL + '/edge/testing\n'
         expected = (EXPECTED_COMMENT_HEADER + main_repo +
                     community_repo + EXPECTED_ALPINE_312_TESTING_COMMENT +
                     testing_repo + '\n')
@@ -188,11 +179,11 @@ class TestConfig(TestCase):
         Test when details of main, community and testing repos
         for Edge version of Alpine are written to file.
         """
-        self._makedir()
+        self.new_root = self.reRoot(root=self.new_root)
         alpine_version = 'edge'
         config = {
             "apk_repos": {
-                "alpine_repos": {
+                "alpine_repo": {
                     "version": alpine_version,
                     "community_enabled": True,
                     "testing_enabled": True
@@ -206,8 +197,7 @@ class TestConfig(TestCase):
         main_repo = DEFAULT_MIRROR_URL + '/' + alpine_version + '/main\n'
         community_repo = (DEFAULT_MIRROR_URL + '/' + alpine_version +
                           '/community\n')
-        testing_repo = (DEFAULT_MIRROR_URL + '/' + alpine_version +
-                        '/testing\n')
+        testing_repo = DEFAULT_MIRROR_URL + '/edge/testing\n'
         expected = (EXPECTED_COMMENT_HEADER + main_repo + community_repo +
                     testing_repo + '\n')
         self.assertEqual(util.load_file(REPO_FILE), expected)
@@ -217,12 +207,12 @@ class TestConfig(TestCase):
         Test when details of main, community, testing and
         local repos are written to file.
         """
-        self._makedir()
+        self.new_root = self.reRoot(root=self.new_root)
         alpine_version = 'v3.12'
         local_repo_url = 'http://some.mirror/whereever'
         config = {
             "apk_repos": {
-                "alpine_repos": {
+                "alpine_repo": {
                     "version": alpine_version,
                     "community_enabled": True,
                     "testing_enabled": True
@@ -237,8 +227,7 @@ class TestConfig(TestCase):
         main_repo = DEFAULT_MIRROR_URL + '/' + alpine_version + '/main\n'
         community_repo = (DEFAULT_MIRROR_URL + '/' + alpine_version +
                           '/community\n')
-        testing_repo = (DEFAULT_MIRROR_URL + '/' + alpine_version +
-                        '/testing\n')
+        testing_repo = DEFAULT_MIRROR_URL + '/edge/testing\n'
         local_repo = local_repo_url + '/' + alpine_version + '\n'
         expected = (EXPECTED_COMMENT_HEADER + main_repo + community_repo +
                     EXPECTED_ALPINE_312_TESTING_COMMENT + testing_repo +
@@ -250,12 +239,12 @@ class TestConfig(TestCase):
         Test when details of main, community, testing and local repos
         for Edge version of Alpine are written to file.
         """
-        self._makedir()
+        self.new_root = self.reRoot(root=self.new_root)
         alpine_version = 'edge'
         local_repo_url = 'http://some.mirror/whereever'
         config = {
             "apk_repos": {
-                "alpine_repos": {
+                "alpine_repo": {
                     "version": alpine_version,
                     "community_enabled": True,
                     "testing_enabled": True
@@ -270,8 +259,7 @@ class TestConfig(TestCase):
         main_repo = DEFAULT_MIRROR_URL + '/' + alpine_version + '/main\n'
         community_repo = (DEFAULT_MIRROR_URL + '/' + alpine_version +
                           '/community\n')
-        testing_repo = (DEFAULT_MIRROR_URL + '/' + alpine_version +
-                        '/testing\n')
+        testing_repo = DEFAULT_MIRROR_URL + '/edge/testing\n'
         local_repo = local_repo_url + '/' + alpine_version + '\n'
         expected = (EXPECTED_COMMENT_HEADER + main_repo + community_repo +
                     testing_repo + EXPECTED_LOCAL_REPO_COMMENT_HEADER +
