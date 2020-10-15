5a2fd3f7e1434acbf65710a507c8872f7218aaa5
Mina Galić <me+git@igalic.co>
2020-10-15 21:14:59 +0200

    fix other flake8 complaints by silencing E501
    this is really unreadable to fix otherwise.

--- cloudinit/sources/DataSourceOracle.py
+++ cloudinit/sources/DataSourceOracle.py
@@ -186,7 +186,7 @@ class DataSourceOracle(sources.DataSource):
 
             if not self._network_config:
                 # this is now v2
-                self._network_config = self.distro.networking.generate_fallback_config()
+                self._network_config = self.distro.networking.generate_fallback_config()  # noqa: E501
 
             if self.ds_cfg.get(
                 'configure_secondary_nics',
