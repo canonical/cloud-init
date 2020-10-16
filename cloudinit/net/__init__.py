a03ffb9b07de9cd5ebefd7a44237194d6c855dfa
Mina Galić <me+git@igalic.co>
2020-10-16 21:32:26 +0200

    remove cloudinit.net.generate_fallback_config and watch the world burn

--- cloudinit/net/__init__.py
+++ cloudinit/net/__init__.py
@@ -428,31 +428,6 @@ def find_fallback_nic_on_linux(blacklist_drivers=None):
     return None
 
 
-def generate_fallback_config(blacklist_drivers=None, config_driver=None):
-    """Generate network cfg v2 for dhcp on the NIC most likely connected."""
-    if not config_driver:
-        config_driver = False
-
-    target_name = find_fallback_nic(blacklist_drivers=blacklist_drivers)
-    if not target_name:
-        # can't read any interfaces addresses (or there are none); give up
-        return None
-
-    # netfail cannot use mac for matching, they have duplicate macs
-    if is_netfail_master(target_name):
-        match = {'name': target_name}
-    else:
-        match = {
-            'macaddress': read_sys_net_safe(target_name, 'address').lower()}
-    cfg = {'dhcp4': True, 'set-name': target_name, 'match': match}
-    if config_driver:
-        driver = device_driver(target_name)
-        if driver:
-            cfg['match']['driver'] = driver
-    nconf = {'ethernets': {target_name: cfg}, 'version': 2}
-    return nconf
-
-
 def extract_physdevs(netcfg):
 
     def _version_1(netcfg):
