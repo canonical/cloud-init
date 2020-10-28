b294f36d3b5098cb88758221910afdcb245f14db
cawamata <1749824+cawamata@users.noreply.github.com>
2020-10-29 00:23:53 +0900

    add rhel support

--- cloudinit/config/cc_ca_certs.py
+++ cloudinit/config/cc_ca_certs.py
@@ -44,48 +44,92 @@ import os
 from cloudinit import subp
 from cloudinit import util
 
-CA_CERT_PATH = "/usr/share/ca-certificates/"
-CA_CERT_FILENAME = "cloud-init-ca-certs.crt"
-CA_CERT_CONFIG = "/etc/ca-certificates.conf"
-CA_CERT_SYSTEM_PATH = "/etc/ssl/certs/"
-CA_CERT_FULL_PATH = os.path.join(CA_CERT_PATH, CA_CERT_FILENAME)
+DISTRO_DEFAULT_CONFIG = {
+    'ubuntu': {
+        'ca_cert_path': '/usr/share/ca-certificates/',
+        'ca_cert_filename': 'cloud-init-ca-certs.crt',
+        'ca_cert_config': '/etc/ca-certificates.conf',
+        'ca_cert_system_path': '/etc/ssl/certs/',
+        'ca_cert_update_exe': 'update-ca-certificates'
+    },
+    'debian': {
+        'ca_cert_path': '/usr/share/ca-certificates/',
+        'ca_cert_filename': 'cloud-init-ca-certs.crt',
+        'ca_cert_config': '/etc/ca-certificates.conf',
+        'ca_cert_system_path': '/etc/ssl/certs/',
+        'ca_cert_update_exe': 'update-ca-certificates'
+    },
+    'alpine': {
+        'ca_cert_path': '/usr/share/ca-certificates/',
+        'ca_cert_filename': 'cloud-init-ca-certs.crt',
+        'ca_cert_config': '/etc/ca-certificates.conf',
+        'ca_cert_system_path': '/etc/ssl/certs/',
+        'ca_cert_update_exe': 'update-ca-certificates'
+    },
+    'rhel': {
+        'ca_cert_path': '/usr/share/pki/ca-trust-source/',
+        'ca_cert_filename': 'anchors/cloud-init-ca-certs.crt',
+        'ca_cert_config': '',
+        'ca_cert_system_path': '/etc/pki/ca-trust/',
+        'ca_cert_update_exe': 'update-ca-trust'
+    }
+}
 
-distros = ['alpine', 'debian', 'ubuntu']
+
+distros = ['alpine', 'debian', 'ubuntu', 'rhel']
 
 
-def update_ca_certs():
+def _distro_ca_certs_configs(distro):
+    """Construct a distro-specific ca_certs config dictionary by merging
+       distro specific changes into base config.
+
+    @param distro: String providing the distro class name.
+    @returns: Dict of distro configurations for ntp clients.
+    """
+    dcfg = DISTRO_DEFAULT_CONFIG
+    if distro in dcfg:
+        cfg = DISTRO_DEFAULT_CONFIG[distro]
+        cfg['ca_cert_full_path'] = os.path.join(cfg['ca_cert_path'], cfg['ca_cert_filename'])
+    else:
+        cfg = {}
+    return cfg
+
+def update_ca_certs(distro_name):
     """
     Updates the CA certificate cache on the current machine.
     """
-    subp.subp(["update-ca-certificates"], capture=False)
+    distro_cfg = _distro_ca_certs_configs(distro_name)
+    subp.subp([distro_cfg['ca_cert_update_exe']], capture=False)
 
 
-def add_ca_certs(certs):
+def add_ca_certs(distro_name, certs):
     """
     Adds certificates to the system. To actually apply the new certificates
     you must also call L{update_ca_certs}.
 
     @param certs: A list of certificate strings.
     """
+    distro_cfg = _distro_ca_certs_configs(distro_name)
     if certs:
         # First ensure they are strings...
         cert_file_contents = "\n".join([str(c) for c in certs])
-        util.write_file(CA_CERT_FULL_PATH, cert_file_contents, mode=0o644)
+        util.write_file(distro_cfg['ca_cert_full_path'], cert_file_contents, mode=0o644)
 
-        if os.stat(CA_CERT_CONFIG).st_size == 0:
-            # If the CA_CERT_CONFIG file is empty (i.e. all existing
-            # CA certs have been deleted) then simply output a single
-            # line with the cloud-init cert filename.
-            out = "%s\n" % CA_CERT_FILENAME
-        else:
-            # Append cert filename to CA_CERT_CONFIG file.
-            # We have to strip the content because blank lines in the file
-            # causes subsequent entries to be ignored. (LP: #1077020)
-            orig = util.load_file(CA_CERT_CONFIG)
-            cur_cont = '\n'.join([line for line in orig.splitlines()
-                                  if line != CA_CERT_FILENAME])
-            out = "%s\n%s\n" % (cur_cont.rstrip(), CA_CERT_FILENAME)
-        util.write_file(CA_CERT_CONFIG, out, omode="wb")
+        if distro_cfg['ca_cert_config'] != '':
+            if os.stat(distro_cfg['ca_cert_config']).st_size == 0:
+                # If the CA_CERT_CONFIG file is empty (i.e. all existing
+                # CA certs have been deleted) then simply output a single
+                # line with the cloud-init cert filename.
+                out = "%s\n" % distro_cfg['ca_cert_filename']
+            else:
+                # Append cert filename to CA_CERT_CONFIG file.
+                # We have to strip the content because blank lines in the file
+                # causes subsequent entries to be ignored. (LP: #1077020)
+                orig = util.load_file(distro_cfg['ca_cert_config'])
+                cur_cont = '\n'.join([line for line in orig.splitlines()
+                                      if line != distro_cfg['ca_cert_filename']])
+                out = "%s\n%s\n" % (cur_cont.rstrip(), distro_cfg['ca_cert_filename'])
+            util.write_file(distro_cfg['ca_cert_config'], out, omode="wb")
 
 
 def remove_default_ca_certs(distro_name):
@@ -93,11 +137,12 @@ def remove_default_ca_certs(distro_name):
     Removes all default trusted CA certificates from the system. To actually
     apply the change you must also call L{update_ca_certs}.
     """
-    util.delete_dir_contents(CA_CERT_PATH)
-    util.delete_dir_contents(CA_CERT_SYSTEM_PATH)
-    util.write_file(CA_CERT_CONFIG, "", mode=0o644)
+    distro_cfg = _distro_ca_certs_configs(distro_name)
+    util.delete_dir_contents(distro_cfg['ca_cert_path'])
+    util.delete_dir_contents(distro_cfg['ca_cert_system_path'])
+    util.write_file(distro_cfg['ca_cert_config'], "", mode=0o644)
 
-    if distro_name != 'alpine':
+    if distro_name == 'debian' or distro_name == 'ubuntu':
         debconf_sel = (
             "ca-certificates ca-certificates/trust_new_crts " + "select no")
         subp.subp(('debconf-set-selections', '-'), debconf_sel)
@@ -132,10 +177,10 @@ def handle(name, cfg, cloud, log, _args):
         trusted_certs = util.get_cfg_option_list(ca_cert_cfg, "trusted")
         if trusted_certs:
             log.debug("Adding %d certificates" % len(trusted_certs))
-            add_ca_certs(trusted_certs)
+            add_ca_certs(cloud.distro.name, trusted_certs)
 
     # Update the system with the new cert configuration.
     log.debug("Updating certificates")
-    update_ca_certs()
+    update_ca_certs(cloud.distro.name)
 
 # vi: ts=4 expandtab
