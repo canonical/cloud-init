36a904e1d7ac6ffd29d4e78b520216f44ee4dbdc
Mina Galić <me+git@igalic.co>
2020-10-29 13:07:51 +0100

    Fix mounting of vfat on *BSD
    normalize different names for vfat to "msdos" which works across BSDS.
    
    FreeBSD Bugzilla ref: https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=250496
    LP# https://bugs.launchpad.net/cloud-init/+bug/1901958

--- cloudinit/util.py
+++ cloudinit/util.py
@@ -1667,10 +1667,12 @@ def mount_cb(device, callback, data=None, mtype=None,
             mtypes = ["auto"]
     elif platsys.endswith("bsd"):
         if mtypes is None:
-            mtypes = ['ufs', 'cd9660', 'vfat']
+            mtypes = ['ufs', 'cd9660', 'msdos']
         for index, mtype in enumerate(mtypes):
             if mtype == "iso9660":
                 mtypes[index] = "cd9660"
+            if mtype in ["vfat", "msdosfs", "msdos"]:
+                mtypes[index] = "msdos"
     else:
         # we cannot do a smart "auto", so just call 'mount' once with no -t
         mtypes = ['']
