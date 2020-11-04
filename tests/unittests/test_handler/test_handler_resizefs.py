6ff33c48bae261b5c610aaadcde3621d2f550239
Mina Galić <me+git@igalic.co>
2020-11-04 22:42:10 +0100

    add test verifying new behaviour

--- tests/unittests/test_handler/test_handler_resizefs.py
+++ tests/unittests/test_handler/test_handler_resizefs.py
@@ -22,24 +22,16 @@ class TestResizefs(CiTestCase):
         super(TestResizefs, self).setUp()
         self.name = "resizefs"
 
-    @mock.patch('cloudinit.config.cc_resizefs._get_dumpfs_output')
-    @mock.patch('cloudinit.config.cc_resizefs._get_gpart_output')
-    def test_skip_ufs_resize(self, gpart_out, dumpfs_out):
+    @mock.patch('cloudinit.subp.subp')
+    def test_skip_ufs_resize(self, growfs_N_out):
         fs_type = "ufs"
         resize_what = "/"
         devpth = "/dev/da0p2"
-        dumpfs_out.return_value = (
-            "# newfs command for / (/dev/label/rootfs)\n"
-            "newfs -O 2 -U -a 4 -b 32768 -d 32768 -e 4096 "
-            "-f 4096 -g 16384 -h 64 -i 8192 -j -k 6408 -m 8 "
-            "-o time -s 58719232 /dev/label/rootfs\n")
-        gpart_out.return_value = textwrap.dedent("""\
-            =>      40  62914480  da0  GPT  (30G)
-                    40      1024    1  freebsd-boot  (512K)
-                  1064  58719232    2  freebsd-ufs  (28G)
-              58720296   3145728    3  freebsd-swap  (1.5G)
-              61866024   1048496       - free -  (512M)
-            """)
+        growfs_N_out.return_value = (
+            None,
+            ("growfs: requested size 2.0GB is not larger than the current "
+             "filesystem size 2.0GB\n")
+        )
         res = can_skip_resize(fs_type, resize_what, devpth)
         self.assertTrue(res)
 
