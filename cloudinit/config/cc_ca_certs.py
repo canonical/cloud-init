4041e8ff1390fa8fc8a49aa87a5b6f7e4a0157d1
cawamata <1749824+cawamata@users.noreply.github.com>
2020-11-24 23:10:47 +0900

    Update cc_ca_certs.py
    reflect PR review comments

--- cloudinit/config/cc_ca_certs.py
+++ cloudinit/config/cc_ca_certs.py
@@ -25,7 +25,7 @@ can be removed from the system with the configuration option
 
 **Module frequency:** per instance
 
-**Supported distros:** alpine, debian, ubuntu
+**Supported distros:** alpine, debian, ubuntu, rhel
 
 **Config keys**::
 
@@ -44,32 +44,18 @@ import os
 from cloudinit import subp
 from cloudinit import util
 
-DISTRO_DEFAULT_CONFIG = {
-    'ubuntu': {
-        'ca_cert_path': '/usr/share/ca-certificates/',
-        'ca_cert_filename': 'cloud-init-ca-certs.crt',
-        'ca_cert_config': '/etc/ca-certificates.conf',
-        'ca_cert_system_path': '/etc/ssl/certs/',
-        'ca_cert_update_exe': 'update-ca-certificates'
-    },
-    'debian': {
-        'ca_cert_path': '/usr/share/ca-certificates/',
-        'ca_cert_filename': 'cloud-init-ca-certs.crt',
-        'ca_cert_config': '/etc/ca-certificates.conf',
-        'ca_cert_system_path': '/etc/ssl/certs/',
-        'ca_cert_update_exe': 'update-ca-certificates'
-    },
-    'alpine': {
-        'ca_cert_path': '/usr/share/ca-certificates/',
-        'ca_cert_filename': 'cloud-init-ca-certs.crt',
-        'ca_cert_config': '/etc/ca-certificates.conf',
-        'ca_cert_system_path': '/etc/ssl/certs/',
-        'ca_cert_update_exe': 'update-ca-certificates'
-    },
+DEFAULT_CONFIG = {
+    'ca_cert_path': '/usr/share/ca-certificates/',
+    'ca_cert_filename': 'cloud-init-ca-certs.crt',
+    'ca_cert_config': '/etc/ca-certificates.conf',
+    'ca_cert_system_path': '/etc/ssl/certs/',
+    'ca_cert_update_exe': 'update-ca-certificates'
+}
+DISTRO_OVERRIDES = {
     'rhel': {
         'ca_cert_path': '/usr/share/pki/ca-trust-source/',
         'ca_cert_filename': 'anchors/cloud-init-ca-certs.crt',
-        'ca_cert_config': '',
+        'ca_cert_config': None,
         'ca_cert_system_path': '/etc/pki/ca-trust/',
         'ca_cert_update_exe': 'update-ca-trust'
     }
@@ -80,25 +66,26 @@ distros = ['alpine', 'debian', 'ubuntu', 'rhel']
 
 
 def _distro_ca_certs_configs(distro):
-    """Construct a distro-specific ca_certs config dictionary by merging
-       distro specific changes into base config.
+    """Return a distro-specific ca_certs config dictionary
 
     @param distro: String providing the distro class name.
-    @returns: Dict of distro configurations for ntp clients.
+    @returns: Dict of distro configurations for ca-cert.
     """
-    dcfg = DISTRO_DEFAULT_CONFIG
+    cfg = DEFAULT_CONFIG
+    dcfg = DISTRO_OVERRIDES
     if distro in dcfg:
-        cfg = DISTRO_DEFAULT_CONFIG[distro]
-        cfg['ca_cert_full_path'] = os.path.join(cfg['ca_cert_path'],
-                                                cfg['ca_cert_filename'])
-    else:
-        cfg = {}
+        cfg = dcfg[distro]
+
+    cfg['ca_cert_full_path'] = os.path.join(cfg['ca_cert_path'],
+                                            cfg['ca_cert_filename'])
     return cfg
 
 
 def update_ca_certs(distro_name):
     """
     Updates the CA certificate cache on the current machine.
+
+    @param distro: String providing the distro class name.
     """
     distro_cfg = _distro_ca_certs_configs(distro_name)
     subp.subp([distro_cfg['ca_cert_update_exe']], capture=False)
@@ -109,32 +96,44 @@ def add_ca_certs(distro_name, certs):
     Adds certificates to the system. To actually apply the new certificates
     you must also call L{update_ca_certs}.
 
+    @param distro: String providing the distro class name.
     @param certs: A list of certificate strings.
     """
     distro_cfg = _distro_ca_certs_configs(distro_name)
-    if certs:
-        # First ensure they are strings...
-        cert_file_contents = "\n".join([str(c) for c in certs])
-        util.write_file(distro_cfg['ca_cert_full_path'],
-                        cert_file_contents,
-                        mode=0o644)
+    if not certs:
+        return
+    # First ensure they are strings...
+    cert_file_contents = "\n".join([str(c) for c in certs])
+    util.write_file(distro_cfg['ca_cert_full_path'],
+                    cert_file_contents,
+                    mode=0o644)
+    update_cert_config(distro_name)
 
-        if distro_cfg['ca_cert_config'] != '':
-            if os.stat(distro_cfg['ca_cert_config']).st_size == 0:
-                # If the CA_CERT_CONFIG file is empty (i.e. all existing
-                # CA certs have been deleted) then simply output a single
-                # line with the cloud-init cert filename.
-                out = "%s\n" % distro_cfg['ca_cert_filename']
-            else:
-                # Append cert filename to CA_CERT_CONFIG file.
-                # We have to strip the content because blank lines in the file
-                # causes subsequent entries to be ignored. (LP: #1077020)
-                orig = util.load_file(distro_cfg['ca_cert_config'])
-                cr_cont = '\n'.join([line for line in orig.splitlines()
-                                    if line != distro_cfg['ca_cert_filename']])
-                out = "%s\n%s\n" % (cr_cont.rstrip(),
-                                    distro_cfg['ca_cert_filename'])
-            util.write_file(distro_cfg['ca_cert_config'], out, omode="wb")
+
+def update_cert_config(distro_name):
+    """
+    Update Certificate config file to add the file path managed cloud-init
+
+    @param distro: String providing the distro class name.
+    """
+    distro_cfg = _distro_ca_certs_configs(distro_name)
+    if distro_cfg['ca_cert_config'] == '':
+        return
+    if os.stat(distro_cfg['ca_cert_config']).st_size == 0:
+        # If the CA_CERT_CONFIG file is empty (i.e. all existing
+        # CA certs have been deleted) then simply output a single
+        # line with the cloud-init cert filename.
+        out = "%s\n" % distro_cfg['ca_cert_filename']
+    else:
+        # Append cert filename to CA_CERT_CONFIG file.
+        # We have to strip the content because blank lines in the file
+        # causes subsequent entries to be ignored. (LP: #1077020)
+        orig = util.load_file(distro_cfg['ca_cert_config'])
+        cr_cont = '\n'.join([line for line in orig.splitlines()
+                            if line != distro_cfg['ca_cert_filename']])
+        out = "%s\n%s\n" % (cr_cont.rstrip(),
+                            distro_cfg['ca_cert_filename'])
+    util.write_file(distro_cfg['ca_cert_config'], out, omode="wb")
 
 
 def remove_default_ca_certs(distro_name):
@@ -147,7 +146,7 @@ def remove_default_ca_certs(distro_name):
     util.delete_dir_contents(distro_cfg['ca_cert_system_path'])
     util.write_file(distro_cfg['ca_cert_config'], "", mode=0o644)
 
-    if distro_name == 'debian' or distro_name == 'ubuntu':
+    if distro_name in ['debian', 'ubuntu']:
         debconf_sel = (
             "ca-certificates ca-certificates/trust_new_crts " + "select no")
         subp.subp(('debconf-set-selections', '-'), debconf_sel)
