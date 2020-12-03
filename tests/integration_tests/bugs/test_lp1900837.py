ed9bd19ca88e4c6458c95d26151c734112615e9a
Daniel Watkins <oddbloke@ubuntu.com>
2020-12-03 12:46:24 -0500

    integration_tests: introduce IntegrationInstance.restart (#708)
    This wraps pycloudlib's `BaseInstance.restart` and `BaseInstance.wait`
    to pass the same parameters as on launch, to avoid cloud-init failures
    on the _reboot_ raising an exception.

--- tests/integration_tests/bugs/test_lp1900837.py
+++ tests/integration_tests/bugs/test_lp1900837.py
@@ -22,7 +22,7 @@ class TestLogPermissionsNotResetOnReboot:
         assert "600" == _get_log_perms(client)
 
         # Reboot
-        client.instance.restart()
+        client.restart()
 
         # Check that permissions are not reset on reboot
         assert "600" == _get_log_perms(client)
