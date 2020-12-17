c137f003f06dd80eecae4cb801d50ded5b300376
cawamata <1749824+cawamata@users.noreply.github.com>
2020-12-17 13:03:02 +0900

    Update cc_ca_certs.py

--- cloudinit/config/cc_ca_certs.py
+++ cloudinit/config/cc_ca_certs.py
@@ -77,25 +77,23 @@ def _distro_ca_certs_configs(distro_name):
     return cfg
 
 
-def update_ca_certs(distro_name):
+def update_ca_certs(distro_cfg):
     """
     Updates the CA certificate cache on the current machine.
 
-    @param distro_name: String providing the distro class name.
+    @param distro_cfg: A hash providing _distro_ca_certs_configs function.
     """
-    distro_cfg = _distro_ca_certs_configs(distro_name)
     subp.subp(distro_cfg['ca_cert_update_cmd'], capture=False)
 
 
-def add_ca_certs(distro_name, certs):
+def add_ca_certs(distro_cfg, certs):
     """
     Adds certificates to the system. To actually apply the new certificates
     you must also call L{update_ca_certs}.
 
-    @param distro_name: String providing the distro class name.
+    @param distro_cfg: A hash providing _distro_ca_certs_configs function.
     @param certs: A list of certificate strings.
     """
-    distro_cfg = _distro_ca_certs_configs(distro_name)
     if not certs:
         return
     # First ensure they are strings...
@@ -103,16 +101,15 @@ def add_ca_certs(distro_name, certs):
     util.write_file(distro_cfg['ca_cert_full_path'],
                     cert_file_contents,
                     mode=0o644)
-    update_cert_config(distro_name)
+    update_cert_config(distro_cfg)
 
 
-def update_cert_config(distro_name):
+def update_cert_config(distro_cfg):
     """
     Update Certificate config file to add the file path managed cloud-init
 
-    @param distro_name: String providing the distro class name.
+    @param distro_cfg: A hash providing _distro_ca_certs_configs function.
     """
-    distro_cfg = _distro_ca_certs_configs(distro_name)
     if distro_cfg['ca_cert_config'] is None:
         return
     if os.stat(distro_cfg['ca_cert_config']).st_size == 0:
@@ -132,12 +129,14 @@ def update_cert_config(distro_name):
     util.write_file(distro_cfg['ca_cert_config'], out, omode="wb")
 
 
-def remove_default_ca_certs(distro_name):
+def remove_default_ca_certs(distro_name, distro_cfg):
     """
     Removes all default trusted CA certificates from the system. To actually
     apply the change you must also call L{update_ca_certs}.
+
+    @param distro_name: String providing the distro class name.
+    @param distro_cfg: A hash providing _distro_ca_certs_configs function.
     """
-    distro_cfg = _distro_ca_certs_configs(distro_name)
     util.delete_dir_contents(distro_cfg['ca_cert_path'])
     util.delete_dir_contents(distro_cfg['ca_cert_system_path'])
     util.write_file(distro_cfg['ca_cert_config'], "", mode=0o644)
@@ -165,22 +164,23 @@ def handle(name, cfg, cloud, log, _args):
         return
 
     ca_cert_cfg = cfg['ca-certs']
+    distro_cfg = _distro_ca_certs_configs(cloud.distro.name)
 
     # If there is a remove-defaults option set to true, remove the system
     # default trusted CA certs first.
     if ca_cert_cfg.get("remove-defaults", False):
         log.debug("Removing default certificates")
-        remove_default_ca_certs(cloud.distro.name)
+        remove_default_ca_certs(cloud.distro.name, distro_cfg)
 
     # If we are given any new trusted CA certs to add, add them.
     if "trusted" in ca_cert_cfg:
         trusted_certs = util.get_cfg_option_list(ca_cert_cfg, "trusted")
         if trusted_certs:
             log.debug("Adding %d certificates" % len(trusted_certs))
-            add_ca_certs(cloud.distro.name, trusted_certs)
+            add_ca_certs(distro_cfg, trusted_certs)
 
     # Update the system with the new cert configuration.
     log.debug("Updating certificates")
-    update_ca_certs(cloud.distro.name)
+    update_ca_certs(distro_cfg)
 
 # vi: ts=4 expandtab
