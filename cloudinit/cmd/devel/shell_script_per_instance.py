bd889659d8a44aa753160ba4aa56491bb9d6b3f3
beantaxi <beantaxi@gmail.com>
2021-01-01 07:25:52 -0600

    cleaned up log statements, in part to fix travis

--- cloudinit/cmd/devel/shell_script_per_instance.py
+++ cloudinit/cmd/devel/shell_script_per_instance.py
@@ -11,16 +11,16 @@ LOG = log.getLogger(__name__)
 
 def list_types():
 # return a list of mime-types that are handled by this module
-    LOG.debug("### in shell_script-per-instance.list_types() ...")
+    LOG.debug("in shell_script-per-instance.list_types() ...")
     return(["text/x-shellscript-per-instance"])
 
 
 def handle_part(data, ctype, script_path, payload):
     if script_path is not None:
-        LOG.debug("### in shell_script-per-instance.handle_part() ...")
-        LOG.debug(f"### x-shellscript-per-instance.handle_part: script_path={script_path}")
+        LOG.debug("in shell_script-per-instance.handle_part() ...")
+        LOG.debug(f"x-shellscript-per-instance.handle_part: {script_path=}")
         (folder, filename) = os.path.split(script_path)
-        LOG.debug(f"### folder={folder} filename={filename}")
+        LOG.debug(f"{folder=} {filename=}")
         path = f"/var/lib/cloud/scripts/per-instance/{filename}"
-        LOG.debug(f"### path={path}")
+        LOG.debug(f"{path=}")
         util.write_file(path, payload, 0o700)
