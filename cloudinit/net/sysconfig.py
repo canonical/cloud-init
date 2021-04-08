868d8228a2af886f205bf35f2f0e7134856c28c4
Jens Sandmann <sandmann@b1-systems.de>
2021-04-08 11:21:34 +0200

    Apply suggestions from code review
    simplify the if statement as suggested by @TheRealFalcon
    
    Co-authored-by: James Falcon <TheRealFalcon@users.noreply.github.com>

--- cloudinit/net/sysconfig.py
+++ cloudinit/net/sysconfig.py
@@ -635,18 +635,17 @@ class Renderer(renderer.Renderer):
                         bond_value = " ".join(bond_value)
                     bond_opts.append(value_tpl % (bond_value))
                     break
-        # suse uses the sysconfig support which requires BONDING_MODULE_OPTS
-        # see https://www.kernel.org/doc/Documentation/networking/bonding.txt
-        # 3.1 Configuration with Sysconfig Support
-        if flavor == 'suse':
-            if bond_opts:
+        if bond_opts:
+            if flavor == 'suse':
+                # suse uses the sysconfig support which requires BONDING_MODULE_OPTS
+                # see https://www.kernel.org/doc/Documentation/networking/bonding.txt
+                # 3.1 Configuration with Sysconfig Support            
                 iface_cfg['BONDING_MODULE_OPTS'] = " ".join(bond_opts)
-        # rhel uses initscript support and thus requires BONDING_OPTS
-        # this is also the old default
-        # see https://www.kernel.org/doc/Documentation/networking/bonding.txt
-        #  3.2 Configuration with Initscripts Support
-        else:
-            if bond_opts:
+            else:
+                # rhel uses initscript support and thus requires BONDING_OPTS
+                # this is also the old default
+                # see https://www.kernel.org/doc/Documentation/networking/bonding.txt
+                #  3.2 Configuration with Initscripts Support
                 iface_cfg['BONDING_OPTS'] = " ".join(bond_opts)
 
     @classmethod
