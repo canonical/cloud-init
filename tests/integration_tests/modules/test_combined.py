6ea0477f29659e70f4023dae0b2e9c2d622eae07
James Falcon <therealfalcon@gmail.com>
2021-07-23 11:53:49 -0500

    Added tests for apt defaults, byobu, locale, output, ntp pools, and ntp chrony

--- tests/integration_tests/modules/test_combined.py
+++ tests/integration_tests/modules/test_combined.py
@@ -9,55 +9,81 @@ import re
 from datetime import date
 
 from tests.integration_tests.instances import IntegrationInstance
+from tests.integration_tests.util import verify_ordered_items_in_text
 
 USER_DATA = """\
 #cloud-config
+apt:
+  primary:
+    - arches: [default]
+      uri: http://us.archive.ubuntu.com/ubuntu/
+byobu_by_default: enable
 final_message: |
   This is my final message!
   $version
   $timestamp
   $datasource
   $uptime
+locale: en_GB.UTF-8
+locale_configfile: /etc/default/locale
 ntp:
   servers: ['ntp.ubuntu.com']
-apt:
-  primary:
-    - arches: [default]
-    uri: http://us.archive.ubuntu.com/ubuntu/
 """
 
 
-@pytest.mark.userdata(USER_DATA)
-def test_final_message(module_client: IntegrationInstance):
-    """ Test that final_message module works as expected.
+@pytest.mark.user_data(USER_DATA)
+class TestCombined:
+    def test_final_message(self, class_client: IntegrationInstance):
+        """Test that final_message module works as expected.
 
-    Also tests LP 1511485: final_message is silent
-    """
-    client = module_client
+        Also tests LP 1511485: final_message is silent
+        """
+        client = class_client
+        log = client.read_from_file('/var/log/cloud-init.log')
+        today = date.today().strftime('%a, %d %b %Y')
+        expected = (
+            'This is my final message!\n'
+            r'\d+\.\d+\n'
+            '{}.*\n'
+            'DataSource.*\n'
+            r'\d+\.\d+'
+        ).format(today)
 
-    log = client.read_from_file('/var/log/cloud-init.log')
-    today = date.today().strftime('%a, %d %b %Y')
-    expected = (
-        'This is my final message!\n'
-        r'\d\d\.\d\n'
-        '{}.*\n'
-        'DataSource.*\n'
-        r'\d+\.\d+'
-    ).format(today)
+        assert re.search(expected, log)
 
-    assert re.search(expected, log)
+    def test_ntp_with_apt(self, class_client: IntegrationInstance):
+        """LP #1628337.
 
+        cloud-init tries to install NTP before even
+        configuring the archives.
+        """
+        client = class_client
+        log = client.read_from_file('/var/log/cloud-init.log')
+        assert 'W: Failed to fetch' not in log
+        assert 'W: Some index files failed to download' not in log
+        assert 'E: Unable to locate package ntp' not in log
 
-@pytest.mark.userdata(USER_DATA)
-def test_ntp_with_apt(module_client: IntegrationInstance):
-    """ LP #1628337.
+    def test_byobu(self, class_client: IntegrationInstance):
+        """Test byobu configured as enabled by default."""
+        client = class_client
+        assert client.execute('test -e "/etc/byobu/autolaunch"').ok
 
-    cloud-init tries to install NTP before even
-    configuring the archives.
-    """
-    client = module_client
+    def test_configured_locale(self, class_client: IntegrationInstance):
+        """Test locale can be configured correctly."""
+        client = class_client
+        default_locale = client.read_from_file('/etc/default/locale')
+        assert 'LANG=en_GB.UTF-8' in default_locale
 
-    log = client.read_from_file('/var/log/cloud-init.log')
-    assert 'W: Failed to fetch' not in log
-    assert 'W: Some index files failed to download' not in log
-    assert 'E: Unable to locate package ntp' not in log
+        locale_a = client.execute('locale -a')
+        verify_ordered_items_in_text([
+            'en_GB.utf8',
+            'en_US.utf8'
+        ], locale_a)
+
+        locale_gen = client.execute(
+            "cat /etc/locale.gen | grep -v '^#' | uniq"
+        )
+        verify_ordered_items_in_text([
+            'en_GB.UTF-8',
+            'en_US.UTF-8'
+        ], locale_gen)
