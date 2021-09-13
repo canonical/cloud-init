f4c47e3e25d1fb79e2673e37f8fc67750d025be2
zhuzaifangxuele <52022596+zhuzaifangxuele@users.noreply.github.com>
2021-09-14 02:21:28 +0800

    Support openEuler OS (#1012)
    openEuler Homepage: https://www.openeuler.org/en/

--- cloudinit/util.py
+++ cloudinit/util.py
@@ -549,8 +549,8 @@ def system_info():
         linux_dist = info['dist'][0].lower()
         if linux_dist in (
                 'almalinux', 'alpine', 'arch', 'centos', 'cloudlinux',
-                'debian', 'eurolinux', 'fedora', 'photon', 'rhel', 'rocky',
-                'suse', 'virtuozzo'):
+                'debian', 'eurolinux', 'fedora', 'openEuler', 'photon',
+                'rhel', 'rocky', 'suse', 'virtuozzo'):
             var = linux_dist
         elif linux_dist in ('ubuntu', 'linuxmint', 'mint'):
             var = 'ubuntu'
