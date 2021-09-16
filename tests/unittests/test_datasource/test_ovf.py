canonical/cloud-init

commit f0ab1e64852d50f4fe0de84e0bca0ee8bb516a9f

VMware: add network-config support in ovf-env.xml (#947)
Details:
1. Support guest set network config through guestinfo.ovfEnv using OVF
2. 'network-config' Property is optional
3. 'network-config' Property's value has to be base64 encoded

Added unittests and updated ovf-env.xml example

cloudinit/sources/DataSourceOVF.py
@@ -358,8 +358,11 @@ class DataSourceOVF(sources.DataSource):
                 if contents:
                     break
             if contents:
-                (md, ud, cfg) = read_ovf_environment(contents)
+                read_network = ('com.vmware.guestinfo' == name)
+                (md, ud, cfg) = read_ovf_environment(contents, read_network)
                 self.environment = contents
+                if 'network-config' in md and md['network-config']:
+                    self._network_config = md['network-config']
                 found.append(name)
 
         # There was no OVF transports found
@@ -507,13 +510,14 @@ def read_vmware_imc(config):
 
 # This will return a dict with some content
 #  meta-data, user-data, some config
-def read_ovf_environment(contents):
+def read_ovf_environment(contents, read_network=False):
     props = get_properties(contents)
     md = {}
     cfg = {}
     ud = None
     cfg_props = ['password']
     md_props = ['seedfrom', 'local-hostname', 'public-keys', 'instance-id']
+    network_props = ['network-config']
     for (prop, val) in props.items():
         if prop == 'hostname':
             prop = "local-hostname"
@@ -521,6 +525,12 @@ def read_ovf_environment(contents):
             md[prop] = val
         elif prop in cfg_props:
             cfg[prop] = val
+        elif prop in network_props and read_network:
+            try:
+                network_config = base64.b64decode(val.encode())
+                md[prop] = safeload_yaml_or_dict(network_config).get('network')
+            except Exception:
+                LOG.debug("Ignore network-config in wrong format")
         elif prop == "user-data":
             try:
                 ud = base64.b64decode(val.encode())

tests/unittests/test_datasource/test_ovf.py
@@ -83,6 +83,103 @@ class TestReadOvfEnv(CiTestCase):
         self.assertEqual({'password': "passw0rd"}, cfg)
         self.assertIsNone(ud)
 
+    def test_with_b64_network_config_enable_read_network(self):
+        network_config = dedent("""\
+        network:
+           version: 2
+           ethernets:
+              nics:
+                 nameservers:
+                    addresses:
+                    - 127.0.0.53
+                    search:
+                    - eng.vmware.com
+                    - vmware.com
+                 match:
+                    name: eth*
+                 gateway4: 10.10.10.253
+                 dhcp4: false
+                 addresses:
+                 - 10.10.10.1/24
+        """)
+        network_config_b64 = base64.b64encode(network_config.encode()).decode()
+        props = {"network-config": network_config_b64,
+                 "password": "passw0rd",
+                 "instance-id": "inst-001"}
+        env = fill_properties(props)
+        md, ud, cfg = dsovf.read_ovf_environment(env, True)
+        self.assertEqual("inst-001", md["instance-id"])
+        self.assertEqual({'password': "passw0rd"}, cfg)
+        self.assertEqual(
+            {'version': 2, 'ethernets':
+                {'nics':
+                    {'nameservers':
+                        {'addresses': ['127.0.0.53'],
+                         'search': ['eng.vmware.com', 'vmware.com']},
+                     'match': {'name': 'eth*'},
+                     'gateway4': '10.10.10.253',
+                     'dhcp4': False,
+                     'addresses': ['10.10.10.1/24']}}},
+            md["network-config"])
+        self.assertIsNone(ud)
+
+    def test_with_non_b64_network_config_enable_read_network(self):
+        network_config = dedent("""\
+        network:
+           version: 2
+           ethernets:
+              nics:
+                 nameservers:
+                    addresses:
+                    - 127.0.0.53
+                    search:
+                    - eng.vmware.com
+                    - vmware.com
+                 match:
+                    name: eth*
+                 gateway4: 10.10.10.253
+                 dhcp4: false
+                 addresses:
+                 - 10.10.10.1/24
+        """)
+        props = {"network-config": network_config,
+                 "password": "passw0rd",
+                 "instance-id": "inst-001"}
+        env = fill_properties(props)
+        md, ud, cfg = dsovf.read_ovf_environment(env, True)
+        self.assertEqual({"instance-id": "inst-001"}, md)
+        self.assertEqual({'password': "passw0rd"}, cfg)
+        self.assertIsNone(ud)
+
+    def test_with_b64_network_config_disable_read_network(self):
+        network_config = dedent("""\
+        network:
+           version: 2
+           ethernets:
+              nics:
+                 nameservers:
+                    addresses:
+                    - 127.0.0.53
+                    search:
+                    - eng.vmware.com
+                    - vmware.com
+                 match:
+                    name: eth*
+                 gateway4: 10.10.10.253
+                 dhcp4: false
+                 addresses:
+                 - 10.10.10.1/24
+        """)
+        network_config_b64 = base64.b64encode(network_config.encode()).decode()
+        props = {"network-config": network_config_b64,
+                 "password": "passw0rd",
+                 "instance-id": "inst-001"}
+        env = fill_properties(props)
+        md, ud, cfg = dsovf.read_ovf_environment(env)
+        self.assertEqual({"instance-id": "inst-001"}, md)
+        self.assertEqual({'password': "passw0rd"}, cfg)
+        self.assertIsNone(ud)
+
 
 class TestMarkerFiles(CiTestCase):
 

doc/sources/ovf/example/ovf-env.xml
@@ -41,6 +41,14 @@
         -->
         <Property oe:key="user-data" oe:value="IyEvYmluL3NoCmVjaG8gImhpIHdvcmxkIgo="/>
         <Property oe:key="password" oe:value="passw0rd"/>
+        <!--
+        network-config is optional, it can only be read from VMware guestinfo.ovfEnv
+        The value for network-config is to be base64 encoded.
+        It will be decoded, and then processed normally as network-config.
+        Set ovf-env.xml to VMware guestinfo.ovfEnv by below command:
+        'vmware-rpctool "info-set guestinfo.ovfEnv `cat ./ovf-env.xml`"' 
+        -->
+        <Property oe:key="network-config" oe:value="bmV0d29yazoKICB2ZXJzaW9uOiAyCiAgZXRoZXJuZXRzOgogICAgbmljczoKICAgICAgbWF0Y2g6CiAgICAgICAgbmFtZTogZXRoKgogICAgICBkaGNwNDogeWVz"/>
     </PropertySection>
 
 </Environment>

commit 5ea2c669d6e8a9ab30f3107bee45cecc5fa1b081

VMware: Fix typo introduced in #947 and add test (#1019)

cloudinit/sources/DataSourceOVF.py
@@ -358,7 +358,7 @@ class DataSourceOVF(sources.DataSource):
                 if contents:
                     break
             if contents:
-                read_network = ('com.vmware.guestinfo' == name)
+                read_network = ('com.vmware.guestInfo' == name)
                 (md, ud, cfg) = read_ovf_environment(contents, read_network)
                 self.environment = contents
                 if 'network-config' in md and md['network-config']:

tests/unittests/test_datasource/test_ovf.py
@@ -518,6 +518,49 @@ class TestDatasourceOVF(CiTestCase):
                         'vmware (%s/seed/ovf-env.xml)' % self.tdir,
                         ds.subplatform)
 
+    def test_get_data_vmware_guestinfo_with_network_config(self):
+        network_config = dedent("""\
+        network:
+           version: 2
+           ethernets:
+              nics:
+                 nameservers:
+                    addresses:
+                    - 127.0.0.53
+                    search:
+                    - vmware.com
+                 match:
+                    name: eth*
+                 gateway4: 10.10.10.253
+                 dhcp4: false
+                 addresses:
+                 - 10.10.10.1/24
+        """)
+        network_config_b64 = base64.b64encode(network_config.encode()).decode()
+        props = {"network-config": network_config_b64,
+                 "password": "passw0rd",
+                 "instance-id": "inst-001"}
+        env = fill_properties(props)
+        paths = Paths({'cloud_dir': self.tdir, 'run_dir': self.tdir})
+        ds = self.datasource(sys_cfg={}, distro={}, paths=paths)
+        with mock.patch(MPATH + 'transport_vmware_guestinfo',
+                        return_value=env):
+            with mock.patch(MPATH + 'transport_iso9660',
+                            return_value=NOT_FOUND):
+                self.assertTrue(ds.get_data())
+                self.assertEqual('inst-001', ds.metadata['instance-id'])
+                self.assertEqual(
+                    {'version': 2, 'ethernets':
+                        {'nics':
+                            {'nameservers':
+                                {'addresses': ['127.0.0.53'],
+                                 'search': ['vmware.com']},
+                             'match': {'name': 'eth*'},
+                             'gateway4': '10.10.10.253',
+                             'dhcp4': False,
+                             'addresses': ['10.10.10.1/24']}}},
+                    ds.network_config)
+
     def test_get_data_cloudinit_metadata_json(self):
         """Test metadata can be loaded to cloud-init metadata and network.
         The metadata format is json.