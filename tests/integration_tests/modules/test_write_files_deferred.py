db1d8dae41a2780af36042c862db1a7f652147aa
Lucendio <dev@lucend.io>
2021-10-24 02:52:11 +0200

    Revert "Just a test - To be reverted"
    This reverts commit a401f726ea1d9b8f390867ec801981d292ce091f.

--- tests/integration_tests/modules/test_write_files_deferred.py
+++ tests/integration_tests/modules/test_write_files_deferred.py
@@ -25,7 +25,7 @@ write_files:
     content: |
       export PATH="/var/lib/test/bin:$PATH"
     append: true
-    defer: false
+    defer: true
     owner: '{user}'
 """.format(user=TEST_USER_NAME, file=TEST_USER_FILE_PATH)
 
