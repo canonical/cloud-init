871280a5787e522421d861bcbea95ddd1f6293d7
Chad Smith <chad.smith@canonical.com>
2021-11-29 14:16:16 -0700

    tests: integration updates for aliased keys

--- tests/integration_tests/modules/test_jinja_templating.py
+++ tests/integration_tests/modules/test_jinja_templating.py
@@ -11,6 +11,7 @@ USER_DATA = """\
 runcmd:
   - echo {{v1.local_hostname}} > /var/tmp/runcmd_output
   - echo {{merged_cfg._doc}} >> /var/tmp/runcmd_output
+  - echo {{v1['local-hostname']}} >> /var/tmp/runcmd_output
 """
 
 
@@ -19,12 +20,15 @@ def test_runcmd_with_variable_substitution(client: IntegrationInstance):
     """Test jinja substitution.
 
     Ensure we can also substitute variables from instance-data-sensitive
-    LP: #1931392
+    LP: #1931392 and that underscore-delimited aliases exist for hyphenated
+    keys.
     """
+    hostname = client.execute('hostname').stdout.strip()
     expected = [
-        client.execute('hostname').stdout.strip(),
+        hostname,
         ('Merged cloud-init system config from /etc/cloud/cloud.cfg and '
-            '/etc/cloud/cloud.cfg.d/')
+            '/etc/cloud/cloud.cfg.d/'),
+        hostname
     ]
     output = client.read_from_file('/var/tmp/runcmd_output')
     verify_ordered_items_in_text(expected, output)
