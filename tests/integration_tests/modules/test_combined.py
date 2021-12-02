9d337b4c9d80efd1799543a80a3d2522775d7e62
James Falcon <james.falcon@canonical.com>
2021-12-01 22:31:53 -0600

    testing: Remove date from final_message test

--- tests/integration_tests/modules/test_combined.py
+++ tests/integration_tests/modules/test_combined.py
@@ -74,23 +74,16 @@ class TestCombined:
         """Test that final_message module works as expected.
 
         Also tests LP 1511485: final_message is silent.
-
-        It's possible that if this test is run within a minute or so of
-        midnight that we'll see a failure because the day in the logs
-        is different from the day specified in the test definition.
         """
         client = class_client
         log = client.read_from_file('/var/log/cloud-init.log')
-        # Get date on host rather than locally as our host could be in a
-        # wildly different timezone (or more likely recording UTC)
-        today = client.execute('date "+%a, %d %b %Y"')
         expected = (
             'This is my final message!\n'
             r'\d+\.\d+.*\n'
-            '{}.*\n'
+            '.*\n'  # Checking date is too error prone due to timezone
             'DataSource.*\n'
             r'\d+\.\d+'
-        ).format(today)
+        )
 
         assert re.search(expected, log)
 
