77f0d68f783665fffcc41f22bef0ab51aa98f74c
Brett Holman <bholman.devel@gmail.com>
2021-12-02 08:45:36 -0700

    s/DistroTester/MockDistro/g

--- tests/unittests/distros/test_manage_service.py
+++ tests/unittests/distros/test_manage_service.py
@@ -1,7 +1,7 @@
 # This file is part of cloud-init. See LICENSE file for license information.
 
 from tests.unittests.helpers import (CiTestCase, mock)
-from tests.unittests.util import DistroTester
+from tests.unittests.util import MockDistro
 
 
 class TestManageService(CiTestCase):
@@ -10,9 +10,9 @@ class TestManageService(CiTestCase):
 
     def setUp(self):
         super(TestManageService, self).setUp()
-        self.dist = DistroTester()
+        self.dist = MockDistro()
 
-    @mock.patch.object(DistroTester, 'uses_systemd', return_value=False)
+    @mock.patch.object(MockDistro, 'uses_systemd', return_value=False)
     @mock.patch("cloudinit.distros.subp.subp")
     def test_manage_service_systemctl_initcmd(self, m_subp, m_sysd):
         self.dist.init_cmd = ['systemctl']
@@ -20,14 +20,14 @@ class TestManageService(CiTestCase):
         m_subp.assert_called_with(['systemctl', 'start', 'myssh'],
                                   capture=True)
 
-    @mock.patch.object(DistroTester, 'uses_systemd', return_value=False)
+    @mock.patch.object(MockDistro, 'uses_systemd', return_value=False)
     @mock.patch("cloudinit.distros.subp.subp")
     def test_manage_service_service_initcmd(self, m_subp, m_sysd):
         self.dist.init_cmd = ['service']
         self.dist.manage_service('start', 'myssh')
         m_subp.assert_called_with(['service', 'myssh', 'start'], capture=True)
 
-    @mock.patch.object(DistroTester, 'uses_systemd', return_value=True)
+    @mock.patch.object(MockDistro, 'uses_systemd', return_value=True)
     @mock.patch("cloudinit.distros.subp.subp")
     def test_manage_service_systemctl(self, m_subp, m_sysd):
         self.dist.init_cmd = ['ignore']
