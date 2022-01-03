776a51d4414145fc0d1b06b7760ff665ccd3a449
Chris Lalos <chris.lalos@gmail.com>
2022-01-03 09:26:07 -0600

    removed Dockerfiles from this branch - they will be added to another feature branch specific to their PR

--- cloudinit/cmd/devel/cloudinit-tester-build.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env sh
-
-# Use this script to build the docker image.
-# Not too fancy, but it does ensure the image gets a predictable name,
-# for use by other scripts.
-
-docker build --tag cloudinit-tester .
--- cloudinit/cmd/devel/cloudinit-tester-run.sh
+++
@@ -1,8 +0,0 @@
-# !/usr/bin/env sh
-
-# Use this script to run various commands inside a cloudinit-tester container.
-# The cloudinit-tester container has an ENTRYPOINT of 'python -m', so use the
-# script like this:
-#
-#    ./cloud-init-tester.sh pylint cloudinit  # This runs python -m pylint cloudinit
-docker run --rm --name cloudinit-tester --volume $PWD:/src cloudinit-tester "$@" 
