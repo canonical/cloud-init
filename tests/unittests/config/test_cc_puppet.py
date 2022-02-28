51d17846400eec1ae1806be4b5f107524fdde019
Robert Schweikert <rjschwei@suse.com>
2022-02-28 09:49:47 -0500

    Fix formatting issues

--- tests/unittests/config/test_cc_puppet.py
+++ tests/unittests/config/test_cc_puppet.py
@@ -9,12 +9,14 @@ from tests.unittests.util import get_cloud
 
 LOG = logging.getLogger(__name__)
 
+
 @mock.patch("cloudinit.config.cc_puppet.subp.which")
 @mock.patch("cloudinit.config.cc_puppet.subp.subp")
 @mock.patch("cloudinit.config.cc_puppet.os")
 class TestAutostartPuppet(CiTestCase):
     def test_wb_autostart_puppet_updates_puppet_default(
-            self, m_os, m_subp, m_subpw):
+        self, m_os, m_subp, m_subpw
+    ):
         """Update /etc/default/puppet to autostart if it exists."""
 
         def _fake_exists(path):
@@ -39,21 +41,21 @@ class TestAutostartPuppet(CiTestCase):
         )
 
     def test_wb_autostart_pupppet_enables_puppet_systemctl(
-            self, m_os, m_subp, m_subpw):
+        self, m_os, m_subp, m_subpw
+    ):
         """If systemctl is present, enable puppet via systemctl."""
 
         m_os.path.exists.return_value = False
-        m_subpw.return_value = '/usr/bin/systemctl'
+        m_subpw.return_value = "/usr/bin/systemctl"
         cc_puppet._autostart_puppet(LOG)
         expected_calls = [
-            mock.call(
-                ["systemctl", "enable", "puppet.service"], capture=False
-            )
+            mock.call(["systemctl", "enable", "puppet.service"], capture=False)
         ]
         self.assertEqual(expected_calls, m_subp.call_args_list)
 
     def test_wb_autostart_pupppet_enables_puppet_chkconfig(
-            self, m_os, m_subp, m_subpw):
+        self, m_os, m_subp, m_subpw
+    ):
         """If chkconfig is present, enable puppet via checkcfg."""
 
         def _fake_exists(path):
