bd486c94f51b35320e8b9c9dc012aab0598bb73a
James Falcon <james.falcon@canonical.com>
2022-04-05 16:03:32 -0500

    comments

--- tests/unittests/config/test_cc_scripts_vendor.py
+++ tests/unittests/config/test_cc_scripts_vendor.py
@@ -14,6 +14,7 @@ class TestScriptsVendorSchema:
         (
             ({"vendor_data": {"enabled": True}}, None),
             ({"vendor_data": {"enabled": "yes"}}, None),
+            ({"vendor_data": {"prefix": []}}, r"\[\] is too short"),
         ),
     )
     @skipUnlessJsonSchema()
