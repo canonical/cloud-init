2665793ce7724f86caecf4587e9a92d3d9d5abfe
Chad Smith <chad.smith@canonical.com>
2022-05-14 22:21:29 -0600

    tests: adapt relative $ref links for jsonschema 2.6.0

--- tests/unittests/config/test_schema.py
+++ tests/unittests/config/test_schema.py
@@ -7,14 +7,14 @@ import itertools
 import json
 import logging
 import os
-import re
 import sys
-from copy import copy
+from copy import copy, deepcopy
 from pathlib import Path
 from textwrap import dedent
 from types import ModuleType
 from typing import List
 
+import jsonschema
 import pytest
 
 from cloudinit.config.schema import (
@@ -96,6 +96,16 @@ def get_module_variable(var_name) -> dict:
 
 
 class TestVersionedSchemas:
+    def _relative_ref_to_local_file_path(self, source_schema):
+        """Replace known relative ref URLs with full file path."""
+        # jsonschema 2.6.0 doesn't support relative URLs in $refs (bionic)
+        full_path_schema = deepcopy(source_schema)
+        relative_ref = full_path_schema["oneOf"][0]["allOf"][1]["$ref"]
+        full_local_filepath = get_schema_dir() + relative_ref[1:]
+        file_ref = f"file://{full_local_filepath}"
+        full_path_schema["oneOf"][0]["allOf"][1]["$ref"] = file_ref
+        return full_path_schema
+
     @pytest.mark.parametrize(
         "schema,error_msg",
         (
@@ -118,14 +128,30 @@ class TestVersionedSchemas:
         version_schema["$id"] = f"file://{version_schemafile}"
         if error_msg:
             with pytest.raises(SchemaValidationError) as context_mgr:
+                try:
+                    validate_cloudconfig_schema(
+                        schema, schema=version_schema, strict=True
+                    )
+                except jsonschema.exceptions.RefResolutionError:
+                    full_path_schema = self._relative_ref_to_local_file_path(
+                        version_schema
+                    )
+                    validate_cloudconfig_schema(
+                        schema, schema=full_path_schema, strict=True
+                    )
+            assert error_msg in str(context_mgr.value)
+        else:
+            try:
                 validate_cloudconfig_schema(
                     schema, schema=version_schema, strict=True
                 )
-            assert error_msg in str(context_mgr.value)
-        else:
-            validate_cloudconfig_schema(
-                schema, schema=version_schema, strict=True
-            )
+            except jsonschema.exceptions.RefResolutionError:
+                full_path_schema = self._relative_ref_to_local_file_path(
+                    version_schema
+                )
+                validate_cloudconfig_schema(
+                    schema, schema=full_path_schema, strict=True
+                )
 
 
 class TestGetSchema:
