4b55bcac7cf2efb55a50002f46b39b788d421ea1
Alberto Contreras <alberto.contreras@canonical.com>
2022-05-16 15:46:48 +0200

    Revert __mock_assert_not_called

--- tests/unittests/helpers.py
+++ tests/unittests/helpers.py
@@ -519,6 +519,21 @@ def skipIfJinja():
     return skipIf(JINJA_AVAILABLE, "Jinja dependency present.")
 
 
+# older versions of mock do not have the useful 'assert_not_called'
+if not hasattr(mock.Mock, "assert_not_called"):
+
+    def __mock_assert_not_called(mmock):
+        if mmock.call_count != 0:
+            msg = (
+                "[citest] Expected '%s' to not have been called. "
+                "Called %s times."
+                % (mmock._mock_name or "mock", mmock.call_count)
+            )
+            raise AssertionError(msg)
+
+    mock.Mock.assert_not_called = __mock_assert_not_called  # type: ignore
+
+
 def get_top_level_dir() -> Path:
     """Return the absolute path to the top cloudinit project directory
 
