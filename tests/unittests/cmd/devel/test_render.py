d7fae2b87c49d5309bd7a933a5b6418cb57f9c05
Alberto Contreras <alberto.contreras@canonical.com>
2022-05-18 16:41:49 +0200

    Fix test due to caplog incompatibility (#1461)
    caplog.text is giving different results on focal. Use caplog.records
    instead on failing test.

--- tests/unittests/cmd/devel/test_render.py
+++ tests/unittests/cmd/devel/test_render.py
@@ -112,10 +112,11 @@ class TestRender:
         with mock.patch("sys.stderr", new_callable=StringIO):
             with mock.patch("sys.stdout", new_callable=StringIO) as m_stdout:
                 assert render.handle_args("anyname", args) == 0
-        assert "Converted jinja variables\n{" in caplog.text
-        # TODO enable after pytest>=3.4
-        # more info: https://docs.pytest.org/en/stable/how-to/logging.html
-        # assert "Converted jinja variables\n{" in m_stderr.getvalue()
+        # Make sure the log is correctly captured. There is an issue
+        # with this fixture in pytest==4.6.9 (focal):
+        assert (
+            "Converted jinja variables\n{" in caplog.records[-1].getMessage()
+        )
         assert "rendering: jinja worked" == m_stdout.getvalue()
 
     @skipUnlessJinja()
