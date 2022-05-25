9681f3c8b6639b67fddb0aff8a45cc6b2c0c68e2
James Falcon <james.falcon@canonical.com>
2022-05-25 10:29:07 -0500

    testing: fix test_status.py
    We removed the '--long' flag but forgot to remove the additional
    verification

--- tests/integration_tests/cmd/test_status.py
+++ tests/integration_tests/cmd/test_status.py
@@ -65,5 +65,4 @@ def test_wait_when_no_datasource(session_cloud: IntegrationCloud, setup_image):
             _remove_nocloud_dir_and_reboot(client)
         status_out = _wait_for_cloud_init(client).stdout.strip()
         assert "status: disabled" in status_out
-        assert "Cloud-init disabled by cloud-init-generator" in status_out
         assert client.execute("cloud-init status --wait").ok
