09389b7ac3edd29c5f2cbbcc2e39f8dac4f1df7a
Alberto Contreras <alberto.contreras@canonical.com>
2022-06-06 18:17:05 +0200

    Remove impossible test

--- tests/integration_tests/datasources/test_oci_networking.py
+++ tests/integration_tests/datasources/test_oci_networking.py
@@ -18,16 +18,12 @@ datasource:
 """
 
 
-def _customize_environment(
+def customize_environment(
     client: IntegrationInstance,
     tmpdir,
     configure_secondary_nics: bool = False,
-    iscsi: bool = True,
 ):
     assert client.execute("rm -f /run/initramfs/open-iscsi.interface").ok
-    if not iscsi:
-        assert client.execute("rm -f /run/net-*.conf").ok
-
     # Force network config
     assert client.execute(
         "rm -f /etc/cloud/cloud.cfg.d/99-disable-network-config.cfg"
@@ -58,9 +54,7 @@ def extract_interface_names(network_config: dict) -> Set[str]:
 
 @pytest.mark.oci
 def test_oci_networking_iscsi_instance(client: IntegrationInstance, tmpdir):
-    _customize_environment(
-        client, tmpdir, configure_secondary_nics=False, iscsi=True
-    )
+    customize_environment(client, tmpdir, configure_secondary_nics=False)
     result_net_files = client.execute("ls /run/net-*.conf")
     assert result_net_files.ok, "No net files found under /run"
 
@@ -74,22 +68,26 @@ def test_oci_networking_iscsi_instance(client: IntegrationInstance, tmpdir):
     netplan_yaml = client.read_from_file("/etc/netplan/50-cloud-init.yaml")
     netplan_cfg = yaml.safe_load(netplan_yaml)
     configured_interfaces = extract_interface_names(netplan_cfg["network"])
+    assert 1 <= len(
+        configured_interfaces
+    ), "Expected at least 1 primary network configuration."
 
-    expeceted_interfaces = set(
+    expected_interfaces = set(
         re.findall(r"/run/net-(.+)\.conf", result_net_files.stdout)
     )
-    for expected_interface in expeceted_interfaces:
+    for expected_interface in expected_interfaces:
         assert (
             f"Reading from /run/net-{expected_interface}.conf" in log
         ), "Expected {expected_interface} not found in: {log}"
 
-    not_found_interfaces = expeceted_interfaces.difference(
+    not_found_interfaces = expected_interfaces.difference(
         configured_interfaces
     )
     assert not not_found_interfaces, (
         f"Interfaces, {not_found_interfaces}, expected to be configured in"
         f" {netplan_cfg['network']}"
     )
+    assert client.execute("ping -c 2 canonical.com").ok
 
 
 @pytest.fixture(scope="function")
@@ -139,9 +137,7 @@ def test_oci_networking_iscsi_instance_secondary_vnics(
     client_with_secondary_vnic: IntegrationInstance, tmpdir
 ):
     client = client_with_secondary_vnic
-    _customize_environment(
-        client, tmpdir, configure_secondary_nics=True, iscsi=True
-    )
+    customize_environment(client, tmpdir, configure_secondary_nics=True)
 
     log = client.read_from_file("/var/log/cloud-init.log")
     verify_clean_log(log)
@@ -150,26 +146,13 @@ def test_oci_networking_iscsi_instance_secondary_vnics(
     netplan_yaml = client.read_from_file("/etc/netplan/50-cloud-init.yaml")
     netplan_cfg = yaml.safe_load(netplan_yaml)
     configured_interfaces = extract_interface_names(netplan_cfg["network"])
-    assert 2 == len(configured_interfaces)
+    assert 2 <= len(
+        configured_interfaces
+    ), "Expected at least 1 primary and 1 secondary network configurations"
 
-
-@pytest.mark.skip(
-    reason="Figure out how to configure a non iscsi network instance in oci"
-)
-@pytest.mark.oci
-def test_oci_networking_non_iscsi_instance(
-    session_cloud: IntegrationCloud, tmpdir
-):
-    launch_options = oci.core.models.LaunchOptions(
-        boot_volume_type=oci.core.models.LaunchOptions.BOOT_VOLUME_TYPE_VFIO,
-        network_type=oci.core.models.LaunchOptions.NETWORK_TYPE_VFIO,
+    result_net_files = client.execute("ls /run/net-*.conf")
+    expected_interfaces = set(
+        re.findall(r"/run/net-(.+)\.conf", result_net_files.stdout)
     )
-    with session_cloud.launch(
-        launch_kwargs={"launch_options": launch_options}
-    ) as client:
-        _customize_environment(
-            client, tmpdir, configure_secondary_nics=False, iscsi=False
-        )
-        log = client.read_from_file("/var/log/cloud-init.log")
-        verify_clean_log(log)
-        assert "opc/v2/vnics/" in log, f"vnics data not fetched in {log}"
+    assert len(expected_interfaces) + 1 == len(configured_interfaces)
+    assert client.execute("ping -c 2 canonical.com").ok
