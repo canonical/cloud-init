f7685beea707d9fc15e1d2ab59eec08d42068051
James Falcon <james.falcon@canonical.com>
2022-06-15 22:33:08 -0500

    Remove extra space from ec2 dual stack crawl message
    Also update integration test to accommodate possible extra text

--- tests/integration_tests/datasources/test_ec2_ipv6.py
+++ tests/integration_tests/datasources/test_ec2_ipv6.py
@@ -10,9 +10,7 @@ def _test_crawl(client, ip):
     assert client.execute("cloud-init init --local").ok
     log = client.read_from_file("/var/log/cloud-init.log")
     assert f"Using metadata source: '{ip}'" in log
-    result = re.findall(
-        r"Crawl of metadata service took (\d+.\d+) seconds", log
-    )
+    result = re.findall(r"Crawl of metadata service.* (\d+.\d+) seconds", log)
     if len(result) != 1:
         pytest.fail(f"Expected 1 metadata crawl time, got {result}")
     # 20 would still be a crazy long time for metadata service to crawl,
