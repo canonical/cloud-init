d824e4fad47d227e008a880e083b5cdf1f570659
Fabian Lichtenegger-Lukas <fabian.lichtenegger-lukas@nts.eu>
2022-07-05 13:56:54 +0200

    improved exception handling

--- cloudinit/config/cc_wireguard.py
+++ cloudinit/config/cc_wireguard.py
@@ -1,18 +1,23 @@
 # This file is part of cloud-init. See LICENSE file for license information.
+
 """Wireguard"""
-from logging import Logger
 from textwrap import dedent
 
-from cloudinit import subp
+from cloudinit import log as logging
+from cloudinit import subp, util
 from cloudinit.cloud import Cloud
 from cloudinit.config.schema import MetaSchema, get_meta_doc
 from cloudinit.settings import PER_INSTANCE
 
-MODULE_DESCRIPTION = dedent("""\
+MODULE_DESCRIPTION = dedent(
+    """\
 Module to set up Wireguard connection.
 Including installation and configuration of WG interfaces.
 In addition certain readinessprobes can be provided.
-""")
+"""
+)
+
+NL = "\n"
 
 meta: MetaSchema = {
     "id": "cc_wireguard",
@@ -26,30 +31,31 @@ meta: MetaSchema = {
             """\
     # Configure one or more WG interfaces and provide optinal readinessprobes
     wireguard:
-    interfaces:
-        - name: <interface_name_wg0>
-        config_path: <path_to_interface_configuration_file_wg0>
-        content: |
+      interfaces:
+        - name: wg0
+          config_path: /etc/wireguard/wg0.conf
+          content: |
             [Interface]
             PrivateKey = <private_key>
             Address = <address>
             [Peer]
             PublicKey = <public_key>
             Endpoint = <endpoint_ip>:<endpoint_ip_port>
-            AllowedIPs = <allowedips>
-        - name: <interface_name_wg1>
-        config_path:  <path_to_interface_configuration_file_wg1>
-        content: |
+            AllowedIPs = <allowedip1>, <allowedip2>, ...
+        - name: wg1
+          config_path: /opt/etc/wireguard/wg1.conf
+          content: |
             [Interface]
             PrivateKey = <private_key>
             Address = <address>
             [Peer]
             PublicKey = <public_key>
             Endpoint = <endpoint_ip>:<endpoint_ip_port>
-            AllowedIPs = <allowedips>
-    readinessprobe:
-        - 'service example restart'
+            AllowedIPs = <allowedip1>
+      readinessprobe:
+        - 'systemctl restart service'
         - 'curl https://webhook.endpoint/example'
+        - 'nc -zv some-service-fqdn 443'
     """
         ),
     ],
@@ -57,57 +63,82 @@ meta: MetaSchema = {
 
 __doc__ = get_meta_doc(meta)
 
+LOG = logging.getLogger(__name__)
 
-def writeconfig(wg_section: dict, log: Logger) -> bool:
+
+def writeconfig(wg_section: dict):
     for i in wg_section["interfaces"]:
-        log.debug("Configuring Wireguard interface {}".format(str(i["name"])))
+        LOG.debug("Configuring Wireguard interface %s", i["name"])
         try:
             with open(i["config_path"], "w", encoding="utf-8") as wgconfig:
                 wgconfig.write(i["content"])
-        except Exception:
-            return False
-    return True
+        except Exception as e:
+            raise RuntimeError(
+                f"Failure writing Wireguard configuration file:" f"{e}"
+            ) from e
 
 
-def enablewg(wg_section: dict, log: Logger) -> bool:
+def enablewg(wg_section: dict):
     for i in wg_section["interfaces"]:
         try:
-            log.debug(
-                "Running: systemctl enable wg-quick@{}".format(str(i["name"]))
-            )
+            LOG.debug("Running: systemctl enable wg-quick@%s", {i["name"]})
             subp.subp(
-                "systemctl enable wg-quick@{}".format(str(i["name"])),
+                f'systemctl enable wg-quick@{i["name"]}',
                 capture=True,
                 shell=True,
             )
             subp.subp(
-                "systemctl start wg-quick@{}".format(str(i["name"])),
+                f'systemctl start wg-quick@{i["name"]}',
                 capture=True,
                 shell=True,
             )
-        except Exception:
-            return False
-    return True
+        except Exception as e:
+            raise RuntimeError(
+                f"Failed enabling Wireguard interface(s):" f"{e}"
+            ) from e
 
 
-def readinessprobe(wg_section: dict, log: Logger, cloud: Cloud) -> bool:
+def readinessprobe_command_validation(wg_section: dict):
+    errors = []
+    pos = 0
+    for c in wg_section[readinessprobe]:
+        if not isinstance(c, str):
+            errors.append(
+                f"Expected a string for readinessprobe at {pos}. Found {c}"
+            )
+            pos += 1
+
+    if errors:
+        raise ValueError(
+            f"Invalid readinessProbe commands:{NL}{NL.join(errors)}"
+        )
+
+
+def readinessprobe(wg_section: dict, cloud: Cloud):
+    errors = []
     for c in wg_section["readinessprobe"]:
-        if isinstance(c, str):
-            log.debug("Running readinessprobe: '{}'".format(str(c)))
+        try:
+            LOG.debug("Running readinessprobe: '%s'", str(c))
             subp.subp(c, capture=True, shell=True)
-    return True
+        except subp.ProcessExecutionError as e:
+            errors.append(f"{c}: {e}")
+
+    if errors:
+        raise RuntimeError(
+            f"Failed running readinessprobe command:{NL}{NL.join(errors)}"
+        )
 
 
-def handle(name: str, cfg: dict, cloud: Cloud, log: Logger, args: list):
-    log.debug(f"Starting clound-init module {name}")
+def handle(name: str, cfg: dict, cloud: Cloud, args: list):
+    LOG.debug("Starting clound-init module %s", name)
     wg_section = None
 
     if "wireguard" in cfg:
-        log.debug("Found Wireguard section in config")
+        LOG.debug("Found Wireguard section in config")
         wg_section = cfg["wireguard"]
 
     if wg_section is None:
-        log.debug(
+        LOG.debug(
             "Skipping module named %s," " no 'wireguard' configuration found",
             name,
         )
@@ -117,26 +148,21 @@ def handle(name: str, cfg: dict, cloud: Cloud, log: Logger, args: list):
     cloud.distro.install_packages(("wireguard-tools",))
     subp.subp("modprobe wireguard", capture=True, shell=True)
 
-    # write wg config files
-    state = writeconfig(wg_section, log)
-    if not state:
-        log.error("Writing Wireguard configuration file failed")
-        raise RuntimeError("Writing Wireguard configuration file failed")
+    try:
+        # write wg config files
+        writeconfig(wg_section)
 
-    # enable wg interfaces
-    state = enablewg(wg_section, log)
-    if not state:
-        log.error("Enable Wireguard interface(s) failed")
-        raise RuntimeError("Enable Wireguard interface(s) failed")
+        # enable wg interfaces
+        enablewg(wg_section)
 
-    # run readinessprobe probe, if any
-    if (
-        "readinessprobe" in wg_section
-        and wg_section["readinessprobe"] is not None
-    ):
-        state = readinessprobe(wg_section, log, cloud)
-        if not state:
-            log.error("Error during readinessprobe")
-            raise RuntimeError("Error during readinessprobe")
-    else:
-        log.debug("Skipping readinessprobe - no checks defined")
+        # parse and run readinessprobe paremeters
+        if (
+            "readinessprobe" in wg_section
+            and wg_section["readinessprobe"] is not None
+        ):
+            readinessprobe_command_validation(wg_section)
+            readinessprobe(wg_section, cloud)
+        else:
+            LOG.debug("Skipping readinessprobe - no checks defined")
+    except RuntimeError as e:
+        util.logexc(LOG, e)
