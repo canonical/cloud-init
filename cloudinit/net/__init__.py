canonical/cloud-init

commit bf94945fb855c40c5188cef5fb00327c51c41fef

Fix sysconfig render when set-name is missing (#1327)
This patch addresses an issue where the absence of set-name
in a network configuration potentially results in an unintended
network configuration.

LP: #1855945

cloudinit/net/network_state.py
@@ -10,6 +10,7 @@ import logging
 
 from cloudinit import safeyaml, util
 from cloudinit.net import (
+    get_interfaces_by_mac,
     ipv4_mask_to_net_prefix,
     ipv6_mask_to_net_prefix,
     is_ipv6_addr,
@@ -665,10 +666,18 @@ class NetworkStateInterpreter(metaclass=CommandHandlerMeta):
              ]
         }
         """
+
+        # Get the interfaces by MAC address to update an interface's
+        # device name to the name of the device that matches a provided
+        # MAC address when the set-name directive is not present.
+        #
+        # Please see https://bugs.launchpad.net/cloud-init/+bug/1855945
+        # for more information.
+        ifaces_by_mac = get_interfaces_by_mac()
+
         for eth, cfg in command.items():
             phy_cmd = {
                 "type": "physical",
-                "name": cfg.get("set-name", eth),
             }
             match = cfg.get("match", {})
             mac_address = match.get("macaddress", None)
@@ -680,6 +689,24 @@ class NetworkStateInterpreter(metaclass=CommandHandlerMeta):
                     str(cfg),
                 )
             phy_cmd["mac_address"] = mac_address
+
+            # Determine the name of the interface by using one of the
+            # following in the order they are listed:
+            #   * set-name
+            #   * interface name looked up by mac
+            #   * value of "eth" key from this loop
+            name = eth
+            set_name = cfg.get("set-name", None)
+            if set_name:
+                name = set_name
+            elif mac_address and ifaces_by_mac:
+                lcase_mac_address = mac_address.lower()
+                for iface_mac, iface_name in ifaces_by_mac.items():
+                    if lcase_mac_address == iface_mac.lower():
+                        name = iface_name
+                        break
+            phy_cmd["name"] = name
+
             driver = match.get("driver", None)
             if driver:
                 phy_cmd["params"] = {"driver": driver}

tests/unittests/test_net.py
@@ -3871,6 +3871,29 @@ iface bond0 inet6 static
             ),
         },
     },
+    "v2-dev-name-via-mac-lookup": {
+        "expected_sysconfig_rhel": {
+            "ifcfg-eth0": textwrap.dedent(
+                """\
+                BOOTPROTO=none
+                DEVICE=eth0
+                HWADDR=cf:d6:af:48:e8:80
+                NM_CONTROLLED=no
+                ONBOOT=yes
+                TYPE=Ethernet
+                USERCTL=no"""
+            ),
+        },
+        "yaml": textwrap.dedent(
+            """\
+            version: 2
+            ethernets:
+              nic0:
+                match:
+                  macaddress: 'cf:d6:af:48:e8:80'
+            """
+        ),
+    },
 }
 
 
@@ -3954,6 +3977,9 @@ DEFAULT_DEV_ATTRS = {
         "device/driver": None,
         "device/device": None,
         "name_assign_type": "4",
+        "addr_assign_type": "0",
+        "uevent": "",
+        "type": "32",
     }
 }
 
@@ -4080,16 +4106,22 @@ class TestGenerateFallbackConfig(CiTestCase):
                 "device/driver": "hv_netsvc",
                 "device/device": "0x3",
                 "name_assign_type": "4",
+                "addr_assign_type": "0",
+                "uevent": "",
+                "type": "32",
             },
             "eth1": {
                 "bridge": False,
                 "carrier": False,
                 "dormant": False,
                 "operstate": "down",
-                "address": "00:11:22:33:44:55",
+                "address": "00:11:22:33:44:56",
                 "device/driver": "mlx4_core",
                 "device/device": "0x7",
                 "name_assign_type": "4",
+                "addr_assign_type": "0",
+                "uevent": "",
+                "type": "32",
             },
         }
 
@@ -4158,16 +4190,22 @@ iface eth0 inet dhcp
                 "device/driver": "hv_netsvc",
                 "device/device": "0x3",
                 "name_assign_type": "4",
+                "addr_assign_type": "0",
+                "uevent": "",
+                "type": "32",
             },
             "eth0": {
                 "bridge": False,
                 "carrier": False,
                 "dormant": False,
                 "operstate": "down",
-                "address": "00:11:22:33:44:55",
+                "address": "00:11:22:33:44:56",
                 "device/driver": "mlx4_core",
                 "device/device": "0x7",
                 "name_assign_type": "4",
+                "addr_assign_type": "0",
+                "uevent": "",
+                "type": "32",
             },
         }
 
@@ -5098,6 +5136,45 @@ USERCTL=no
             self._compare_files_to_expected(
                 expected, self._render_and_read(network_config=v2data)
             )
+
+    @mock.patch("cloudinit.net.sys_dev_path")
+    @mock.patch("cloudinit.net.read_sys_net")
+    @mock.patch("cloudinit.net.get_devicelist")
+    def test_iface_name_from_device_with_matching_mac_address(
+        self,
+        mock_get_devicelist,
+        mock_read_sys_net,
+        mock_sys_dev_path,
+    ):
+        devices = {
+            "eth0": {
+                "bridge": False,
+                "carrier": False,
+                "dormant": False,
+                "operstate": "down",
+                "address": "CF:D6:AF:48:E8:80",
+                "device/driver": "hv_netsvc",
+                "device/device": "0x3",
+                "name_assign_type": "4",
+                "addr_assign_type": "0",
+                "uevent": "",
+                "type": "32",
+            },
+        }
+
+        tmp_dir = self.tmp_dir()
+        _setup_test(
+            tmp_dir,
+            mock_get_devicelist,
+            mock_read_sys_net,
+            mock_sys_dev_path,
+            dev_attrs=devices,
+        )
+
+        entry = NETWORK_CONFIGS["v2-dev-name-via-mac-lookup"]
+        found = self._render_and_read(network_config=yaml.load(entry["yaml"]))
+        self._compare_files_to_expected(entry[self.expected_name], found)
+        self._assert_headers(found)
 
 
 @mock.patch(

commit af404781299d3223af306456f994b4d0f5f27ce4

net: fix interface matching support (#1552)
- broken in bf94945fb855c40c5188cef5fb
- factor out a reusable helper function
- stylistic change for Python's new match keyword

LP: #1979877

cloudinit/net/__init__.py
@@ -928,6 +928,13 @@ def get_interfaces_by_mac(blacklist_drivers=None) -> dict:
         )
 
 
+def find_interface_name_from_mac(mac: str) -> Optional[str]:
+    for interface_mac, interface_name in get_interfaces_by_mac().items():
+        if mac.lower() == interface_mac.lower():
+            return interface_name
+    return None
+
+
 def get_interfaces_by_mac_on_freebsd(blacklist_drivers=None) -> dict:
     (out, _) = subp.subp(["ifconfig", "-a", "ether"])
 

cloudinit/net/network_state.py
@@ -11,6 +11,7 @@ from typing import Any, Dict
 
 from cloudinit import safeyaml, util
 from cloudinit.net import (
+    find_interface_name_from_mac,
     get_interfaces_by_mac,
     ipv4_mask_to_net_prefix,
     ipv6_mask_to_net_prefix,
@@ -700,15 +701,14 @@ class NetworkStateInterpreter(metaclass=CommandHandlerMeta):
             #   * interface name looked up by mac
             #   * value of "eth" key from this loop
             name = eth
-            set_name = cfg.get("set-name", None)
+            set_name = cfg.get("set-name")
             if set_name:
                 name = set_name
             elif mac_address and ifaces_by_mac:
                 lcase_mac_address = mac_address.lower()
-                for iface_mac, iface_name in ifaces_by_mac.items():
-                    if lcase_mac_address == iface_mac.lower():
-                        name = iface_name
-                        break
+                mac = find_interface_name_from_mac(lcase_mac_address)
+                if mac:
+                    name = mac
             phy_cmd["name"] = name
 
             driver = match.get("driver", None)
@@ -780,6 +780,12 @@ class NetworkStateInterpreter(metaclass=CommandHandlerMeta):
                 if len(dns) > 0:
                     name_cmd.update({"address": dns})
                 self.handle_nameserver(name_cmd)
+
+                mac_address = dev_cfg.get("match", {}).get("macaddress")
+                real_if_name = find_interface_name_from_mac(mac_address)
+                if real_if_name:
+                    iface = real_if_name
+
                 self._handle_individual_nameserver(name_cmd, iface)
 
     def _handle_bond_bridge(self, command, cmd_type=None):