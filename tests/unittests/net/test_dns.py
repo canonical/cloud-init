82f817efa62587dd7ebf14001cc43988428b8455
Jonas Konrad <me@yawk.at>
2022-07-11 22:20:23 +0200

    tests: add test for parsing static dns for existing devices (#1557)
    This patch adds a test case for the launchpad bug below. When parsing a
    network configuration where a device on the system has the same mac
    address as given in the `match` section of the config, but with the real
    device name not matching the config name, parsing of a static nameserver
    entry fails with a `KeyError`.
    
    Validates behavior in af404781299 which fixes LP #197877.

---
+++ tests/unittests/net/test_dns.py
@@ -0,0 +1,32 @@
+# This file is part of cloud-init. See LICENSE file for license information.
+
+from unittest import mock
+
+from cloudinit import safeyaml
+from cloudinit.net import network_state
+
+
+class TestNetDns:
+    @mock.patch("cloudinit.net.network_state.get_interfaces_by_mac")
+    @mock.patch("cloudinit.net.get_interfaces_by_mac")
+    def test_system_mac_address_does_not_break_dns_parsing(
+        self, by_mac_state, by_mac_init
+    ):
+        by_mac_state.return_value = {"00:11:22:33:44:55": "foobar"}
+        by_mac_init.return_value = {"00:11:22:33:44:55": "foobar"}
+        state = network_state.parse_net_config_data(
+            safeyaml.load(
+                """\
+version: 2
+ethernets:
+  eth:
+    match:
+      macaddress: '00:11:22:33:44:55'
+    addresses: [10.0.0.2/24]
+    gateway4: 10.0.0.1
+    nameservers:
+      addresses: [10.0.0.3]
+"""
+            )
+        )
+        assert "10.0.0.3" in state.dns_nameservers
