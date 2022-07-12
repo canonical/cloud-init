78cd14595b7e1ed2eed7aafb3f0f00a338d29cb2
Alberto Contreras <alberto.contreras@canonical.com>
2022-07-12 13:52:50 +0200

    Skip not implemented integration tests

--- tests/integration_tests/modules/test_ubuntu_advantage.py
+++ tests/integration_tests/modules/test_ubuntu_advantage.py
@@ -14,26 +14,26 @@ ubuntu_advantage:
 
 def did_ua_service_noop(client: IntegrationInstance) -> bool:
     """Determine if ua.service did run or not"""
-    raise NotImplementedError
+    raise NotImplementedError("TODO")
 
 
 @pytest.mark.ubuntu
 @pytest.mark.user_data(AUTO_ATTACH_DISABLED)
+@pytest.mark.skip(reason="TODO")
 def test_ubuntu_advantage_noop(client: IntegrationInstance):
     log = client.read_from_file("/var/log/cloud-init.log")
     verify_clean_log(log)
     assert did_ua_service_noop(client)
-    # TODO check that cc_ubuntu_advantage did noop
 
 
 @pytest.mark.ubuntu
+@pytest.mark.skip(reason="TODO")
 def test_ubuntu_advantage_noop_cloud_cfg(client: IntegrationInstance):
     client.write_to_file(UA_CLOUD_CONFIG, AUTO_ATTACH_DISABLED)
     client.execute("cloud-init clean --logs --reboot")
     log = client.read_from_file("/var/log/cloud-init.log")
     verify_clean_log(log)
     assert did_ua_service_noop(client)
-    # TODO check that cc_ubuntu_advantage did noop
 
 
 CUSTOM_SERVICES = """\
@@ -53,11 +53,12 @@ ubuntu_advantage:
 @pytest.mark.gce
 @pytest.mark.ubuntu
 @pytest.mark.user_data(CUSTOM_SERVICES)
+@pytest.mark.skip(reason="TODO")
 def test_ubuntu_advantage_custom_services(client: IntegrationInstance):
     log = client.read_from_file("/var/log/cloud-init.log")
     verify_clean_log(log)
     assert did_ua_service_noop(client)
-    # TODO check that cc_ubuntu_advantage did call auto-attach
+    # TODO check that cc_ubuntu_advantage did auto-attach properly
     # TODO check only fips and realtime-kernel are enabled
 
 
@@ -76,6 +77,7 @@ ubuntu_advantage:
 @pytest.mark.gce
 @pytest.mark.ubuntu
 @pytest.mark.user_data(DISALLOW_BETA)
+@pytest.mark.skip(reason="TODO")
 def test_ubuntu_advantage_disallow_beta(client: IntegrationInstance):
     assert did_ua_service_noop(client)
     # log = client.read_from_file("/var/log/cloud-init.log")
