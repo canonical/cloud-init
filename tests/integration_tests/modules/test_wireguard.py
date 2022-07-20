cd3f4bfbecb832c035c2b96f55d37ed7d73056bf
Fabian Lichtenegger-Lukas <lilufa1993@gmail.com>
2022-07-20 14:35:22 +0200

    improved integration tests

--- tests/integration_tests/modules/test_wireguard.py
+++ tests/integration_tests/modules/test_wireguard.py
@@ -7,18 +7,6 @@ import pytest
 
 from tests.integration_tests.instances import IntegrationInstance
 
-WG1_CONTENT = """\
-        [Interface]
-        PrivateKey = GGLU4+5vIcK9lGyfz4AJn9fR5/FN/6sf4Fd5chZ16Vc=
-        Address = 192.168.254.2/24
-        ListenPort = 51820
-
-        [Peer]
-        PublicKey = 2as8z3EDjSsfFEkvOQGVnJ1Hv+h1jRAh2BKJg+DHvGk=
-        Endpoint = 127.0.0.1:51820
-        AllowedIPs = 0.0.0.0/0
-"""
-
 ASCII_TEXT = "ASCII text"
 
 USER_DATA = """\
@@ -29,7 +17,7 @@ wireguard:
       config_path: /etc/wireguard/wg0.conf
       content: |
         [Interface]
-        Address = 192.168.254.1/24
+        Address = 192.168.254.1/32
         ListenPort = 51820
         PrivateKey = iNlmgtGo6yiFhD9TuVnx/qJSp+C5Cwg4wwPmOJwlZXI=
 
@@ -39,10 +27,18 @@ wireguard:
     - name: wg1
       config_path: /etc/wireguard/wg1.conf
       content: |
-{}
-""".format(
-    WG1_CONTENT
-)
+        [Interface]
+        PrivateKey = GGLU4+5vIcK9lGyfz4AJn9fR5/FN/6sf4Fd5chZ16Vc=
+        Address = 192.168.254.2/24
+
+        [Peer]
+        PublicKey = 2as8z3EDjSsfFEkvOQGVnJ1Hv+h1jRAh2BKJg+DHvGk=
+        Endpoint = 127.0.0.1:51820
+        AllowedIPs = 0.0.0.0/0
+  readinessprobe:
+    - ping -qc 5 192.168.254.1 2>&1 > /dev/null
+    - echo $? > /tmp/ping
+"""
 
 
 @pytest.mark.ci
@@ -51,6 +47,8 @@ class TestWireguard:
     @pytest.mark.parametrize(
         "cmd,expected_out",
         (
+            # check if wireguard module is loaded
+            ("lsmod | grep '^wireguard' | awk '{print $1}'", "wireguard"),
             # test if file was written for wg0
             (
                 "stat -c '%N' /etc/wireguard/wg0.conf",
@@ -63,14 +61,20 @@ class TestWireguard:
             # md5sum check wg1
             (
                 "md5sum </etc/wireguard/wg1.conf",
-                "0e621de7a38ba4a8e2a8f0fe5b9cc6b5",
+                "cff31c9879da0967313d3f561aed766b",
             ),
             # sha256sum check
             (
                 "sha256sum </etc/wireguard/wg1.conf",
-                "95cb51d9c343bee0a99f025402a0709e"
-                "b9ac469a79c1f7f47fb846e81031b11a",
+                "8443055d1442d051588beb03f7895b58"
+                "269196eb9916617969dc5220c1a90d54",
             ),
+            # check if systemd started wg0
+            ("systemctl is-active wg-quick@wg0", "active"),
+            # check if systemd started wg1
+            ("systemctl is-active wg-quick@wg1", "active"),
+            # check readiness probe (ping wg0)
+            ("cat /tmp/ping", "0"),
         ),
     )
     def test_wireguard(
@@ -84,8 +88,3 @@ class TestWireguard:
     ):
         """Test that 'wg version' succeeds, indicating installation."""
         assert class_client.execute("wg version").ok
-
-    def test_wireguard_systemd_running(
-        self, class_client: IntegrationInstance
-    ):
-        return
