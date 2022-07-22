fcaecf6a46ac2a24e6a4069f321ca3ea4ab3527e
Brett Holman <brett.holman@canonical.com>
2022-07-22 16:13:32 -0600

    " ".join() as Guido intended

--- cloudinit/distros/gentoo.py
+++ cloudinit/distros/gentoo.py
@@ -72,14 +72,15 @@ class Distro(distros.Distro):
         nameservers = []
 
         for (dev, info) in entries.items():
+            dns_nameservers_fmt: str = ""
             if "dns-nameservers" in info:
                 nameservers.extend(info["dns-nameservers"])
             if dev == "lo":
                 continue
             net_fn = self.network_conf_fn + "." + dev
-            dns_nameservers = info.get("dns-nameservers")
-            if isinstance(dns_nameservers, (list, tuple)):
-                dns_nameservers = str(tuple(dns_nameservers)).replace(",", "")
+            dns_nameservers: list = info.get("dns-nameservers", [])
+            if dns_nameservers:
+                dns_nameservers_fmt = " ".join(dns_nameservers)
             # eth0, {'auto': True, 'ipv6': {}, 'bootproto': 'dhcp'}
             # lo, {'dns-nameservers': ['10.0.1.3'], 'ipv6': {}, 'auto': True}
             results = ""
@@ -98,9 +99,9 @@ class Distro(distros.Distro):
                 results += 'routes_{name}="default via {gateway}"\n'.format(
                     name=dev, gateway=info.get("gateway")
                 )
-            if info.get("dns-nameservers"):
+            if dns_nameservers_fmt:
                 results += 'dns_servers_{name}="{dnsservers}"\n'.format(
-                    name=dev, dnsservers=dns_nameservers
+                    name=dev, dnsservers=dns_nameservers_fmt
                 )
             util.write_file(net_fn, results)
             self._create_network_symlink(dev)
