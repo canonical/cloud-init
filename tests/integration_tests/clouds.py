3acf306a21022aa1a73d06997db1c59e0f031644
Alberto Contreras <alberto.contreras@canonical.com>
2022-08-01 15:48:21 +0200

    testing: Fix user-data redacting

--- tests/integration_tests/clouds.py
+++ tests/integration_tests/clouds.py
@@ -162,8 +162,9 @@ class IntegrationCloud(ABC):
         }
         launch_kwargs = {**default_launch_kwargs, **launch_kwargs}
         display_launch_kwargs = deepcopy(launch_kwargs)
-        if "token" in display_launch_kwargs["user_data"]:
-            display_launch_kwargs["user_data"] = "REDACTED"
+        if display_launch_kwargs.get("user_data") is not None:
+            if "token" in display_launch_kwargs.get("user_data"):
+                display_launch_kwargs["user_data"] = "REDACTED"
         log.info(
             "Launching instance with launch_kwargs:\n%s",
             "\n".join(
