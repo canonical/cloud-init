668a68fe577368555ea7f71577fc352494a98c25
Brett Holman <brett.holman@canonical.com>
2022-08-05 18:11:06 +0200

    util: Fix error path and parsing in get_proc_ppid()
    If IOError was thrown, the variable "contents" was
    unbound and threw a name error exception.
    
    Parsing /proc/<num>/stat was broken in less common
    cases.
    
    Fix both and add parsing tests.

--- cloudinit/util.py
+++ cloudinit/util.py
@@ -2894,13 +2894,13 @@ def get_proc_ppid(pid):
     ppid = 0
     try:
         contents = load_file("/proc/%s/stat" % pid, quiet=True)
+        if contents:
+            # see proc.5 for format
+            m = re.search(r"^\d+ \(.+\) [RSDZTtWXxKWP] (\d+)", str(contents))
+            if m:
+                ppid = int(m.group(1))
     except IOError as e:
         LOG.warning("Failed to load /proc/%s/stat. %s", pid, e)
-    if contents:
-        parts = contents.split(" ", 4)
-        # man proc says
-        #  ppid %d     (4) The PID of the parent.
-        ppid = int(parts[3])
     return ppid
 
 
