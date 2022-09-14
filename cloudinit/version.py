4f97bbaceebe0048e5fe65ceef96c3f4a9274ffe
Brett Holman <brett.holman@canonical.com>
2022-09-14 15:49:04 -0600

    Release 22.3.2
    Bump the version in cloudinit/version.py to 22.3.2 and
    update ChangeLog.
    
    LP: #1986703

--- cloudinit/version.py
+++ cloudinit/version.py
@@ -4,7 +4,7 @@
 #
 # This file is part of cloud-init. See LICENSE file for license information.
 
-__VERSION__ = "22.3.1"
+__VERSION__ = "22.3.2"
 _PACKAGED_VERSION = "@@PACKAGED_VERSION@@"
 
 FEATURES = [
