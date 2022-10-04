57d303b4d49b692c3e37d272cba28e376a394479
Mina Galić <me+git@igalic.co>
2022-10-04 22:15:03 +0100

    distros/manage_services: disable service
    in order to support some modules on BSD, which could replace
    base-services, we'll need to be able to disable those base-services.
    This PR adds support for the "disable" command to 'systemct', BSD
    'service', and OpenBSD 'rcctl', which currently suppor that.
    For SysV init 'service', we follow the 'enable' path, and 'stop' the
    service instead.
    
    LP 1991024
    
    Sponsored by: FreeBSD Foundation

--- cloudinit/distros/openbsd.py
+++ cloudinit/distros/openbsd.py
@@ -37,6 +37,7 @@ class Distro(cloudinit.distros.netbsd.NetBSD):
             "stop": ["stop", service],
             "start": ["start", service],
             "enable": ["enable", service],
+            "disable": ["disable", service],
             "restart": ["restart", service],
             "reload": ["restart", service],
             "try-reload": ["restart", service],
