5481f48f1ac3119101af2b840527a5f9292a86e0
Mina Galić <me+git@igalic.co>
2022-10-18 21:50:17 +0100

    tests: introduce a setUp() method

--- tests/unittests/distros/test_ifconfig.py
+++ tests/unittests/distros/test_ifconfig.py
@@ -5,31 +5,30 @@ from tests.unittests.helpers import TestCase, readResource
 
 
 class TestIfconfigParser(TestCase):
+    def setUp(self):
+        super(TestIfconfigParser, self).setUp()
+        self.ifs_txt = readResource("netinfo/freebsd-ifconfig-output")
+
     def test_parse_freebsd(self):
         """assert parsing works without any exceptions"""
-        ifs_txt = readResource("netinfo/freebsd-ifconfig-output")
-        Ifconfig().parse(ifs_txt)
+        Ifconfig().parse(self.ifs_txt)
 
     def test_is_bridge(self):
         """assert bridge0 is_bridge"""
-        ifs_txt = readResource("netinfo/freebsd-ifconfig-output")
-        ifs = Ifconfig().parse(ifs_txt)
+        ifs = Ifconfig().parse(self.ifs_txt)
         assert ifs[2].is_bridge
 
     def test_is_vlan(self):
         """assert re0.33 is_vlan"""
-        ifs_txt = readResource("netinfo/freebsd-ifconfig-output")
-        ifs = Ifconfig().parse(ifs_txt)
+        ifs = Ifconfig().parse(self.ifs_txt)
         assert ifs[1].is_vlan
 
     def test_description(self):
         """assert vnet0:11 is associated with jail: webirc"""
-        ifs_txt = readResource("netinfo/freebsd-ifconfig-output")
-        ifs = Ifconfig().parse(ifs_txt)
+        ifs = Ifconfig().parse(self.ifs_txt)
         assert ifs[3].description == "'associated with jail: webirc'"
 
     def test_vtnet_options(self):
         """assert vtnet has TXCSUM"""
-        ifs_txt = readResource("netinfo/freebsd-ifconfig-output")
-        ifs = Ifconfig().parse(ifs_txt)
+        ifs = Ifconfig().parse(self.ifs_txt)
         assert "txcsum" in ifs[0].options
