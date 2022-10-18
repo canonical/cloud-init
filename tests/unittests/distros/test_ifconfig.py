c79b952cb31bc78f6e4a5554d4e4c58e7be129fd
Mina Galić <me+git@igalic.co>
2022-10-18 21:42:29 +0100

    tests: add more tests. This now needs a setup …

--- tests/unittests/distros/test_ifconfig.py
+++ tests/unittests/distros/test_ifconfig.py
@@ -4,20 +4,32 @@ from cloudinit.distros.parsers.ifconfig import Ifconfig
 from tests.unittests.helpers import TestCase, readResource
 
 
-class TestSysConfHelper(TestCase):
+class TestIfconfigParser(TestCase):
     def test_parse_freebsd(self):
         """assert parsing works without any exceptions"""
         ifs_txt = readResource("netinfo/freebsd-ifconfig-output")
         Ifconfig().parse(ifs_txt)
 
-    def test_parse_is_bridge(self):
+    def test_is_bridge(self):
         """assert bridge0 is_bridge"""
         ifs_txt = readResource("netinfo/freebsd-ifconfig-output")
         ifs = Ifconfig().parse(ifs_txt)
         assert ifs[2].is_bridge
 
-    def test_parse_is_vlan(self):
+    def test_is_vlan(self):
         """assert re0.33 is_vlan"""
         ifs_txt = readResource("netinfo/freebsd-ifconfig-output")
         ifs = Ifconfig().parse(ifs_txt)
         assert ifs[1].is_vlan
+
+    def test_description(self):
+        """assert vnet0:11 is associated with jail: webirc"""
+        ifs_txt = readResource("netinfo/freebsd-ifconfig-output")
+        ifs = Ifconfig().parse(ifs_txt)
+        assert ifs[3].description == "'associated with jail: webirc'"
+
+    def test_vtnet_options(self):
+        """assert vtnet has TXCSUM"""
+        ifs_txt = readResource("netinfo/freebsd-ifconfig-output")
+        ifs = Ifconfig().parse(ifs_txt)
+        assert "txcsum" in ifs[0].options
