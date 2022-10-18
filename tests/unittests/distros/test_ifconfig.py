8e2877679da693786bd7b902ebb249f75b8d0b34
Mina Galić <me+git@igalic.co>
2022-10-18 16:07:50 +0100

    start adding tests

--- tests/unittests/distros/test_ifconfig.py
+++ tests/unittests/distros/test_ifconfig.py
@@ -6,5 +6,18 @@ from tests.unittests.helpers import TestCase, readResource
 
 class TestSysConfHelper(TestCase):
     def test_parse_freebsd(self):
+        """assert parsing works without any exceptions"""
+        ifs_txt = readResource("netinfo/freebsd-ifconfig-output")
+        Ifconfig().parse(ifs_txt)
+
+    def test_parse_is_bridge(self):
+        """assert bridge0 is_bridge"""
         ifs_txt = readResource("netinfo/freebsd-ifconfig-output")
         ifs = Ifconfig().parse(ifs_txt)
+        assert ifs[2].is_bridge
+
+    def test_parse_is_vlan(self):
+        """assert re0.33 is_vlan"""
+        ifs_txt = readResource("netinfo/freebsd-ifconfig-output")
+        ifs = Ifconfig().parse(ifs_txt)
+        assert ifs[1].is_vlan
