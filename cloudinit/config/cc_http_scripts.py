2d97c0db5fa939213381c76f322159e6185987b1
ophum <inagaki0106@gmail.com>
2022-12-24 04:44:04 +0900

    Add HttpScripts Module
    format
    
    fix lint

---
+++ cloudinit/config/cc_http_scripts.py
@@ -0,0 +1,82 @@
+"""HttpScripts Module: Run scripts fetched by http"""
+import subprocess
+import sys
+import urllib.request
+from logging import Logger, getLogger
+from subprocess import PIPE
+from textwrap import dedent
+from typing import List
+
+from cloudinit.cloud import Cloud
+from cloudinit.config import Config
+from cloudinit.config.schema import MetaSchema, get_meta_doc
+from cloudinit.distros import ALL_DISTROS
+from cloudinit.settings import PER_INSTANCE
+
+MODULE_DESCRIPTION = """\
+This module is like ``curl http://example.com/install.sh | sh``
+"""
+
+meta: MetaSchema = {
+    "id": "cc_http_scripts",
+    "name": "HttpScripts",
+    "title": "run scripts fetched by http",
+    "description": MODULE_DESCRIPTION,
+    "distros": [ALL_DISTROS],
+    "frequency": PER_INSTANCE,
+    "activate_by_schema_keys": ["http_scripts"],
+    "examples": [
+        dedent(
+            """\
+            http_scripts:
+              - url: http://example.com/install.sh
+                environments:
+                  ENV: val
+            """
+        )
+    ],
+}
+
+__doc__ = get_meta_doc(meta)
+LOG = getLogger(__name__)
+
+
+def handle(
+    name: str, cfg: Config, cloud: Cloud, log: Logger, args: list
+) -> None:
+    http_scripts: List[dict] = cfg.get("http_scripts", [])
+    for http_script in http_scripts:
+        if "url" not in http_script:
+            raise ValueError(f"Missing required key 'url' from {http_script}")
+
+        url = http_script.get("url", "")
+        environments = http_script.get("environments", {})
+
+        LOG.debug("fetch script: %s", url)
+        script = fetch_script(url)
+
+        LOG.debug("run script: %s", url)
+        p = subprocess.run(
+            ["/bin/sh"],
+            input=script,
+            stdout=PIPE,
+            stderr=PIPE,
+            env=environments,
+            check=False,
+        )
+
+        if p.returncode == 0:
+            LOG.debug("succeeded %s", url)
+        else:
+            LOG.debug("failed %s, return %d", url, p.returncode)
+
+        if p.stdout:
+            sys.stdout.write(f"{p.stdout!r}")
+        if p.stderr:
+            sys.stderr.write(f"{p.stderr!r}")
+
+
+def fetch_script(url: str) -> bytes:
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as res:
+        return res.read()
