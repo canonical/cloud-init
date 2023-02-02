e06e501e2d93254001f157fda29dadada072add6
James Falcon <james.falcon@canonical.com>
2023-02-02 17:16:03 -0600

    tests: Remove restart check from test
    If no SSH restart happens, we get no log. Since this is perfectly
    normal, we shouldn't be checking for a log.

--- tests/integration_tests/modules/test_set_password.py
+++ tests/integration_tests/modules/test_set_password.py
@@ -199,10 +199,6 @@ class Mixin:
             "'systemctl', 'show', '--property', 'ActiveState', "
             "'--value', 'ssh'" in log
         )
-        assert (
-            "Restarted the SSH daemon" in log
-            or "'ssh_pwauth' configuration may not be applied" in log
-        )
 
     def test_sshd_config(self, class_client):
         """Test that SSH password auth is enabled."""
