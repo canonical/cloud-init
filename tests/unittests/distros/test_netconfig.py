d8f1db6e0a7501d68334f1f52b7a3e0e29f85557
Chad Smith <chad.smith@canonical.com>
2023-02-03 20:54:17 -0700

    test: comment about masks

--- tests/unittests/distros/test_netconfig.py
+++ tests/unittests/distros/test_netconfig.py
@@ -1315,6 +1315,7 @@ class TestNetCfgDistroMariner(TestNetCfgDistroBase):
 
 
 def get_mode(path, target=None):
+    # Mask upper st_mode bits like S_IFREG bit preserve sticky and isuid/osgid
     return os.stat(subp.target_path(target, path)).st_mode & 0o7777
 
 
