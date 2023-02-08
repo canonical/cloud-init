aa7ea85302d0ad2bd1d2711ce78dba0bce52c6b0
Chad Smith <chad.smith@canonical.com>
2023-02-08 06:55:51 -0700

    docs: fix 404 render use default notfound_urls_prefix in RTD conf (#2004)
    404 styling was off due to invalid config
    notfound_urls_prefix = "/"
    
    Drop custom config. The default "/en/latest"
    value properly renders the 404 page with style.

--- doc/rtd/conf.py
+++ doc/rtd/conf.py
@@ -85,7 +85,6 @@ autosectionlabel_prefix_document = True
 autosectionlabel_maxdepth = 2
 
 # Sphinx-copybutton config options:
-notfound_urls_prefix = "/"
 notfound_body = (
     "<h1>Page not found</h1><p>Sorry we missed you! Our docs have had a"
     " remodel and some deprecated links have changed.</p><p>"
