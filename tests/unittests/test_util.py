c0ea137b344a5ad1c9cff6bd5438ac9dd121bdc2
Brett Holman <brett.holman@canonical.com>
2023-02-27 10:08:15 -0700

    do not attempt dns resolution on ip addresses

--- tests/unittests/test_util.py
+++ tests/unittests/test_util.py
@@ -3026,3 +3026,15 @@ class TestVersion:
     )
     def test_from_str(self, str_ver, cls_ver):
         assert util.Version.from_str(str_ver) == cls_ver
+
+
+@pytest.mark.allow_dns_lookup
+class TestResolvable:
+    def test_ips_need_not_be_resolved(self):
+        """Optimization test: dns resolution may timeout during early boot, and
+        often the urls being checked use IP addresses rather than dns names.
+        Therefore, the fast path checks if the address contains an IP and exits
+        early if the path is a valid IP.
+        """
+        assert util.is_resolvable("http://169.254.169.254/")
+        assert util.is_resolvable("http://[fd00:ec2::254]/")
