b10607f4570654b6249853c24b8395c5f93feba0
James Falcon <james.falcon@canonical.com>
2023-03-02 11:35:39 -0600

    Release 23.1.1
    Bump the version in cloudinit/version.py to 23.1.1 and
    update ChangeLog.

--- cloudinit/version.py
+++ cloudinit/version.py
@@ -4,7 +4,7 @@
 #
 # This file is part of cloud-init. See LICENSE file for license information.
 
-__VERSION__ = "23.1"
+__VERSION__ = "23.1.1"
 _PACKAGED_VERSION = "@@PACKAGED_VERSION@@"
 
 FEATURES = [
