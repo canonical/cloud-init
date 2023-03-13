37b02477dd7a434a46af17508bcab2b9b49e91bc
Brett Holman <brett.holman@canonical.com>
2023-03-13 13:39:23 -0600

    comments

--- tests/unittests/test_util.py
+++ tests/unittests/test_util.py
@@ -3030,11 +3030,14 @@ class TestVersion:
 
 @pytest.mark.allow_dns_lookup
 class TestResolvable:
-    def test_ips_need_not_be_resolved(self):
+    @mock.patch.object(util, "_DNS_REDIRECT_IP", return_value=True)
+    @mock.patch.object(util.socket, "getaddrinfo")
+    def test_ips_need_not_be_resolved(self, m_getaddr, m_dns):
         """Optimization test: dns resolution may timeout during early boot, and
         often the urls being checked use IP addresses rather than dns names.
         Therefore, the fast path checks if the address contains an IP and exits
         early if the path is a valid IP.
         """
-        assert util.is_resolvable("http://169.254.169.254/")
-        assert util.is_resolvable("http://[fd00:ec2::254]/")
+        assert util.is_resolvable("http://169.254.169.254/") is True
+        assert util.is_resolvable("http://[fd00:ec2::254]/") is True
+        assert not m_getaddr.called
