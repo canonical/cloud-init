06a14757748fdce6878ffd2ef53101b1af925729
Mina Galić <freebsd@igalic.co>
2023-06-12 18:35:52 +0100

    Ephemeral Networking for FreeBSD (#2165)
    Implement ephemeral networking for BSD
    
    After #2127 lay the foundation, we now implement the BSD side of this
    
    Sponsored by: The FreeBSD Foundation

--- cloudinit/net/netops/bsd_netops.py
+++ cloudinit/net/netops/bsd_netops.py
@@ -1,16 +1,17 @@
 from typing import Optional
 
 import cloudinit.net.netops as netops
+from cloudinit import subp
 
 
 class BsdNetOps(netops.NetOps):
     @staticmethod
     def link_up(interface: str):
-        pass
+        subp.subp(["ifconfig", interface, "up"])
 
     @staticmethod
     def link_down(interface: str):
-        pass
+        subp.subp(["ifconfig", interface, "down"])
 
     @staticmethod
     def add_route(
@@ -20,11 +21,15 @@ class BsdNetOps(netops.NetOps):
         gateway: Optional[str] = None,
         source_address: Optional[str] = None
     ):
-        pass
+        subp.subp(["route", "add", route, "-interface", interface])
+        if gateway and gateway != "0.0.0.0":
+            subp.subp(
+                ["route", "change", route, gateway],
+            )
 
     @staticmethod
     def append_route(interface: str, address: str, gateway: str):
-        pass
+        return BsdNetOps.add_route(interface, route=address, gateway=gateway)
 
     @staticmethod
     def del_route(
@@ -34,16 +39,36 @@ class BsdNetOps(netops.NetOps):
         gateway: Optional[str] = None,
         source_address: Optional[str] = None
     ):
-        pass
+        subp.subp(
+            ["route", "del", address]
+            + ([gateway] if gateway and gateway != "0.0.0.0" else []),
+        )
 
     @staticmethod
     def get_default_route() -> str:
-        pass
+        std, _ = subp.subp(["route", "-nv", "get", "0.0.0.0/0"])
+        return std.splitlines()[-1].strip()
 
     @staticmethod
     def add_addr(interface: str, address: str, broadcast: str):
-        pass
+        subp.subp(
+            [
+                "ifconfig",
+                interface,
+                address,
+                "broadcast",
+                broadcast,
+                "alias",
+            ],
+        )
 
     @staticmethod
     def del_addr(interface: str, address: str):
-        pass
+        subp.subp(
+            [
+                "ifconfig",
+                interface,
+                address,
+                "-alias",
+            ],
+        )
