9fdc7a9cdb1efb06bf10891e3b0c32d9ee2ddf00
dermotbradley <dermot_bradley@yahoo.com>
2023-08-01 22:04:27 +0100

    cc_ssh_import_id: add Alpine support and add doas support (#4277)
    Add Alpine to distros supported by this module.
    
    If sudo is not available then try and use doas as an alternative
    if it is available.

--- tests/unittests/config/test_cc_ssh_import_id.py
+++ tests/unittests/config/test_cc_ssh_import_id.py
@@ -76,3 +76,52 @@ class TestHandleSshImportIDs:
         cloud = get_cloud("ubuntu")
         cc_ssh_import_id.handle("name", cfg, cloud, [])
         assert log in caplog.text
+
+    @mock.patch("cloudinit.ssh_util.pwd.getpwnam")
+    @mock.patch("cloudinit.config.cc_ssh_import_id.subp.subp")
+    @mock.patch("cloudinit.subp.which")
+    def test_use_sudo(self, m_which, m_subp, m_getpwnam):
+        """Check that sudo is available and use that"""
+        m_which.return_value = "/usr/bin/ssh-import-id"
+        ids = ["waffle"]
+        user = "bob"
+        cc_ssh_import_id.import_ssh_ids(ids, user)
+        m_subp.assert_called_once_with(
+            [
+                "sudo",
+                "--preserve-env=https_proxy",
+                "-Hu",
+                user,
+                "ssh-import-id",
+            ]
+            + ids,
+            capture=False,
+        )
+
+    @mock.patch("cloudinit.ssh_util.pwd.getpwnam")
+    @mock.patch("cloudinit.config.cc_ssh_import_id.subp.subp")
+    @mock.patch("cloudinit.subp.which")
+    def test_use_doas(self, m_which, m_subp, m_getpwnam):
+        """Check that doas is available and use that"""
+        m_which.side_effect = [None, "/usr/bin/doas"]
+        ids = ["waffle"]
+        user = "bob"
+        cc_ssh_import_id.import_ssh_ids(ids, user)
+        m_subp.assert_called_once_with(
+            ["doas", "-u", user, "ssh-import-id"] + ids, capture=False
+        )
+
+    @mock.patch("cloudinit.ssh_util.pwd.getpwnam")
+    @mock.patch("cloudinit.config.cc_ssh_import_id.subp.subp")
+    @mock.patch("cloudinit.subp.which")
+    def test_use_neither_sudo_nor_doas(
+        self, m_which, m_subp, m_getpwnam, caplog
+    ):
+        """Test when neither sudo nor doas is available"""
+        m_which.return_value = None
+        ids = ["waffle"]
+        user = "bob"
+        cc_ssh_import_id.import_ssh_ids(ids, user)
+        assert (
+            "Neither sudo nor doas available! Unable to import SSH ids"
+        ) in caplog.text
