canonical/cloud-init

commit f550c8765ca03d313e54edf35209b877ef3381ff

Adding BOOTPROTO = dhcp to render sysconfig dhcp6 stateful on RHEL (#685)
BOOTPROTO needs to be set to 'dhcp' on RHEL so NetworkManager can
properly acquire ipv6 address.

rhbz: #1859695

Signed-off-by: Eduardo Otubo <otubo@redhat.com>

Co-authored-by: Daniel Watkins <oddbloke@ubuntu.com>
Co-authored-by: Scott Moser <smoser@brickies.net>

cloudinit/net/sysconfig.py
@@ -396,6 +396,12 @@ class Renderer(renderer.Renderer):
                         # Only IPv6 is DHCP, IPv4 may be static
                         iface_cfg['BOOTPROTO'] = 'dhcp6'
                     iface_cfg['DHCLIENT6_MODE'] = 'managed'
+                # only if rhel AND dhcpv6 stateful
+                elif (flavor == 'rhel' and
+                        subnet_type == 'ipv6_dhcpv6-stateful'):
+                    iface_cfg['BOOTPROTO'] = 'dhcp'
+                    iface_cfg['DHCPV6C'] = True
+                    iface_cfg['IPV6INIT'] = True
                 else:
                     iface_cfg['IPV6INIT'] = True
                     # Configure network settings using DHCPv6

tests/unittests/test_net.py
@@ -1365,7 +1365,7 @@ NETWORK_CONFIGS = {
         },
         'expected_sysconfig_rhel': {
             'ifcfg-iface0': textwrap.dedent("""\
-            BOOTPROTO=none
+            BOOTPROTO=dhcp
             DEVICE=iface0
             DHCPV6C=yes
             IPV6INIT=yes

commit 8cad8c1df2fa9a25d7a24399e81253f7ff01efed

net: fix ipv6_dhcpv6_stateful configuration for rhel
When network type is ipv6_dhcpv6-stateful, cloud-init seems to enable dhcp for
both ipv4 and ipv6. Network manager prefers dhcp over ipv4 and hence dhcp6 is
not used to obtain the IP address. This is incorrect. For ipv6_dhcpv6-stateful
networks, we should set:

ipv4.method = disabled // disables all ipv4 dhcp
ipv6.method = dhcp
ipv6.may-fail = no // dhcp6 must succeed.

For ifcfg files (sysconfig renderer), the corresponding changes should be:

BOOTPROTO = none // instead of dhcp so that dhcp4 is disabled.
IPV6_FAILURE_FATAL = yes // so that dhcp6 should succeed.

This patch fixes this. This patch has been tested by Red Hat QE and it seems
to fix the above issue.

RHBZ: 2227767
fixes: f550c8765ca03d ("Adding BOOTPROTO = dhcp to render sysconfig dhcp6 stateful on RHEL (#685)")

Signed-off-by: Ani Sinha <anisinha@redhat.com>

cloudinit/net/network_manager.py
@@ -36,7 +36,7 @@ LOG = logging.getLogger(__name__)
 class NMConnection:
     """Represents a NetworkManager connection profile."""
 
-    def __init__(self, con_id):
+    def __init__(self, con_id, cloud_config=None):
         """
         Initializes the connection with some very basic properties,
         notably the UUID so that the connection can be referred to.
@@ -48,6 +48,8 @@ class NMConnection:
         self.config = configparser.ConfigParser()
         # Identity option name mapping, to achieve case sensitivity
         self.config.optionxform = str
+
+        self.cloud_config = {} if not cloud_config else cloud_config
 
         self.config["connection"] = {
             "id": f"cloud-init {con_id}",
@@ -104,6 +106,12 @@ class NMConnection:
             return
         if self.config[family]["method"] == "auto" and method == "manual":
             return
+
+        flavor = self.cloud_config.get("flavor", "rhel")
+        if flavor == "rhel" and subnet_type == "ipv6_dhcpv6-stateful":
+            # set ipv4 method to 'disabled' so that dhcp4 is turned off and
+            # the setting aligns with sysconfig renderer
+            self._set_default("ipv4", "method", "disabled")
 
         self.config[family]["method"] = method
         self._set_default(family, "may-fail", "false")
@@ -342,6 +350,7 @@ class Renderer(renderer.Renderer):
 
     def __init__(self, config=None):
         self.connections = {}
+        self.config = config
 
     def get_conn(self, con_id):
         return self.connections[con_id]
@@ -363,7 +372,9 @@ class Renderer(renderer.Renderer):
         # interfaces that have UUIDs that can be linked to from related
         # interfaces
         for iface in network_state.iter_interfaces():
-            self.connections[iface["name"]] = NMConnection(iface["name"])
+            self.connections[iface["name"]] = NMConnection(
+                iface["name"], self.config
+            )
 
         # Now render the actual interface configuration
         for iface in network_state.iter_interfaces():

cloudinit/net/sysconfig.py
@@ -439,10 +439,11 @@ class Renderer(renderer.Renderer):
                 elif (
                     flavor == "rhel" and subnet_type == "ipv6_dhcpv6-stateful"
                 ):
-                    iface_cfg["BOOTPROTO"] = "dhcp"
+                    iface_cfg["BOOTPROTO"] = "none"
                     iface_cfg["DHCPV6C"] = True
                     iface_cfg["IPV6INIT"] = True
                     iface_cfg["IPV6_AUTOCONF"] = False
+                    iface_cfg["IPV6_FAILURE_FATAL"] = True
                 else:
                     iface_cfg["IPV6INIT"] = True
                     # Configure network settings using DHCPv6

tests/unittests/test_net.py
@@ -2091,11 +2091,12 @@ NETWORK_CONFIGS = {
         "expected_sysconfig_rhel": {
             "ifcfg-iface0": textwrap.dedent(
                 """\
-            BOOTPROTO=dhcp
+            BOOTPROTO=none
             DEVICE=iface0
             DHCPV6C=yes
             IPV6INIT=yes
             IPV6_AUTOCONF=no
+            IPV6_FAILURE_FATAL=yes
             IPV6_FORCE_ACCEPT_RA=yes
             DEVICE=iface0
             NM_CONTROLLED=no