8d9da9024de96c8f3afd0030fea009736cf6bc3b
dermotbradley <dermot_bradley@yahoo.com>
2023-08-15 15:42:55 +0100

    cc_keyboard: add Alpine support (#4278)
    Add support for Alpine Linux to cc_keyboard.

--- tests/unittests/config/test_cc_keyboard.py
+++ tests/unittests/config/test_cc_keyboard.py
@@ -2,19 +2,27 @@
 
 """Tests cc_keyboard module"""
 
+import os
 import re
+from unittest import mock
 
 import pytest
 
+from cloudinit.config import cc_keyboard
 from cloudinit.config.schema import (
     SchemaValidationError,
     get_schema,
     validate_cloudconfig_schema,
 )
-from tests.unittests.helpers import skipUnlessJsonSchema
+from tests.unittests.helpers import (
+    FilesystemMockingTestCase,
+    populate_dir,
+    skipUnlessJsonSchema,
+)
+from tests.unittests.util import get_cloud
 
 
-class TestKeyboard:
+class TestKeyboardSchema:
     @pytest.mark.parametrize(
         "config, error_msg",
         (
@@ -72,6 +80,95 @@ class TestKeyboard:
         else:
             with pytest.raises(SchemaValidationError, match=error_msg):
                 validate_cloudconfig_schema(config, schema, strict=True)
+
+
+class TestKeyboard(FilesystemMockingTestCase):
+    with_logs = True
+
+    def setUp(self):
+        super(TestKeyboard, self).setUp()
+        self.root_d = self.tmp_dir()
+        self.root_d = self.reRoot()
+
+    @mock.patch("cloudinit.distros.Distro.uses_systemd")
+    @mock.patch("cloudinit.distros.subp.subp")
+    def test_systemd_linux_cmd(self, m_subp, m_uses_systemd, *args):
+        """Ubuntu runs localectl"""
+        cfg = {"keyboard": {"layout": "us", "variant": "us"}}
+        layout = "us"
+        model = "pc105"
+        variant = "us"
+        options = ""
+        m_uses_systemd.return_value = True
+        cloud = get_cloud("ubuntu")
+        cc_keyboard.handle("cc_keyboard", cfg, cloud, [])
+        locale_calls = [
+            mock.call(
+                [
+                    "localectl",
+                    "set-x11-keymap",
+                    layout,
+                    model,
+                    variant,
+                    options,
+                ]
+            ),
+            mock.call(
+                ["systemctl", "restart", "console-setup"],
+                capture=True,
+                rcs=None,
+            ),
+        ]
+        m_subp.assert_has_calls(locale_calls)
+
+    @mock.patch("cloudinit.distros.subp.subp")
+    def test_alpine_linux_cmd(self, m_subp, *args):
+        """Alpine Linux runs setup-keymap"""
+        cfg = {"keyboard": {"layout": "us", "variant": "us"}}
+        layout = "us"
+        variant = "us"
+        cloud = get_cloud("alpine")
+
+        # Create a dummy directory and file for keymap
+        keymap_dir = "/usr/share/bkeymaps/%s" % "us"
+        keymap_file = "%s/%s.bmap.gz" % (keymap_dir, "us")
+        os.makedirs("%s%s" % (self.root_d, keymap_dir))
+        populate_dir(self.root_d, {keymap_file: "# Test\n"})
+
+        cc_keyboard.handle("cc_keyboard", cfg, cloud, [])
+        m_subp.assert_called_once_with(["setup-keymap", layout, variant])
+
+    @mock.patch("cloudinit.distros.subp.subp")
+    def test_alpine_linux_ignore_model(self, m_subp):
+        """Alpine Linux ignores model setting"""
+        cfg = {
+            "keyboard": {
+                "layout": "us",
+                "model": "pc105",
+                "variant": "us",
+            },
+        }
+        layout = "us"
+        variant = "us"
+        cloud = get_cloud("alpine")
+
+        keymap_dir = "/usr/share/bkeymaps/%s" % "us"
+        keymap_file = "%s/%s.bmap.gz" % (keymap_dir, "us")
+        os.makedirs("%s%s" % (self.root_d, keymap_dir))
+        populate_dir(self.root_d, {keymap_file: "# Test\n"})
+
+        cc_keyboard.handle("cc_keyboard", cfg, cloud, [])
+        assert (
+            "Keyboard model is ignored for Alpine Linux."
+            in self.logs.getvalue()
+        )
+        m_subp.assert_called_once_with(
+            [
+                "setup-keymap",
+                layout,
+                variant,
+            ],
+        )
 
 
 # vi: ts=4 expandtab
