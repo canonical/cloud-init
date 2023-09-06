3caa602fdcaf1d064f4cff88f883724fe4c34eff
Brett Holman <brett.holman@canonical.com>
2023-09-06 17:08:46 -0600

    Work around no instance ip
    When an instance has `execute_via_ssh`, it might be that in the
    lifecycle of the instance in the test an ip address is not assigned.
    In this case, do not attempt to print the ip address during system
    teardown.

--- tests/integration_tests/instances.py
+++ tests/integration_tests/instances.py
@@ -201,7 +201,9 @@ class IntegrationInstance:
         if self._ip:
             return self._ip
         try:
-            self._ip = self.instance.ip
+            # in some cases that ssh is not used, an address is not assigned
+            if self.instance.execute_via_ssh:
+                self._ip = self.instance.ip
         except NotImplementedError:
             self._ip = "Unknown"
         return self._ip
