25c08f67234fc532f8d1438de68191a38bc0af7f
Alberto Contreras <alberto.contreras@canonical.com>
2023-09-18 17:18:03 +0200

    fix test leak

--- tests/unittests/test_apport.py
+++ tests/unittests/test_apport.py
@@ -1,25 +1,13 @@
 import os
+import sys
+from importlib import reload
 
 import pytest
 
+from cloudinit import apport
 from tests.unittests.helpers import mock
 
 M_PATH = "cloudinit.apport."
-
-
-@pytest.fixture()
-def apport(request, mocker, paths):
-    """Mock apport.hookutils before importing cloudinit.apport.
-
-    This avoids our optional import dependency on apport, providing tests with
-    mocked apport.hookutils function call counts.
-    """
-    m_hookutils = mock.Mock()
-    mocker.patch.dict("sys.modules", {"apport.hookutils": m_hookutils})
-    mocker.patch(M_PATH + "read_cfg_paths", return_value=paths)
-    from cloudinit import apport
-
-    yield apport
 
 
 class TestApport:
@@ -45,10 +33,10 @@ class TestApport:
         ),
     )
     def test_attach_cloud_info(
-        self, instance_data, choice_idx, expected_report, apport, paths
+        self, instance_data, choice_idx, expected_report, mocker, paths
     ):
         """Prompt for cloud name when instance-data.json is not-json/absent."""
-
+        mocker.patch(M_PATH + "read_cfg_paths", return_value=paths)
         instance_data_file = paths.get_runpath("instance_data")
         if instance_data is None:
             assert not os.path.exists(instance_data_file)
@@ -66,11 +54,16 @@ class TestApport:
         else:
             assert ui.choice.call_count == 0
 
-    def test_attach_user_data(self, apport, paths):
+    def test_attach_user_data(self, mocker, paths):
         user_data_file = paths.get_ipath_cur("userdata_raw")
         ui = mock.Mock()
         ui.yesno.return_value = True
         report = object()
+        m_hookutils = mock.Mock()
+
+        with mock.patch.dict(sys.modules, {"apport.hookutils": m_hookutils}):
+            reload(sys.modules["cloudinit.apport"])
+            mocker.patch(M_PATH + "read_cfg_paths", return_value=paths)
         apport.attach_user_data(report, ui)
         assert [
             mock.call(report, user_data_file, "user_data.txt"),
@@ -104,7 +97,7 @@ class TestApport:
             ),
         ),
     )
-    def test_add_bug_tags_assigns_proper_tags(self, report, tags, apport):
+    def test_add_bug_tags_assigns_proper_tags(self, report, tags):
         """Tags are assigned based on non-empty project report key values."""
 
         apport.add_bug_tags(report)
@@ -120,15 +113,19 @@ class TestApport:
         ),
     )
     @mock.patch("cloudinit.apport.stages.Init")
-    def test_attach_ubuntu_pro_info(self, m_init, cfg, info_attached, apport):
-        m_init.return_value = mock.Mock(cfg=cfg)
-        report = {}
-        apport.attach_ubuntu_pro_info(report)
+    def test_attach_ubuntu_pro_info(self, m_init, cfg, info_attached):
+
+        m_hookutils = mock.Mock()
+        with mock.patch.dict(sys.modules, {"apport.hookutils": m_hookutils}):
+            reload(sys.modules["cloudinit.apport"])
+            m_init.return_value = mock.Mock(cfg=cfg)
+            report = {}
+            apport.attach_ubuntu_pro_info(report)
 
         if info_attached:
             assert [
                 mock.call(report, "/var/log/ubuntu-advantage.log"),
-            ] == apport.attach_file_if_exists.call_args_list
+            ] == m_hookutils.attach_file_if_exists.call_args_list
 
             assert report.get("Tags", "") == "ubuntu-pro"
         else:
