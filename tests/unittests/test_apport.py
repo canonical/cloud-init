506e70fa195574851aed0a88819adf30bbc98947
Alberto Contreras <alberto.contreras@canonical.com>
2023-09-28 05:26:01 +0200

    feat(apport): collect ubuntu-pro logs if ubuntu-advantage.log present (#4443)
    Add ubuntu-pro tag to bug report when ubuntu-advantage.log is present.

--- tests/unittests/test_apport.py
+++ tests/unittests/test_apport.py
@@ -1,25 +1,13 @@
 import os
+import sys
+from importlib import reload
 
 import pytest
 
+from cloudinit import apport
 from tests.unittests.helpers import mock
 
 M_PATH = "cloudinit.apport."
-
-
-@pytest.fixture()
-def apport(request, mocker, paths):
-    """Mock apport.hookutils before importing cloudinit.apport.
-
-    This avoids our optional import dependency on apport, providing tests with
-    mocked apport.hookutils function call counts.
-    """
-    m_hookutils = mock.Mock()
-    mocker.patch.dict("sys.modules", {"apport.hookutils": m_hookutils})
-    mocker.patch(M_PATH + "read_cfg_paths", return_value=paths)
-    from cloudinit import apport
-
-    yield apport
 
 
 class TestApport:
@@ -45,10 +33,10 @@ class TestApport:
         ),
     )
     def test_attach_cloud_info(
-        self, instance_data, choice_idx, expected_report, apport, paths
+        self, instance_data, choice_idx, expected_report, mocker, paths
     ):
         """Prompt for cloud name when instance-data.json is not-json/absent."""
-
+        mocker.patch(M_PATH + "read_cfg_paths", return_value=paths)
         instance_data_file = paths.get_runpath("instance_data")
         if instance_data is None:
             assert not os.path.exists(instance_data_file)
@@ -66,11 +54,16 @@ class TestApport:
         else:
             assert ui.choice.call_count == 0
 
-    def test_attach_user_data(self, apport, paths):
+    def test_attach_user_data(self, mocker, paths):
         user_data_file = paths.get_ipath_cur("userdata_raw")
         ui = mock.Mock()
         ui.yesno.return_value = True
         report = object()
+        m_hookutils = mock.Mock()
+
+        with mock.patch.dict(sys.modules, {"apport.hookutils": m_hookutils}):
+            reload(sys.modules["cloudinit.apport"])
+            mocker.patch(M_PATH + "read_cfg_paths", return_value=paths)
         apport.attach_user_data(report, ui)
         assert [
             mock.call(report, user_data_file, "user_data.txt"),
@@ -104,8 +97,34 @@ class TestApport:
             ),
         ),
     )
-    def test_add_bug_tags_assigns_proper_tags(self, report, tags, apport):
+    def test_add_bug_tags_assigns_proper_tags(self, report, tags):
         """Tags are assigned based on non-empty project report key values."""
 
         apport.add_bug_tags(report)
         assert report.get("Tags", "") == tags
+
+    @mock.patch(M_PATH + "os.path.exists", return_value=True)
+    def test_attach_ubuntu_pro_info(self, m_exists):
+        m_hookutils = mock.Mock()
+        with mock.patch.dict(sys.modules, {"apport.hookutils": m_hookutils}):
+            reload(sys.modules["cloudinit.apport"])
+            report = {}
+            apport.attach_ubuntu_pro_info(report)
+
+        assert [
+            mock.call(report, "/var/log/ubuntu-advantage.log"),
+        ] == m_hookutils.attach_file_if_exists.call_args_list
+        assert report.get("Tags", "") == "ubuntu-pro"
+
+    @mock.patch(M_PATH + "os.path.exists", return_value=False)
+    def test_attach_ubuntu_pro_info_log_non_present(self, m_exists):
+        m_hookutils = mock.Mock()
+        with mock.patch.dict(sys.modules, {"apport.hookutils": m_hookutils}):
+            reload(sys.modules["cloudinit.apport"])
+            report = {}
+            apport.attach_ubuntu_pro_info(report)
+
+        assert [
+            mock.call(report, "/var/log/ubuntu-advantage.log"),
+        ] == m_hookutils.attach_file_if_exists.call_args_list
+        assert report.get("Tags", "") == ""
