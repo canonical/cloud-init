cb36bf38b823f811a3e938ccffc03d7d13190095
Brett Holman <brett.holman@canonical.com>
2024-01-03 09:11:40 -0700

    fix(cloudstack): Use parsed lease file for virtual router in cloudstack
    Fixes 5942f4023e2581a

--- cloudinit/sources/DataSourceCloudStack.py
+++ cloudinit/sources/DataSourceCloudStack.py
@@ -281,18 +281,18 @@ def get_vr_address():
         )
         return latest_address
 
-    # Try dhcp lease files next...
+    # Try dhcp lease files next
     lease_file = dhcp.IscDhclient.get_latest_lease()
-    if not lease_file:
-        LOG.debug("No lease file found, using default gateway")
-        return get_default_gateway()
+    if lease_file:
+        latest_address = dhcp.IscDhclient.parse_dhcp_server_from_lease_file(
+            lease_file
+        )
+        if latest_address:
+            return latest_address
 
-    lease_file = dhcp.IscDhclient.parse_dhcp_server_from_lease_file(lease_file)
-    if not latest_address:
-        # No virtual router found, fallback on default gateway
-        LOG.debug("No DHCP found, using default gateway")
-        return get_default_gateway()
-    return latest_address
+    # No virtual router found, fallback to default gateway
+    LOG.debug("No DHCP found, using default gateway")
+    return get_default_gateway()
 
 
 # Used to match classes to dependencies
