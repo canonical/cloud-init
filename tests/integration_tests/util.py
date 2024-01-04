39abae9ec05c8fcca313d56aeee63d3b5c8eb0e3
Chad Smith <chad.smith@canonical.com>
2024-01-04 14:11:38 -0700

    feat(apt): remove /etc/apt/sources.list when deb22 preferred (#4740)
    Some images may contain /etc/apt/sources.list which cloud-init
    historically would silently overwrite on first boot.
    Remove this file rather than preserve a vestigial disabled file as
    /etc/apt/sources.list.disabled because the disabled file may
    just confuse users as to the why the file exists and whether they
    should consider re-enabling it.
    
    If a customer reenables a previously disabled
    /etc/apt/sources.list.disabled file and the same APT sources are
    defined in both /etc/apt/sources.list and deb822 formatted
    /etc/apt/sources.list.d/ubuntu.sources, apt update and cache commands
    will emit warnings about duplicate sources being detected which
    could break tooling which attempts to parse apt command output.
    
    Let's avoid leaving unnecessary artifacts on the sytem given all
    previous versions of cloud-init would silently overwrite this
    /etc/apt/sources.list file in on first boot anyway.

--- tests/integration_tests/util.py
+++ tests/integration_tests/util.py
@@ -68,7 +68,7 @@ def verify_clean_log(log: str, ignore_deprecations: bool = True):
         "thinpool by default on Ubuntu due to LP #1982780",
         "WARNING]: Could not match supplied host pattern, ignoring:",
         # Old Ubuntu cloud-images contain /etc/apt/sources.list
-        "WARNING]: Disabling /etc/apt/sources.list to favor deb822 source"
+        "WARNING]: Removing /etc/apt/sources.list to favor deb822 source"
         " format",
     ]
     traceback_texts = []
