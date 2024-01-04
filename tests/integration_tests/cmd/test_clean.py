fe97af0c3c25c11c3a2cd2ddac6490211f3b11df
Chad Smith <chad.smith@canonical.com>
2024-01-04 11:21:22 -0700

    tests: add expected exit 2 on noble from cloud-init status (#4738)
    Commit d662ffc5 introduced a warning from cloud-init when disabling
    an image build artifact /etc/apt/sources.lists in favor of writing
    deb822 format sources file at /etc/apt/sources.list.d/ubuntu.sources.
    
    As such, integration tests cannot check execute(cloud-init status).ok,
    they have to instead expect to see non-zero exit 2 due to the warning.
    
    We want to make this strict expectation of exit 2 on noble so we can
    track in jenkins when Ubuntu Noble image builds correctly remove this
    undesired build artifact /etc/apt/sources.list.

--- tests/integration_tests/cmd/test_clean.py
+++ tests/integration_tests/cmd/test_clean.py
@@ -4,6 +4,7 @@ import re
 import pytest
 
 from tests.integration_tests.instances import IntegrationInstance
+from tests.integration_tests.releases import CURRENT_RELEASE
 
 USER_DATA = """\
 #cloud-config
@@ -32,7 +33,15 @@ write_files:
 class TestCleanCommand:
     def test_clean_by_param(self, class_client: IntegrationInstance):
         """Clean with various params alters expected files without error"""
-        assert class_client.execute("cloud-init status --wait").ok
+        result = class_client.execute("cloud-init status --wait --long")
+
+        # Expect warning exit code 2 on Ubuntu Noble due to cloud-init
+        # disabling /etc/apt/sources.list build artifact in favor of deb822
+        return_code = 2 if CURRENT_RELEASE.series == "noble" else 0
+        assert return_code == result.return_code, (
+            f"Unexpected cloud-init status exit code {result.return_code}\n"
+            f"Output:\n{result}"
+        )
         result = class_client.execute("cloud-init clean")
         assert (
             result.ok
