e69ef2a5e0eec385846e45e7d8adc2842273bc2c
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2024-01-16 18:09:34 -0300

    Implements win->WSL translations
    - Translates Win paths to WSL with wslpath.
    - Find cmd.exe by looking up in the known mount points that match the
      Windows fstype and mountpoint options
    - Finds the Windows user profile dir by running cmd.exe.
    
    All possible error conditions can be dealt with at the caller site
    Let's allow the callee to LOG some stuff and return None
    
    This should make the code simpler.

--- cloudinit/sources/DataSourceWSL.py
+++ cloudinit/sources/DataSourceWSL.py
@@ -6,13 +6,15 @@
 """ Datasource to support the Windows Subsystem for Linux platform. """
 
 import logging
+import os
 from pathlib import PurePath
+from typing import List, Optional, cast
 
-from cloudinit import subp
+from cloudinit import sources, subp, util
 
 LOG = logging.getLogger(__name__)
 
-WSLPATH_CMD = "wslpath"
+WSLPATH_CMD = "/usr/bin/wslpath"
 
 
 def wsl_path_2_win(path: str) -> PurePath:
@@ -36,3 +38,86 @@ def instance_name() -> str:
     """
     root_net_path = wsl_path_2_win("/")
     return root_net_path.name
+
+
+def mounted_win_drives() -> List[str]:
+    """
+    Return a list of mount points of the Windows drives inside the current
+    WSL instance, if drives are mounted, or an empty list otherwise
+    """
+    FS_TYPE = "9p"
+    OPTIONS_CONTAIN = "aname=drvfs"
+
+    mounts = util.mounts()
+    mounted = []
+    for mnt in mounts.values():
+        if mnt["fstype"] == FS_TYPE and OPTIONS_CONTAIN in mnt["opts"]:
+            mounted.append(mnt["mountpoint"])
+
+    return mounted
+
+
+def win_path_2_wsl(path: str) -> Optional[PurePath]:
+    """
+    Returns a translation of a Windows path to a Linux path that can be
+    accessed inside the current instance filesystem, or None if failed.
+
+    It requires the Windows drive mounting feature to be enabled and the
+    disk drive must exist for this to succeed.
+
+    Example:
+    # Assuming Windows drives are mounted under /mnt/ and "S:" doesn't exist:
+    p = winpath2wsl("C:\\ProgramData") # p == "/mnt/c/ProgramData/"
+    n = winpath2wsl("S:\\CoolFolder") # n is None (S doesn't exist)
+
+    :param path: string representing a Windows path. The root drive must exist,
+    although the path is not required to.
+    """
+    out, err = subp.subp([WSLPATH_CMD, "-au", path], rcs=[0, 1])
+    if err:
+        LOG.debug(err)
+        return None
+
+    return PurePath(out.rstrip())
+
+
+def cmd_executable() -> Optional[PurePath]:
+    """
+    Returns the Linux path to the Windows host's cmd.exe.
+    """
+
+    mounts = mounted_win_drives()
+    if not mounts:
+        LOG.error("Windows drives are not mounted.")
+        return None
+
+    # cmd.exe path is being stable for decades.
+    candidate = "%s/Windows/System32/cmd.exe"
+    for mnt in mounts:
+        cmd = candidate % mnt
+        if not os.access(cmd, os.X_OK):
+            continue
+
+        LOG.debug("Found cmd.exe at <%s>", cmd)
+        return PurePath(cmd)
+
+    LOG.error("Couldn't find cmd.exe in any mount point.")
+    return None
+
+
+def win_user_profile_dir() -> Optional[PurePath]:
+    """
+    Returns the Windows user profile directory translated as a Linux path
+    accessible inside the current WSL instance.
+    """
+    cmd = cmd_executable()
+    if cmd is None:
+        return None
+
+    home, _ = subp.subp([cmd.as_posix(), "/C", "echo %USERPROFILE%"])
+    home = home.rstrip()
+    if not home:
+        LOG.error("No output from cmd to show the user profile dir.")
+        return None
+
+    return win_path_2_wsl(home.rstrip())
--- tests/unittests/sources/test_wsl.py
+++ tests/unittests/sources/test_wsl.py
@@ -4,13 +4,45 @@
 #
 # This file is part of cloud-init. See LICENSE file for license information.
 
-from pathlib import PurePath
+from copy import deepcopy
 
 from cloudinit import util
 from cloudinit.sources import DataSourceWSL as wsl
 from tests.unittests.helpers import CiTestCase, mock
 
 INSTANCE_NAME = "Noble-MLKit"
+GOOD_MOUNTS = {
+    "none": {
+        "fstype": "tmpfs",
+        "mountpoint": "/mnt/wsl",
+        "opts": "rw,relatime",
+    },
+    "/dev/sdd": {
+        "fstype": "ext4",
+        "mountpoint": "/",
+        "opts": "rw,relatime,...",
+    },
+    "sysfs": {
+        "fstype": "sysfs",
+        "mountpoint": "/sys",
+        "opts": "rw,nosuid...",
+    },
+    "C:\\": {
+        "fstype": "9p",
+        "mountpoint": "/mnt/c",
+        "opts": "rw,noatime,dirsync,aname=drvfs;path=C:\\;...",
+    },
+    "D:\\": {
+        "fstype": "9p",
+        "mountpoint": "/mnt/d",
+        "opts": "rw,noatime,dirsync,aname=drvfs;path=D:\\;...",
+    },
+    "hugetblfs": {
+        "fstype": "hugetblfs",
+        "mountpoint": "/dev/hugepages",
+        "opts": "rw,relatime...",
+    },
+}
 
 
 class TestWSLHelperFunctions(CiTestCase):
@@ -24,3 +56,90 @@ class TestWSLHelperFunctions(CiTestCase):
 
         self.assertEqual(INSTANCE_NAME, inst)
 
+    @mock.patch("cloudinit.util.mounts")
+    def test_mounted_drives(self, m_mounts):
+        # A good output
+        m_mounts.return_value = deepcopy(GOOD_MOUNTS)
+        mounts = wsl.mounted_win_drives()
+        self.assertListEqual(["/mnt/c", "/mnt/d"], mounts)
+
+        # no more drvfs in C:\ options
+        m_mounts.return_value["C:\\"]["opts"] = "rw,relatime..."
+        mounts = wsl.mounted_win_drives()
+        self.assertListEqual(["/mnt/d"], mounts)
+
+        # fstype mismatch for D:\
+        m_mounts.return_value["D:\\"]["fstype"] = "zfs"
+        mounts = wsl.mounted_win_drives()
+        self.assertListEqual([], mounts)
+
+    @mock.patch("cloudinit.util.subp.subp")
+    def test_path_2_wsl_logic(self, m_subp):
+        """
+        Validates that we interpret stderr correctly when translating paths
+        from Windows into Linux.
+        """
+        m_subp.return_value = util.subp.SubpResult("/mnt/c/ProgramData/", "")
+
+        translated = wsl.win_path_2_wsl("C:\\ProgramData")
+        self.assertIsNotNone(translated)
+
+        # When an invalid drive is passed, wslpath prints the following pattern
+        # to stderr:
+        # wslpath: <ARGV_1 (aka the path)>
+        m_subp.return_value = util.subp.SubpResult(
+            "", "wslpath: X:\\ProgramData\\"
+        )
+
+        translated = wsl.win_path_2_wsl("X:\\ProgramData")
+        self.assertIsNone(translated)
+
+    @mock.patch("os.access")
+    @mock.patch("cloudinit.util.mounts")
+    def test_cmd_exe_ok(self, m_mounts, m_os_access):
+        """
+        Validates the happy path, when we find the Windows system drive and
+        cmd.exe is executable.
+        """
+        m_mounts.return_value = deepcopy(GOOD_MOUNTS)
+        m_os_access.return_value = True
+        cmd = wsl.cmd_executable()
+        # To please pyright not to complain about optional member access.
+        assert cmd is not None
+        self.assertIsNotNone(
+            cmd.relative_to(GOOD_MOUNTS["C:\\"]["mountpoint"])
+        )
+
+    @mock.patch("os.access")
+    @mock.patch("cloudinit.util.mounts")
+    def test_cmd_not_executable(self, m_mounts, m_os_access):
+        """
+        When the cmd.exe found is not executable, then RuntimeError is raised.
+        """
+        m_mounts.return_value = deepcopy(GOOD_MOUNTS)
+        m_os_access.return_value = True
+        cmd = wsl.cmd_executable()
+        # To please pyright not to complain about optional member access.
+        assert cmd is not None
+        self.assertIsNotNone(
+            cmd.relative_to(GOOD_MOUNTS["C:\\"]["mountpoint"])
+        )
+
+        m_os_access.return_value = False
+        self.assertIsNone(wsl.cmd_executable())
+
+    @mock.patch("os.access")
+    @mock.patch("cloudinit.util.mounts")
+    def test_cmd_exe_no_win_mounts(self, m_mounts, m_os_access):
+        """
+        When no Windows drives are found, then RuntimeError is raised.
+        """
+        m_os_access.return_value = True
+
+        m_mounts.return_value = deepcopy(GOOD_MOUNTS)
+        m_mounts.return_value.pop("C:\\")
+        m_mounts.return_value.pop("D:\\")
+        with self.assertRaises(RuntimeError) as ctx:
+            _ = wsl.cmd_executable()
+
+        self.assertIn("drives", str(ctx.exception))
