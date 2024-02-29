4c854389d2908c579980f4c2f75b7c63ac79ed86
Chad Smith <chad.smith@canonical.com>
2024-02-29 13:35:48 -0700

    apt: kill spawned keyboxd after gpg cmd interaction
    gnupg version 2.4.4 spawns keyboxd daemon automatically when
    interacting with gpg on the commandline.
    
    Cleanup this spawned daemon after cloud-init is done setting up
    any APT configuration to avoid leaving resident 'unused' processes
    on the system.

--- tests/unittests/config/test_apt_configure_sources_list_v3.py
+++ tests/unittests/config/test_apt_configure_sources_list_v3.py
@@ -331,7 +331,17 @@ class TestAptSourceConfigSourceList:
         assert expected == sources_file.read()
         assert 0o644 == stat.S_IMODE(sources_file.stat().mode)
         self.subp.assert_called_once_with(
-            ["ps", "-o", "ppid,pid", "-C", "dirmngr", "-C", "gpg-agent"],
+            [
+                "ps",
+                "-o",
+                "ppid,pid",
+                "-C",
+                "keyboxd",
+                "-C",
+                "dirmngr",
+                "-C",
+                "gpg-agent",
+            ],
             capture=True,
             rcs=[0, 1],
         )
