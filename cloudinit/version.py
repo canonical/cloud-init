3c8488dccb1c7a2f34095cf09473c9b918a8f29e
Chad Smith <chad.smith@canonical.com>
2024-04-03 08:29:07 -0600

    Release 24.1.4
    Bump the version in cloudinit/version.py to 24.1.4 and
    update ChangeLog.

--- cloudinit/version.py
+++ cloudinit/version.py
@@ -4,7 +4,7 @@
 #
 # This file is part of cloud-init. See LICENSE file for license information.
 
-__VERSION__ = "24.1.3"
+__VERSION__ = "24.1.4"
 _PACKAGED_VERSION = "@@PACKAGED_VERSION@@"
 
 FEATURES = [
