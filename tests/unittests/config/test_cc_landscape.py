aa357fa23712230745b52c770bcc7f7535ceaae1
Fabian Lichtenegger-Lukas <48928888+chifac08@users.noreply.github.com>
2024-05-03 21:02:07 +0200

    feat(landscape-client): handle already registered client (#4784)
    Use "--is-registered" parameter to prevent duplicate registration 
    of clients on a Landscape server when cloud-init runs more than
    once.
     
    Log a warning if the computer was already registered with Landscape.
    
    Generally, this condition can only happen across system reboot
    in cases where:
     - an image that was already registered with landscape was cloned
       and launched as a new instance
     - the cloud platform issued a new instance-id to to the instance
       in cases like image/vm clone, new network configuration or new
       meta-data changes
     - cloud-init clean was run forcing allowing all modules to re-run

--- tests/unittests/config/test_cc_landscape.py
+++ tests/unittests/config/test_cc_landscape.py
@@ -3,6 +3,7 @@ import logging
 
 import pytest
 
+from cloudinit import subp
 from cloudinit.config import cc_landscape
 from cloudinit.config.schema import (
     SchemaValidationError,
@@ -14,8 +15,10 @@ from tests.unittests.util import get_cloud
 
 LOG = logging.getLogger(__name__)
 
+MPATH = "cloudinit.config.cc_landscape"
 
-@mock.patch("cloudinit.config.cc_landscape.subp.subp")
+
+@mock.patch(f"{MPATH}.subp.subp")
 class TestLandscape:
     def test_skip_empty_landscape_cloudconfig(self, m_subp):
         """Empty landscape cloud-config section does no work."""
@@ -60,6 +63,9 @@ class TestLandscape:
         )
         assert [
             mock.call(
+                ["landscape-config", "--silent", "--is-registered"], rcs=[5]
+            ),
+            mock.call(
                 [
                     "landscape-config",
                     "--silent",
@@ -92,6 +98,9 @@ class TestLandscape:
         }
         expected_calls = [
             mock.call(
+                ["landscape-config", "--silent", "--is-registered"], rcs=[5]
+            ),
+            mock.call(
                 [
                     "landscape-config",
                     "--silent",
@@ -122,7 +131,6 @@ class TestLandscape:
             ["landscape-client"]
         )
         assert expected_calls == m_subp.call_args_list
-        assert "RUN=1\n" == default_fn.read()
 
     def test_handler_writes_merged_client_config_file_with_defaults(
         self, m_subp, tmpdir
@@ -136,6 +144,9 @@ class TestLandscape:
         mycloud.distro = mock.MagicMock()
         cfg = {"landscape": {"client": {}}}
         expected_calls = [
+            mock.call(
+                ["landscape-config", "--silent", "--is-registered"], rcs=[5]
+            ),
             mock.call(
                 [
                     "landscape-config",
@@ -180,6 +191,9 @@ class TestLandscape:
         cfg = {"landscape": {"client": {"computer_title": 'My" PC'}}}
         expected_calls = [
             mock.call(
+                ["landscape-config", "--silent", "--is-registered"], rcs=[5]
+            ),
+            mock.call(
                 [
                     "landscape-config",
                     "--silent",
@@ -209,6 +223,37 @@ class TestLandscape:
             None,
         )
         assert expected_calls == m_subp.call_args_list
+
+    @mock.patch(f"{MPATH}.merge_together")
+    def test_handler_client_failed_registering(self, m_merge_together, m_subp):
+        """landscape-client could not be registered"""
+        mycloud = get_cloud("ubuntu")
+        mycloud.distro = mock.MagicMock()
+        cfg = {"landscape": {"client": {"computer_title": 'My" PC'}}}
+        m_subp.side_effect = subp.ProcessExecutionError(
+            "Could not register client"
+        )
+        match = (
+            "Failure registering client:\nUnexpected error while"
+            " running command.\nCommand: -\nExit code: -\nReason: -\n"
+            "Stdout: Could not register client\nStderr: -"
+        )
+        with pytest.raises(RuntimeError, match=match):
+            cc_landscape.handle("notimportant", cfg, mycloud, None)
+
+    @mock.patch(f"{MPATH}.merge_together")
+    def test_handler_client_is_already_registered(
+        self, m_merge_together, m_subp, caplog
+    ):
+        """landscape-client is already registered"""
+        mycloud = get_cloud("ubuntu")
+        mycloud.distro = mock.MagicMock()
+        cfg = {"landscape": {"client": {"computer_title": 'My" PC'}}}
+        m_subp.side_effect = subp.ProcessExecutionError(
+            "Client already registered to Landscape", exit_code=0
+        )
+        cc_landscape.handle("notimportant", cfg, mycloud, None)
+        assert "Client already registered to Landscape" in caplog.text
 
 
 class TestLandscapeSchema:
