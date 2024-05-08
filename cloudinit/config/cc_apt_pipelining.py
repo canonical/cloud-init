68829b1bc1074ebc20c3cae41639517bde637cfa
Alex Ratner <al-x@users.noreply.github.com>
2024-05-08 11:36:01 -0600

    cc_apt_pipelining docstring typo fix

--- cloudinit/config/cc_apt_pipelining.py
+++ cloudinit/config/cc_apt_pipelining.py
@@ -35,7 +35,7 @@ meta: MetaSchema = {
     "title": "Configure apt pipelining",
     "description": dedent(
         """\
-        This module configures apt's ``Acquite::http::Pipeline-Depth`` option,
+        This module configures apt's ``Acquire::http::Pipeline-Depth`` option,
         which controls how apt handles HTTP pipelining. It may be useful for
         pipelining to be disabled, because some web servers, such as S3 do not
         pipeline properly (LP: #948461).
