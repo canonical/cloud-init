174ee22e55dc55229863e8d78c0844fdfc83fb29
Chad Smith <chad.smith@canonical.com>
2024-05-11 16:06:17 -0600

    chore(doc): migrate cc modules i through r to templates (#5313)
    Modules migrated:
    install_hotplug, keyboard, _keys_to_console, landscape, locale, lxd,
    cc_mcollective, cc_mounts, ntp, package_update_upgrade_install,
    phone_home, power_state_change, puppet, resizefs, resolv_conf,
    rh_subscription

--- cloudinit/config/cc_keys_to_console.py
+++ cloudinit/config/cc_keys_to_console.py
@@ -10,63 +10,22 @@
 
 import logging
 import os
-from textwrap import dedent
 
 from cloudinit import subp, util
 from cloudinit.cloud import Cloud
 from cloudinit.config import Config
-from cloudinit.config.schema import MetaSchema, get_meta_doc
+from cloudinit.config.schema import MetaSchema
 from cloudinit.settings import PER_INSTANCE
 
 # This is a tool that cloud init provides
 HELPER_TOOL_TPL = "%s/cloud-init/write-ssh-key-fingerprints"
 
-distros = ["all"]
-
 meta: MetaSchema = {
     "id": "cc_keys_to_console",
-    "name": "Keys to Console",
-    "title": "Control which SSH host keys may be written to console",
-    "description": (
-        "For security reasons it may be desirable not to write SSH host keys"
-        " and their fingerprints to the console. To avoid either being written"
-        " to the console the ``emit_keys_to_console`` config key under the"
-        " main ``ssh`` config key can be used. To avoid the fingerprint of"
-        " types of SSH host keys being written to console the"
-        " ``ssh_fp_console_blacklist`` config key can be used. By default,"
-        " all types of keys will have their fingerprints written to console."
-        " To avoid host keys of a key type being written to console the"
-        "``ssh_key_console_blacklist`` config key can be used. By default"
-        " all supported host keys are written to console."
-    ),
-    "distros": distros,
-    "examples": [
-        dedent(
-            """\
-            # Do not print any SSH keys to system console
-            ssh:
-              emit_keys_to_console: false
-            """
-        ),
-        dedent(
-            """\
-            # Do not print certain ssh key types to console
-            ssh_key_console_blacklist: [rsa]
-            """
-        ),
-        dedent(
-            """\
-            # Do not print specific ssh key fingerprints to console
-            ssh_fp_console_blacklist:
-            - E25451E0221B5773DEBFF178ECDACB160995AA89
-            - FE76292D55E8B28EE6DB2B34B2D8A784F8C0AAB0
-            """
-        ),
-    ],
+    "distros": ["all"],
     "frequency": PER_INSTANCE,
     "activate_by_schema_keys": [],
-}
-__doc__ = get_meta_doc(meta)
+}  # type: ignore
 
 LOG = logging.getLogger(__name__)
 
--- cloudinit/config/cc_package_update_upgrade_install.py
+++ cloudinit/config/cc_package_update_upgrade_install.py
@@ -9,12 +9,11 @@
 import logging
 import os
 import time
-from textwrap import dedent
 
 from cloudinit import subp, util
 from cloudinit.cloud import Cloud
 from cloudinit.config import Config
-from cloudinit.config.schema import MetaSchema, get_meta_doc
+from cloudinit.config.schema import MetaSchema
 from cloudinit.distros import ALL_DISTROS
 from cloudinit.log import flush_loggers
 from cloudinit.settings import PER_INSTANCE
@@ -22,40 +21,10 @@ from cloudinit.settings import PER_INSTANCE
 REBOOT_FILES = ("/var/run/reboot-required", "/run/reboot-needed")
 REBOOT_CMD = ["/sbin/reboot"]
 
-MODULE_DESCRIPTION = """\
-This module allows packages to be updated, upgraded or installed during boot.
-If any packages are to be installed or an upgrade is to be performed then the
-package cache will be updated first. If a package installation or upgrade
-requires a reboot, then a reboot can be performed if
-``package_reboot_if_required`` is specified.
-"""
-
 meta: MetaSchema = {
     "id": "cc_package_update_upgrade_install",
-    "name": "Package Update Upgrade Install",
-    "title": "Update, upgrade, and install packages",
-    "description": MODULE_DESCRIPTION,
     "distros": [ALL_DISTROS],
     "frequency": PER_INSTANCE,
-    "examples": [
-        dedent(
-            """\
-            packages:
-              - pwgen
-              - pastebinit
-              - [libpython3.8, 3.8.10-0ubuntu1~20.04.2]
-              - snap:
-                - certbot
-                - [juju, --edge]
-                - [lxd, --channel=5.15/stable]
-              - apt:
-                - mg
-            package_update: true
-            package_upgrade: true
-            package_reboot_if_required: true
-            """
-        )
-    ],
     "activate_by_schema_keys": [
         "apt_update",
         "package_update",
@@ -63,9 +32,8 @@ meta: MetaSchema = {
         "package_upgrade",
         "packages",
     ],
-}
+}  # type: ignore
 
-__doc__ = get_meta_doc(meta)
 LOG = logging.getLogger(__name__)
 
 
