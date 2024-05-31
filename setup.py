0b1fa9033be19a0ffd7fa2a45d4ab4285ee74f68
Brett Holman <brett.holman@canonical.com>
2024-05-31 12:56:50 -0600

    ci(mypy): Set default follow_imports value (#5350)
    Remove code for detecting a virtualenv. This has been broken since ~3.7.

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
 # isort: off
 from setup_utils import (  # noqa: E402
     get_version,
-    in_virtualenv,
     is_f,
     is_generator,
     pkg_config_read,
@@ -266,13 +265,12 @@ class InitsysInstallData(install):
         self.distribution.reinitialize_command("install_data", True)
 
 
-if not in_virtualenv():
-    USR = "/" + USR
-    ETC = "/" + ETC
-    USR_LIB_EXEC = "/" + USR_LIB_EXEC
-    LIB = "/" + LIB
-    for k in INITSYS_ROOTS.keys():
-        INITSYS_ROOTS[k] = "/" + INITSYS_ROOTS[k]
+USR = "/" + USR
+ETC = "/" + ETC
+USR_LIB_EXEC = "/" + USR_LIB_EXEC
+LIB = "/" + LIB
+for k in INITSYS_ROOTS.keys():
+    INITSYS_ROOTS[k] = "/" + INITSYS_ROOTS[k]
 
 data_files = [
     (ETC + "/cloud", [render_tmpl("config/cloud.cfg.tmpl", is_yaml=True)]),
@@ -308,8 +306,7 @@ data_files = [
 ]
 if not platform.system().endswith("BSD"):
     RULES_PATH = pkg_config_read("udev", "udevdir")
-    if not in_virtualenv():
-        RULES_PATH = "/" + RULES_PATH
+    RULES_PATH = "/" + RULES_PATH
 
     data_files.extend(
         [
