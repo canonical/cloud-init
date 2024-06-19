912423180b1193ae9b466befd8353e1f85332253
cofob <cofob@riseup.net>
2024-06-19 19:16:20 +0400

    Fix the code according to the suggested edits

--- cloudinit/sources/DataSourceAeza.py
+++ cloudinit/sources/DataSourceAeza.py
@@ -4,64 +4,155 @@
 #
 # This file is part of cloud-init. See LICENSE file for license information.
 
-from cloudinit import sources, util
-import cloudinit.sources.helpers.aeza as aeza_helper
+import time
+
+import requests
+from requests.exceptions import HTTPError, RequestException
+
+from cloudinit import sources, dmi, util
 
 BASE_URL_V1 = "http://77.221.156.49/v1/cloudinit"
 
 BUILTIN_DS_CONFIG = {
-    "retries": 60,
-    "timeout": 2,
-    "wait_retry": 2,
     "metadata_url": BASE_URL_V1 + "/{id}/meta-data",
     "userdata_url": BASE_URL_V1 + "/{id}/user-data",
     "vendordata_url": BASE_URL_V1 + "/{id}/vendor-data",
 }
+
+
+def read_url(url, timeout_seconds=None, max_wait_seconds=None, sec_between_retries=1, retries=0):
+    """
+    A simplified HTTP GET request function with retry capability and specific handling for 404 errors.
+
+    :param url: URL to request.
+    :param timeout_seconds: Timeout in seconds for each request.
+    :param max_wait_seconds: Timeout for whole request process.
+    :param sec_between_retries: Seconds to wait between retries.
+    :param retries: Number of retry attempts after the first failed request.
+    """
+    attempt = 0
+    start = time.time()
+    while attempt <= retries:
+        try:
+            response = requests.get(url, timeout=timeout_seconds)
+            response.raise_for_status()  # Will raise an HTTPError for bad responses
+            return response
+        except HTTPError as e:
+            if e.response.status_code == 404:
+                # Handle 404 specifically: exit the loop and return the error
+                raise e
+            if attempt == retries:
+                # If it's the last attempt, re-raise the last exception for non-404 errors
+                raise e
+            # Wait before retrying if not a 404 error and retries are left
+            time.sleep(sec_between_retries)
+            attempt += 1
+        except RequestException as e:
+            if attempt == retries:
+                # If it's the last attempt, re-raise the last exception
+                raise e
+            # Wait before retrying if retries are left
+            time.sleep(sec_between_retries)
+            attempt += 1
+        # If max_wait timeout is enabled check for max wait timeout
+        if max_wait_seconds is not None:
+            if start + max_wait_seconds < time.time():
+                raise RequestException(
+                    f"Failed to retrieve data from {url} after {retries + 1} attempts because of max wait timeout."
+                )
+
+    # If retries are exhausted, raise an exception indicating the failure
+    raise RequestException(f"Failed to retrieve data from {url} after {retries + 1} attempts.")
+
+
+def read_metadata(url, timeout_seconds=2, max_wait_seconds=120, sec_between_retries=2, retries=30):
+    """
+    Read user data from URL or raise error.
+    """
+    response = read_url(
+        url,
+        timeout_seconds=timeout_seconds,
+        max_wait_seconds=max_wait_seconds,
+        sec_between_retries=sec_between_retries,
+        retries=retries,
+    )
+    if not response.ok:
+        raise RuntimeError("unable to read metadata at %s" % url)
+    return util.load_json(response.content.decode())
+
+
+def read_data(url, timeout_seconds=2, max_wait_seconds=120, sec_between_retries=2, retries=30):
+    """
+    Read data from URL.
+    """
+    try:
+        response = read_url(
+            url,
+            timeout_seconds=timeout_seconds,
+            max_wait_seconds=max_wait_seconds,
+            sec_between_retries=sec_between_retries,
+            retries=retries,
+        )
+        if not response.ok:
+            raise RuntimeError("unable to read data at %s" % url)
+        return response.content
+    except HTTPError as e:
+        if e.response.status_code == 404:
+            return None
+        raise e
+
+
+def format_url(url, system_uuid=None):
+    if system_uuid is None:
+        system_uuid = dmi.read_dmi_data("system-uuid")
+    return url.format(id=system_uuid)
+
 
 class DataSourceAeza(sources.DataSource):
 
     dsname = "Aeza"
 
     def __init__(self, sys_cfg, distro, paths, ud_proc=None):
-        sources.DataSource.__init__(self, sys_cfg, distro, paths, ud_proc)
-        self.metadata = {}
-        self.vendordata_raw = None
-        self.userdata_raw = ""
+        super().__init__(sys_cfg, distro, paths, ud_proc)
 
-        self.ds_cfg = util.mergemanydict(
-            [
-                util.get_cfg_by_path(sys_cfg, ["datasource", "Aeza"], {}),
-                BUILTIN_DS_CONFIG,
-            ]
-        )
+        self.ds_cfg = util.mergemanydict([self.ds_cfg, BUILTIN_DS_CONFIG])
 
-        self.timeout = self.ds_cfg["timeout"]
-        self.wait_retry = self.ds_cfg["wait_retry"]
-        self.retries = self.ds_cfg["retries"]
+        url_params = self.get_url_params()
+        self.timeout_seconds = url_params.timeout_seconds
+        self.max_wait_seconds = url_params.max_wait_seconds
+        self.retries = url_params.num_retries
+        self.sec_between_retries = url_params.sec_between_retries
 
         self.metadata_address = self.ds_cfg["metadata_url"]
         self.userdata_address = self.ds_cfg["userdata_url"]
         self.vendordata_address = self.ds_cfg["vendordata_url"]
 
-    def _get_data(self):
-        system_uuid = aeza_helper.read_system_uuid()
+    @staticmethod
+    def ds_detect():
+        return dmi.read_dmi_data("system-manufacturer") == "Aeza"
 
-        md = aeza_helper.read_metadata(
-            aeza_helper.format_url(self.metadata_address, system_uuid),
-            timeout=self.timeout,
-            sec_between=self.wait_retry,
+    def _get_data(self):
+        system_uuid = dmi.read_dmi_data("system-uuid")
+
+        md = read_metadata(
+            format_url(self.metadata_address, system_uuid),
+            timeout_seconds=self.timeout_seconds,
+            max_wait_seconds=self.max_wait_seconds,
+            sec_between_retries=self.sec_between_retries,
             retries=self.retries,
         )
-        ud = aeza_helper.read_data(
-            aeza_helper.format_url(self.userdata_address, system_uuid),
-            timeout=self.timeout,
-            sec_between=self.wait_retry,
+        ud = read_data(
+            format_url(self.userdata_address, system_uuid),
+            timeout_seconds=self.timeout_seconds,
+            max_wait_seconds=self.max_wait_seconds,
+            sec_between_retries=self.sec_between_retries,
             retries=self.retries,
         )
-        vd = aeza_helper.read_data(
-            aeza_helper.format_url(self.vendordata_address, system_uuid),
-            timeout=self.timeout,
-            sec_between=self.wait_retry,
+        vd = read_data(
+            format_url(self.vendordata_address, system_uuid),
+            timeout_seconds=self.timeout_seconds,
+            max_wait_seconds=self.max_wait_seconds,
+            sec_between_retries=self.sec_between_retries,
             retries=self.retries,
         )
 
--- doc/rtd/reference/datasources/aeza.rst
+++ doc/rtd/reference/datasources/aeza.rst
@@ -17,9 +17,6 @@ Aeza's datasource can be configured as follows: ::
       metadata_url: "http://77.221.156.49/v1/cloudinit/{id}/meta-data"
       userdata_url: "http://77.221.156.49/v1/cloudinit/{id}/user-data"
       vendordata_url: "http://77.221.156.49/v1/cloudinit/{id}/vendor-data"
-      retries: 60
-      timeout: 2
-      wait_retry: 2
 
 * ``metadata_url``
 
@@ -32,18 +29,6 @@ Aeza's datasource can be configured as follows: ::
 * ``vendordata_url``
 
   Specifies the URL to retrieve the vendor-data. (optional)
-
-* ``retries``
-
-  The number of times the data retrieval operation should be retried in case of failures. (optional)
-
-* ``timeout``
-
-  The maximum number of seconds to wait for a response from the server for each attempt. (optional)
-
-* ``wait_retry``
-
-  The number of seconds to wait between retries. (optional)
 
 .. note::
    ``{id}`` in URLs is system-uuid DMI value.
