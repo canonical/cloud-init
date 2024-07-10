bef09f4928c626c5fb2047acd3aacf581e63c698
Alberto Contreras <alberto.contreras@canonical.com>
2024-07-10 13:27:42 +0200

    add unittest

--- tests/unittests/sources/helpers/test_openstack.py
+++ tests/unittests/sources/helpers/test_openstack.py
@@ -231,3 +231,129 @@ class TestConvertNetJson:
         assert expected == openstack.convert_net_json(
             network_json=network_json, known_macs=macs
         )
+
+    def test_dns_servers(self):
+        """
+        Verify additional properties under subnet.routes are not rendered
+        """
+        network_json = {
+            "links": [
+                {
+                    "id": "ens1f0np0",
+                    "name": "ens1f0np0",
+                    "type": "phy",
+                    "ethernet_mac_address": "xx:xx:xx:xx:xx:00",
+                    "mtu": 9000,
+                },
+                {
+                    "id": "ens1f1np1",
+                    "name": "ens1f1np1",
+                    "type": "phy",
+                    "ethernet_mac_address": "xx:xx:xx:xx:xx:01",
+                    "mtu": 9000,
+                },
+                {
+                    "id": "bond0",
+                    "name": "bond0",
+                    "type": "bond",
+                    "bond_links": ["ens1f0np0", "ens1f1np1"],
+                    "mtu": 9000,
+                    "ethernet_mac_address": "xx:xx:xx:xx:xx:00",
+                    "bond_mode": "802.3ad",
+                    "bond_xmit_hash_policy": "layer3+4",
+                    "bond_miimon": 100,
+                },
+                {
+                    "id": "bond0.123",
+                    "name": "bond0.123",
+                    "type": "vlan",
+                    "vlan_link": "bond0",
+                    "vlan_id": 123,
+                    "vlan_mac_address": "xx:xx:xx:xx:xx:00",
+                },
+            ],
+            "networks": [
+                {
+                    "id": "publicnet-ipv4",
+                    "type": "ipv4",
+                    "link": "bond0.123",
+                    "ip_address": "x.x.x.x",
+                    "netmask": "255.255.255.0",
+                    "routes": [
+                        {
+                            "network": "0.0.0.0",
+                            "netmask": "0.0.0.0",
+                            "gateway": "x.x.x.1",
+                            "services": [
+                                {"type": "dns", "address": "1.1.1.1"},
+                                {"type": "dns", "address": "8.8.8.8"},
+                            ],
+                        }
+                    ],
+                    "network_id": "00000000-0000-0000-0000-000000000000",
+                }
+            ],
+            "services": [],
+        }
+        expected = {
+            "version": 1,
+            "config": [
+                {
+                    "name": "ens1f0np0",
+                    "type": "physical",
+                    "mtu": 9000,
+                    "subnets": [],
+                    "mac_address": "xx:xx:xx:xx:xx:00",
+                },
+                {
+                    "name": "ens1f1np1",
+                    "type": "physical",
+                    "mtu": 9000,
+                    "subnets": [],
+                    "mac_address": "xx:xx:xx:xx:xx:01",
+                },
+                {
+                    "name": "bond0",
+                    "type": "bond",
+                    "mtu": 9000,
+                    "subnets": [],
+                    "mac_address": "xx:xx:xx:xx:xx:00",
+                    "params": {
+                        "bond-mode": "802.3ad",
+                        "bond-xmit_hash_policy": "layer3+4",
+                        "bond-miimon": 100,
+                    },
+                    "bond_interfaces": ["ens1f0np0", "ens1f1np1"],
+                },
+                {
+                    "name": "bond0.123",
+                    "type": "vlan",
+                    "subnets": [
+                        {
+                            "type": "static",
+                            "netmask": "255.255.255.0",
+                            "routes": [
+                                {
+                                    "network": "0.0.0.0",
+                                    "netmask": "0.0.0.0",
+                                    "gateway": "x.x.x.1",
+                                }
+                            ],
+                            "address": "x.x.x.x",
+                            "dns_nameservers": ["8.8.8.8", "1.1.1.1"],
+                            "ipv4": True,
+                        }
+                    ],
+                    "vlan_id": 123,
+                    "vlan_link": "bond0",
+                },
+            ],
+        }
+        macs = {
+            "xx:xx:xx:xx:xx:00": "ens1f0np0",
+            "xx:xx:xx:xx:xx:01": "ens1f1np1",
+        }
+        netcfg = openstack.convert_net_json(
+            network_json=network_json, known_macs=macs
+        )
+        assert expected == netcfg
