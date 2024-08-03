canonical/cloud-init

commit 5322dca2f3da7bb9b8d6f1fac6ccb00ef33ef8ee

fix(NoCloudNet): Add network-config support (#5566)
This enables support for network config v2 and v1 to NoCloud
when used with http / ftp / etc. 

BREAKING_CHANGE: Adds an additional network request to NoCloud.

cloudinit/sources/DataSourceNoCloud.py
@@ -190,7 +190,7 @@ class DataSourceNoCloud(sources.DataSource):
 
             # This could throw errors, but the user told us to do it
             # so if errors are raised, let them raise
-            (md_seed, ud, vd) = util.read_seeded(seedfrom, timeout=None)
+            md_seed, ud, vd, network = util.read_seeded(seedfrom, timeout=None)
             LOG.debug("Using seeded cache data from %s", seedfrom)
 
             # Values in the command line override those from the seed
@@ -199,6 +199,7 @@ class DataSourceNoCloud(sources.DataSource):
             )
             mydata["user-data"] = ud
             mydata["vendor-data"] = vd
+            mydata["network-config"] = network
             found.append(seedfrom)
 
         # Now that we have exhausted any other places merge in the defaults

cloudinit/util.py
@@ -977,10 +977,11 @@ def read_optional_seed(fill, base="", ext="", timeout=5):
     'meta-data' entries
     """
     try:
-        md, ud, vd = read_seeded(base=base, ext=ext, timeout=timeout)
+        md, ud, vd, network = read_seeded(base=base, ext=ext, timeout=timeout)
         fill["user-data"] = ud
         fill["vendor-data"] = vd
         fill["meta-data"] = md
+        fill["network-config"] = md
         return True
     except url_helper.UrlError as e:
         if e.code == url_helper.NOT_FOUND:
@@ -1066,6 +1067,7 @@ def read_seeded(base="", ext="", timeout=5, retries=10):
         ud_url = base.replace("%s", "user-data" + ext)
         vd_url = base.replace("%s", "vendor-data" + ext)
         md_url = base.replace("%s", "meta-data" + ext)
+        network_url = base.replace("%s", "network-config" + ext)
     else:
         if features.NOCLOUD_SEED_URL_APPEND_FORWARD_SLASH:
             if base[-1] != "/" and parse.urlparse(base).query == "":
@@ -1074,12 +1076,19 @@ def read_seeded(base="", ext="", timeout=5, retries=10):
         ud_url = "%s%s%s" % (base, "user-data", ext)
         vd_url = "%s%s%s" % (base, "vendor-data", ext)
         md_url = "%s%s%s" % (base, "meta-data", ext)
+        network_url = "%s%s%s" % (base, "network-config", ext)
+    network_resp = url_helper.read_file_or_url(
+        network_url, timeout=timeout, retries=retries
+    )
+    network = None
+    if network_resp.ok():
+        network = load_yaml(network_resp.contents)
     md_resp = url_helper.read_file_or_url(
         md_url, timeout=timeout, retries=retries
     )
     md = None
     if md_resp.ok():
-        md = load_yaml(decode_binary(md_resp.contents), default={})
+        md = load_yaml(md_resp.contents, default={})
 
     ud_resp = url_helper.read_file_or_url(
         ud_url, timeout=timeout, retries=retries
@@ -1101,7 +1110,7 @@ def read_seeded(base="", ext="", timeout=5, retries=10):
         else:
             LOG.debug("Error in vendor-data response")
 
-    return (md, ud, vd)
+    return md, ud, vd, network
 
 
 def read_conf_d(confd, *, instance_data_file=None) -> dict:

cloudinit/sources/DataSourceOVF.py
@@ -92,7 +92,7 @@ class DataSourceOVF(sources.DataSource):
                 LOG.debug("Seed from %s not supported by %s", seedfrom, self)
                 return False
 
-            (md_seed, ud, vd) = util.read_seeded(seedfrom, timeout=None)
+            (md_seed, ud, vd, _) = util.read_seeded(seedfrom, timeout=None)
             LOG.debug("Using seeded cache data from %s", seedfrom)
 
             md = util.mergemanydict([md, md_seed])

tests/unittests/test_util.py
@@ -2452,16 +2452,23 @@ class TestReadSeeded:
     def test_unicode_not_messed_up(self, tmpdir):
         ud = b"userdatablob"
         vd = b"vendordatablob"
+        network = b"test: 'true'"
         helpers.populate_dir(
             tmpdir.strpath,
-            {"meta-data": "key1: val1", "user-data": ud, "vendor-data": vd},
+            {
+                "meta-data": "key1: val1",
+                "user-data": ud,
+                "vendor-data": vd,
+                "network-config": network,
+            },
         )
-        (found_md, found_ud, found_vd) = util.read_seeded(
+        found_md, found_ud, found_vd, found_network = util.read_seeded(
             tmpdir.strpath + os.path.sep
         )
         assert found_md == {"key1": "val1"}
         assert found_ud == ud
         assert found_vd == vd
+        assert found_network == {"test": "true"}
 
     @pytest.mark.parametrize(
         "base, feature_flag, req_urls",
@@ -2470,6 +2477,7 @@ class TestReadSeeded:
                 "http://10.0.0.1/%s?qs=1",
                 True,
                 [
+                    "http://10.0.0.1/network-config?qs=1",
                     "http://10.0.0.1/meta-data?qs=1",
                     "http://10.0.0.1/user-data?qs=1",
                     "http://10.0.0.1/vendor-data?qs=1",
@@ -2480,6 +2488,7 @@ class TestReadSeeded:
                 "https://10.0.0.1:8008/",
                 True,
                 [
+                    "https://10.0.0.1:8008/network-config",
                     "https://10.0.0.1:8008/meta-data",
                     "https://10.0.0.1:8008/user-data",
                     "https://10.0.0.1:8008/vendor-data",
@@ -2490,6 +2499,7 @@ class TestReadSeeded:
                 "https://10.0.0.1:8008",
                 True,
                 [
+                    "https://10.0.0.1:8008/network-config",
                     "https://10.0.0.1:8008/meta-data",
                     "https://10.0.0.1:8008/user-data",
                     "https://10.0.0.1:8008/vendor-data",
@@ -2500,6 +2510,7 @@ class TestReadSeeded:
                 "https://10.0.0.1:8008",
                 False,
                 [
+                    "https://10.0.0.1:8008network-config",
                     "https://10.0.0.1:8008meta-data",
                     "https://10.0.0.1:8008user-data",
                     "https://10.0.0.1:8008vendor-data",
@@ -2510,6 +2521,7 @@ class TestReadSeeded:
                 "https://10.0.0.1:8008?qs=",
                 True,
                 [
+                    "https://10.0.0.1:8008?qs=network-config",
                     "https://10.0.0.1:8008?qs=meta-data",
                     "https://10.0.0.1:8008?qs=user-data",
                     "https://10.0.0.1:8008?qs=vendor-data",
@@ -2540,12 +2552,15 @@ class TestReadSeeded:
             "NOCLOUD_SEED_URL_APPEND_FORWARD_SLASH",
             feature_flag,
         ):
-            (found_md, found_ud, found_vd) = util.read_seeded(base)
+            found_md, found_ud, found_vd, found_network = util.read_seeded(
+                base
+            )
         # Meta-data treated as YAML
         assert found_md == {"/meta-data": 1}
         # user-data, vendor-data read raw. It could be scripts or other format
         assert found_ud == "/user-data: 1"
         assert found_vd == "/vendor-data: 1"
+        assert found_network == {"/network-config": 1}
         assert [
             mock.call(req_url, timeout=5, retries=10) for req_url in req_urls
         ] == m_read.call_args_list
@@ -2560,15 +2575,22 @@ class TestReadSeededWithoutVendorData(helpers.TestCase):
     def test_unicode_not_messed_up(self):
         ud = b"userdatablob"
         vd = None
+        network = b"test: 'true'"
         helpers.populate_dir(
-            self.tmp, {"meta-data": "key1: val1", "user-data": ud}
+            self.tmp,
+            {
+                "meta-data": "key1: val1",
+                "user-data": ud,
+                "network-config": network,
+            },
         )
         sdir = self.tmp + os.path.sep
-        (found_md, found_ud, found_vd) = util.read_seeded(sdir)
+        found_md, found_ud, found_vd, found_network = util.read_seeded(sdir)
 
         self.assertEqual(found_md, {"key1": "val1"})
         self.assertEqual(found_ud, ud)
         self.assertEqual(found_vd, vd)
+        self.assertEqual(found_network, {"test": "true"})
 
 
 class TestEncode(helpers.TestCase):

commit c04e0b52e95c5af20c8828a04d76ff336cdf6c98

fix: nocloud no fail when network-config absent
Commit 5322dca2 introduced an assumption to read_seeded that
network-config must always be present for NoCloud datasource.
Since it is still considered and optional supplemental configuration
allow the read_seeed calls to succeed in the absence of network-config.

Avoids failures seen in tests/integration-tests/datasources/test_nocloud.py::
  test_nocloud_seedfrom_vendordata

cloudinit/sources/DataSourceNoCloud.py
@@ -481,7 +481,8 @@ if __name__ == "__main__":
 
     logging.basicConfig(level=logging.DEBUG)
     seedfrom = argv[1]
-    md_seed, ud, vd = util.read_seeded(seedfrom)
+    md_seed, ud, vd, network = util.read_seeded(seedfrom)
     print(f"seeded: {md_seed}")
     print(f"ud: {ud}")
     print(f"vd: {vd}")
+    print(f"network: {network}")

cloudinit/util.py
@@ -1069,12 +1069,16 @@ def read_seeded(base="", ext="", timeout=5, retries=10):
         vd_url = "%s%s%s" % (base, "vendor-data", ext)
         md_url = "%s%s%s" % (base, "meta-data", ext)
         network_url = "%s%s%s" % (base, "network-config", ext)
-    network_resp = url_helper.read_file_or_url(
-        network_url, timeout=timeout, retries=retries
-    )
     network = None
-    if network_resp.ok():
-        network = load_yaml(network_resp.contents)
+    try:
+        network_resp = url_helper.read_file_or_url(
+            network_url, timeout=timeout, retries=retries
+        )
+    except url_helper.UrlError as e:
+        LOG.debug("No network config provided: %s", e)
+    else:
+        if network_resp.ok():
+            network = load_yaml(network_resp.contents)
     md_resp = url_helper.read_file_or_url(
         md_url, timeout=timeout, retries=retries
     )