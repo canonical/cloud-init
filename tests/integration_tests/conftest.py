568b317361a353d7c3c9152f9fdc1452077f9911
Brett Holman <brett.holman@canonical.com>
2024-11-25 18:13:06 -0700

    fixup! test: add option to keep instance when test fails

--- tests/integration_tests/conftest.py
+++ tests/integration_tests/conftest.py
@@ -94,8 +94,8 @@ def session_cloud() -> Generator[IntegrationCloud, None, None]:
     yield cloud
 
     if not integration_settings.KEEP_INSTANCE or (
-        integration_settings.KEEP_INSTANCE == "ON_ERROR"
-        and not cloud.test_failed
+        not cloud.test_failed and
+        integration_settings.KEEP_INSTANCE == "ON_ERROR"  # type: ignore
     ):
         cloud.destroy()
 
