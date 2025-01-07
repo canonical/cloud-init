49c4f8f626a0eee6758ede777c14fa1e6244d9db
Brett Holman <brett.holman@canonical.com>
2025-01-06 22:06:38 -0700

    test: disambiguate resource cleanup from test failure (#5926)
    Add a background thread which is responsible for instance cleanup in integration tests.

--- tests/integration_tests/instances.py
+++ tests/integration_tests/instances.py
@@ -14,7 +14,7 @@ from pycloudlib.lxd.instance import LXDInstance
 from pycloudlib.result import Result
 
 from tests.helpers import cloud_init_project_dir
-from tests.integration_tests import integration_settings
+from tests.integration_tests import conftest, integration_settings
 from tests.integration_tests.decorators import retry
 from tests.integration_tests.util import ASSETS_DIR
 
@@ -330,6 +330,6 @@ Pin-Priority: 1001"""
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if not self.settings.KEEP_INSTANCE:
-            self.destroy()
+            conftest.REAPER.reap(self)
         else:
             log.info("Keeping Instance, public ip: %s", self.ip())
