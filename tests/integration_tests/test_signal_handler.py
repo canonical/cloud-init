a9f19a92defd8616d16948f2c4ca90c78331d145
James Falcon <james.falcon@canonical.com>
2025-03-05 19:49:08 -0600

    test: Add integration test for signal handling warnings/errors (#6037)
    Also, added context above "traceback" in log output.

---
+++ tests/integration_tests/test_signal_handler.py
@@ -0,0 +1,21 @@
+import pytest
+
+from tests.integration_tests.instances import IntegrationInstance
+from tests.integration_tests.util import verify_clean_boot
+
+USER_DATA = """\
+#cloud-config
+runcmd:
+ - pkill cloud-init
+ - sleep 5
+ - echo 'whoops' > /var/tmp/whoops
+"""
+
+
+@pytest.mark.user_data(USER_DATA)
+def test_no_warnings(client: IntegrationInstance):
+    """Test that the signal handler does not log errors when suppressed."""
+    verify_clean_boot(client)
+    log = client.read_from_file("/var/log/cloud-init.log")
+    assert "Received signal 15 resulting in exit" in log
+    assert client.execute("test -f /var/tmp/whoops").failed
