c2f31260e0e9c93fd724164ae493866383dc714c
paulober <44974737+paulober@users.noreply.github.com>
2025-04-12 22:28:41 +0200

    Fix module naming
    Signed-off-by: paulober <44974737+paulober@users.noreply.github.com>

--- tests/unittests/config/test_cc_netplan_nm_patch.py
+++
@@ -1,9 +0,0 @@
-# This file is part of cloud-init. See LICENSE file for license information.
-
-from tests.unittests.helpers import skipUnlessJsonSchema
-
-
-@skipUnlessJsonSchema()
-class TestCCNetplanNmPatch:
-    def test_schema_validation(self):
-        pass
