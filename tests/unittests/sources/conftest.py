67e5f4ee12ae6f0ba00ff1fbd7121a4b5c4a0c43
Chris Patterson <cpatterson@microsoft.com>
2025-04-22 15:30:33 +0000

    feat(azure): allow unspecified user name
    Default configured user will be used.
    
    - hide resource disk from unit tests as it may exist
      in github actions runner.
    
    - cleanup and type fixes for build_minimal_ovf()
    
    Signed-off-by: Chris Patterson <cpatterson@microsoft.com>

--- tests/unittests/sources/conftest.py
+++ tests/unittests/sources/conftest.py
@@ -1,3 +1,4 @@
+import os
 from unittest import mock
 
 import pytest
@@ -7,3 +8,16 @@ import pytest
 def mock_util_get_cmdline():
     with mock.patch("cloudinit.util.get_cmdline", return_value="") as m:
         yield m
+
+
+@pytest.fixture(autouse=True)
+def hide_resource_disk():
+    """GitHub runner may have a resource disk which may affect tests."""
+    real_exists = os.path.exists
+    with mock.patch("os.path.exists") as mock_exists:
+        mock_exists.side_effect = lambda path: (
+            False
+            if path == "/dev/disk/cloud/azure_resource"
+            else real_exists(path)
+        )
+        yield
