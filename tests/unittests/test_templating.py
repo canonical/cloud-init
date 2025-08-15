987088664e998d52fde0ee9e5239ac4cf8b2e235
Alberto Contreras <alberto.contreras@canonical.com>
2025-08-15 22:23:47 +0200

    test: drop citestcase from t* tests (#6379)

--- tests/unittests/test_templating.py
+++ tests/unittests/test_templating.py
@@ -4,7 +4,9 @@
 #
 # This file is part of cloud-init. See LICENSE file for license information.
 
+import logging
 import textwrap
+from unittest import mock
 
 import pytest
 
@@ -14,9 +16,7 @@ from cloudinit.util import load_binary_file, write_file
 from tests.unittests import helpers as test_helpers
 
 
-class TestTemplates(test_helpers.CiTestCase):
-
-    with_logs = True
+class TestTemplates:
 
     jinja_utf8 = b"It\xe2\x80\x99s not ascii, {{name}}\n"
     jinja_utf8_rbob = b"It\xe2\x80\x99s not ascii, bob\n".decode("utf-8")
@@ -45,18 +45,18 @@ class TestTemplates(test_helpers.CiTestCase):
             """
         )
         out_data = templater.basic_render(in_data, {"b": 2})
-        self.assertEqual(expected_data.strip(), out_data)
+        assert expected_data.strip() == out_data
 
     def test_render_jinja(self):
         blob = """## template:jinja
 {{a}},{{b}}"""
         c = templater.render_string(blob, {"a": 1, "b": 2})
-        self.assertEqual("1,2", c)
+        assert "1,2" == c
 
     def test_render_default(self):
         blob = """$a,$b"""
         c = templater.render_string(blob, {"a": 1, "b": 2})
-        self.assertEqual("1,2", c)
+        assert "1,2" == c
 
     def test_render_basic_deeper(self):
         hn = "myfoohost.yahoo.com"
@@ -68,21 +68,21 @@ class TestTemplates(test_helpers.CiTestCase):
             },
         }
         out_data = templater.render_string(in_data, params)
-        self.assertEqual(expected_data, out_data)
+        assert expected_data == out_data
 
     def test_render_basic_no_parens(self):
         hn = "myfoohost"
         in_data = "h=$hostname\nc=d\n"
         expected_data = "h=%s\nc=d\n" % hn
         out_data = templater.basic_render(in_data, {"hostname": hn})
-        self.assertEqual(expected_data, out_data)
+        assert expected_data == out_data
 
     def test_render_basic_parens(self):
         hn = "myfoohost"
         in_data = "h = ${hostname}\nc=d\n"
         expected_data = "h = %s\nc=d\n" % hn
         out_data = templater.basic_render(in_data, {"hostname": hn})
-        self.assertEqual(expected_data, out_data)
+        assert expected_data == out_data
 
     def test_render_basic2(self):
         mirror = "mymirror"
@@ -96,30 +96,27 @@ class TestTemplates(test_helpers.CiTestCase):
         out_data = templater.basic_render(
             in_data, {"mirror": mirror, "codename": codename}
         )
-        self.assertEqual(ex_data, out_data)
+        assert ex_data == out_data
 
     def test_jinja_nonascii_render_to_string(self):
         """Test jinja render_to_string with non-ascii content."""
-        self.assertEqual(
+        assert (
             templater.render_string(
                 self.add_header("jinja", self.jinja_utf8), {"name": "bob"}
-            ),
-            self.jinja_utf8_rbob,
+            )
+            == self.jinja_utf8_rbob
         )
 
     def test_jinja_nonascii_render_undefined_variables_to_default_py3(self):
         """Test py3 jinja render_to_string with undefined variable default."""
-        self.assertEqual(
-            templater.render_string(
-                self.add_header("jinja", self.jinja_utf8), {}
-            ),
-            self.jinja_utf8_rbob.replace("bob", "CI_MISSING_JINJA_VAR/name"),
-        )
+        assert templater.render_string(
+            self.add_header("jinja", self.jinja_utf8), {}
+        ) == self.jinja_utf8_rbob.replace("bob", "CI_MISSING_JINJA_VAR/name")
 
-    def test_jinja_nonascii_render_to_file(self):
+    def test_jinja_nonascii_render_to_file(self, tmp_path):
         """Test jinja render_to_file of a filename with non-ascii content."""
-        tmpl_fn = self.tmp_path("j-render-to-file.template")
-        out_fn = self.tmp_path("j-render-to-file.out")
+        tmpl_fn = str(tmp_path / "j-render-to-file.template")
+        out_fn = str(tmp_path / "j-render-to-file.out")
         write_file(
             filename=tmpl_fn,
             omode="wb",
@@ -127,21 +124,21 @@ class TestTemplates(test_helpers.CiTestCase):
         )
         templater.render_to_file(tmpl_fn, out_fn, {"name": "bob"})
         result = load_binary_file(out_fn).decode("utf-8")
-        self.assertEqual(result, self.jinja_utf8_rbob)
+        assert result == self.jinja_utf8_rbob
 
-    def test_jinja_nonascii_render_from_file(self):
+    def test_jinja_nonascii_render_from_file(self, tmp_path):
         """Test jinja render_from_file with non-ascii content."""
-        tmpl_fn = self.tmp_path("j-render-from-file.template")
+        tmpl_fn = str(tmp_path / "j-render-from-file.template")
         write_file(
             tmpl_fn,
             omode="wb",
             content=self.add_header("jinja", self.jinja_utf8).encode("utf-8"),
         )
         result = templater.render_from_file(tmpl_fn, {"name": "bob"})
-        self.assertEqual(result, self.jinja_utf8_rbob)
+        assert result == self.jinja_utf8_rbob
 
     @test_helpers.skipIfJinja()
-    def test_jinja_warns_on_missing_dep_and_uses_basic_renderer(self):
+    def test_jinja_warns_on_missing_dep_and_uses_basic_renderer(self, caplog):
         """Test jinja render_from_file will fallback to basic renderer."""
         tmpl_fn = self.tmp_path("j-render-from-file.template")
         write_file(
@@ -150,12 +147,13 @@ class TestTemplates(test_helpers.CiTestCase):
             content=self.add_header("jinja", self.jinja_utf8).encode("utf-8"),
         )
         result = templater.render_from_file(tmpl_fn, {"name": "bob"})
-        self.assertEqual(result, self.jinja_utf8.decode())
-        self.assertIn(
-            "WARNING: Jinja not available as the selected renderer for desired"
+        assert result == self.jinja_utf8.decode()
+        assert (
+            mock.ANY,
+            logging.WARNING,
+            "Jinja not available as the selected renderer for desired"
             " template, reverting to the basic renderer.",
-            self.logs.getvalue(),
-        )
+        ) in caplog.record_tuples
 
     def test_jinja_do_extension_render_to_string(self):
         """Test jinja render_to_string using do extension."""
@@ -164,11 +162,11 @@ class TestTemplates(test_helpers.CiTestCase):
             "{% set r = [] %} {% set input = [1,2,3] %} "
             "{% for i in input %} {% do r.append(i) %} {% endfor %} {{r}}"
         )
-        self.assertEqual(
+        assert (
             templater.render_string(
                 self.add_header("jinja", jinja_template), {}
-            ).strip(),
-            expected_result,
+            ).strip()
+            == expected_result
         )
 
 
