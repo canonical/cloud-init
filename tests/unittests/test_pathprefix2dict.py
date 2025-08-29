f881f901946ddf0d011c33e32028b1522e19eb9b
Aamir017 <skaamir2005@gmail.com>
2025-08-29 19:58:21 +0530

    tests: convert util.pathprefix1dict tests from unittest to pytest and keep class grouping

--- tests/unittests/test_pathprefix2dict.py
+++ tests/unittests/test_pathprefix2dict.py
@@ -1,46 +1,39 @@
 # This file is part of cloud-init. See LICENSE file for license information.
 
-import shutil
-import tempfile
-import pytest
-
 from cloudinit import util
 from tests.unittests.helpers import populate_dir
+import pytest
 
 
-@pytest.fixture
-def tmpdir_path(tmp_path):
-    # make a temp directory that gets cleaned up automatically
-    return str(tmp_path)
+class TestPathPrefix2Dict:
+    def test_required_only(self, tmp_path):
+        dirdata = {"f1": b"f1content", "f2": b"f2content"}
+        populate_dir(tmp_path, dirdata)
 
+        ret = util.pathprefix2dict(str(tmp_path), required=["f1", "f2"])
+        assert dirdata == ret
 
-def test_required_only(tmpdir_path):
-    dirdata = {"f1": b"f1content", "f2": b"f2content"}
-    populate_dir(tmpdir_path, dirdata)
+    def test_required_missing(self, tmp_path):
+        dirdata = {"f1": b"f1content"}
+        populate_dir(tmp_path, dirdata)
+        kwargs = {"required": ["f1", "f2"]}
+        with pytest.raises(ValueError):
+            util.pathprefix2dict(str(tmp_path), **kwargs)
 
-    ret = util.pathprefix2dict(tmpdir_path, required=["f1", "f2"])
-    assert dirdata == ret
+    def test_no_required_and_optional(self, tmp_path):
+        dirdata = {"f1": b"f1c", "f2": b"f2c"}
+        populate_dir(tmp_path, dirdata)
 
+        ret = util.pathprefix2dict(
+            str(tmp_path), required=None, optional=["f1", "f2"]
+        )
+        assert dirdata == ret
 
-def test_required_missing(tmpdir_path):
-    dirdata = {"f1": b"f1content"}
-    populate_dir(tmpdir_path, dirdata)
-    kwargs = {"required": ["f1", "f2"]}
-    with pytest.raises(ValueError):
-        util.pathprefix2dict(tmpdir_path, **kwargs)
+    def test_required_and_optional(self, tmp_path):
+        dirdata = {"f1": b"f1c", "f2": b"f2c"}
+        populate_dir(tmp_path, dirdata)
 
-
-def test_no_required_and_optional(tmpdir_path):
-    dirdata = {"f1": b"f1c", "f2": b"f2c"}
-    populate_dir(tmpdir_path, dirdata)
-
-    ret = util.pathprefix2dict(tmpdir_path, required=None, optional=["f1", "f2"])
-    assert dirdata == ret
-
-
-def test_required_and_optional(tmpdir_path):
-    dirdata = {"f1": b"f1c", "f2": b"f2c"}
-    populate_dir(tmpdir_path, dirdata)
-
-    ret = util.pathprefix2dict(tmpdir_path, required=["f1"], optional=["f2"])
-    assert dirdata == ret
+        ret = util.pathprefix2dict(
+            str(tmp_path), required=["f1"], optional=["f2"]
+        )
+        assert dirdata == ret
