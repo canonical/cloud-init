7b850fbf83b1adb441b8b953e3c3d930904eea26
Aamir017 <skaamir2005@gmail.com>
2025-08-29 20:33:45 +0530

    Fix import order in test_pathprefix2dict

--- tests/unittests/test_pathprefix2dict.py
+++ tests/unittests/test_pathprefix2dict.py
@@ -1,28 +1,29 @@
 # This file is part of cloud-init. See LICENSE file for license information.
 
+import pytest
+
 from cloudinit import util
 from tests.unittests.helpers import populate_dir
-import pytest
 
 
 class TestPathPrefix2Dict:
     def test_required_only(self, tmp_path):
         dirdata = {"f1": b"f1content", "f2": b"f2content"}
-        populate_dir(tmp_path, dirdata)
+        populate_dir(str(tmp_path), dirdata)
 
         ret = util.pathprefix2dict(str(tmp_path), required=["f1", "f2"])
         assert dirdata == ret
 
     def test_required_missing(self, tmp_path):
         dirdata = {"f1": b"f1content"}
-        populate_dir(tmp_path, dirdata)
+        populate_dir(str(tmp_path), dirdata)
         kwargs = {"required": ["f1", "f2"]}
         with pytest.raises(ValueError):
             util.pathprefix2dict(str(tmp_path), **kwargs)
 
     def test_no_required_and_optional(self, tmp_path):
         dirdata = {"f1": b"f1c", "f2": b"f2c"}
-        populate_dir(tmp_path, dirdata)
+        populate_dir(str(tmp_path), dirdata)
 
         ret = util.pathprefix2dict(
             str(tmp_path), required=None, optional=["f1", "f2"]
@@ -31,7 +32,7 @@ class TestPathPrefix2Dict:
 
     def test_required_and_optional(self, tmp_path):
         dirdata = {"f1": b"f1c", "f2": b"f2c"}
-        populate_dir(tmp_path, dirdata)
+        populate_dir(str(tmp_path), dirdata)
 
         ret = util.pathprefix2dict(
             str(tmp_path), required=["f1"], optional=["f2"]
