bd45dec604d258d6b8078acc8008578d5a31685b
Philipp Bender <sweet-guy@hotmail.de>
2025-09-11 10:07:13 +0200

    feat(hetzner): enable hotplug support and prepare IPv6 integration (#6445)
    This PR tries to accomplish hotplug support for the Hetzner cloud.
    cloud-init should pick up the newly attached private network and
    configure the interface accordingly. Furthermore, this PR also prepares
    cloud-init to use the IPv6 Hetzner metadata-service

--- cloudinit/sources/helpers/hetzner.py
+++ cloudinit/sources/helpers/hetzner.py
@@ -3,22 +3,41 @@
 #
 # This file is part of cloud-init. See LICENSE file for license information.
 
-from cloudinit import url_helper, util
+from typing import Optional, Tuple
+
+from cloudinit import net, url_helper
 
 
-def read_metadata(url, timeout=2, sec_between=2, retries=30):
-    response = url_helper.readurl(
-        url, timeout=timeout, sec_between=sec_between, retries=retries
-    )
-    if not response.ok():
-        raise RuntimeError("unable to read metadata at %s" % url)
-    return util.load_yaml(response.contents.decode(), allowed=(dict, list))
+def _skip_retry_on_empty_response(cause: url_helper.UrlError) -> bool:
+    return cause.code != 204
 
 
-def read_userdata(url, timeout=2, sec_between=2, retries=30):
-    response = url_helper.readurl(
-        url, timeout=timeout, sec_between=sec_between, retries=retries
-    )
-    if not response.ok():
-        raise RuntimeError("unable to read userdata at %s" % url)
-    return response.contents
+def get_metadata(
+    urls,
+    max_wait=120,
+    timeout=2,
+    sleep_time=2,
+) -> Tuple[Optional[str], bytes]:
+    try:
+        url, contents = url_helper.wait_for_url(
+            urls=urls,
+            max_wait=max_wait,
+            timeout=timeout,
+            sleep_time=sleep_time,
+            # It is ok for userdata to not exist (that's why we are stopping if
+            # HTTP code is 204) and just in that case returning an empty
+            # string.
+            exception_cb=_skip_retry_on_empty_response,
+        )
+        if not url:
+            raise RuntimeError("No data received from urls: '%s':" % urls)
+        return url, contents
+    except url_helper.UrlError as e:
+        if e.code == 204:
+            return e.url, b""
+        raise
+
+
+def get_interface_name_from_mac(mac: str) -> Optional[str]:
+    mac_to_iface = net.get_interfaces_by_mac()
+    return mac_to_iface.get(mac.lower())
