d54a242811bde418ed4316c4d186dc996a785ce9
Leah Goldberg <leah.goldberg@canonical.com>
2026-01-05 17:30:58 -0500

    fix linting errors

--- cloudinit/sources/DataSourceCloudStack.py
+++ cloudinit/sources/DataSourceCloudStack.py
@@ -336,11 +336,11 @@ def get_vr_address(distro):
         if latest_address:
             LOG.debug("Found SERVER_ADDRESS '%s' via dhclient", latest_address)
             return latest_address
-        
-    # Try ephemeral dhcp lease last
     # get_newest_lease() expects a network interface string
     with suppress(FileNotFoundError):
-        latest_lease = distro.dhcp_client.get_newest_lease(distro.fallback_interface)
+        latest_lease = distro.dhcp_client.get_newest_lease(
+            distro.fallback_interface
+        )
         if latest_lease:
             LOG.debug(
                 "Found SERVER_ADDRESS '%s' via ephemeral %s lease ",
