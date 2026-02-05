1f8f9e0662f06f1d46408f68189b48edced4ff05
Cade Jacobson <cadejacobson@microsoft.com>
2026-02-05 09:00:49 -0800

    Correct unit test

--- tests/unittests/sources/test_azure.py
+++ tests/unittests/sources/test_azure.py
@@ -2434,15 +2434,21 @@ class TestLoadAzureDsDir:
             == cm.value.reason
         )
 
-    def test_import_error_from_failed_import():
-        """ Attempt to import a module that is not present"""
+    def test_import_error_from_failed_import(self):
+        """Attempt to import a module that is not present"""
         try:
-            import nonexistent_module_that_will_never_exist
+            import nonexistent_module_that_will_never_exist  # noqa: F401
         except ImportError as error:
             reportable_error = errors.ReportableErrorImportError(error=error)
 
-            assert reportable_error.reason == "error importing nonexistent_module_that_will_never_exist library"
-            assert reportable_error.supporting_data["error"] == repr(error.value)
+            assert (
+                reportable_error.reason == "error importing"
+                "nonexistent_module_that_will_never_exist library"
+            )
+            assert reportable_error.supporting_data["error"] == repr(
+                error.value
+            )
+
 
 class TestReadAzureOvf:
     def test_invalid_xml_raises_non_azure_ds(self):
