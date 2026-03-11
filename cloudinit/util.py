7132625c6f9294ea73eedcfdab0c4e2dae6a4d0d
Alejandro Perez <69607775+aale24@users.noreply.github.com>
2026-03-10 18:17:32 -0700

    feat(util): fail early when hostname is not resolvable in is_resolvable (#6772)
    Building on the previous optimization in #2040, add an additional 
    optimization to fail early when the hostname is not resolvable at all.

--- cloudinit/util.py
+++ cloudinit/util.py
@@ -1300,6 +1300,10 @@ def is_resolvable(url) -> bool:
     with suppress(ValueError):
         if net.is_ip_address(parsed_url.netloc.strip("[]")):
             return True
+    try:
+        hostname_result = socket.getaddrinfo(name, None)
+    except (socket.gaierror, socket.error):
+        return False
 
     if _DNS_REDIRECT_IP is None:
         badips = set()
@@ -1324,13 +1328,11 @@ def is_resolvable(url) -> bool:
         if badresults:
             LOG.debug("detected dns redirection: %s", badresults)
 
-    try:
-        result = socket.getaddrinfo(name, None)
-        # check first result's sockaddr field
-        addr = result[0][4][0]
-        return addr not in _DNS_REDIRECT_IP
-    except (socket.gaierror, socket.error):
+    # check first result's sockaddr field
+    addr = hostname_result[0][4][0]
+    if addr in _DNS_REDIRECT_IP:
         return False
+    return True
 
 
 def get_hostname():
