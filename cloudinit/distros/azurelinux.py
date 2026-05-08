fe50391f2eebcc1e31b0b58035d181baf0f319bc
Dan Streetman <ddstreet@microsoft.com>
2026-05-08 12:53:27 -0400

    fix(azurelinux): remove overridden package_command() method (#6874)
    Azure Linux 3.0 used tdnf and needed to override this method; Azure
    Linux 4.0 does not use tdnf and can use the inherited method.

--- cloudinit/distros/azurelinux.py
+++ cloudinit/distros/azurelinux.py
@@ -6,7 +6,6 @@
 
 import logging
 
-from cloudinit import subp, util
 from cloudinit.distros import rhel
 from cloudinit.net.netplan import CLOUDINIT_NETPLAN_FILE
 
@@ -45,30 +44,3 @@ class Distro(rhel.Distro):
                 "postcmds": "True",
             },
         }
-
-    def package_command(self, command, args=None, pkgs=None):
-        if pkgs is None:
-            pkgs = []
-
-        if subp.which("dnf"):
-            LOG.debug("Using DNF for package management")
-            cmd = ["dnf"]
-        else:
-            LOG.debug("Using TDNF for package management")
-            cmd = ["tdnf"]
-        # Determines whether or not dnf/tdnf prompts for confirmation
-        # of critical actions. We don't want to prompt...
-        cmd.append("-y")
-
-        if args and isinstance(args, str):
-            cmd.append(args)
-        elif args and isinstance(args, list):
-            cmd.extend(args)
-
-        cmd.append(command)
-
-        pkglist = util.expand_package_list("%s-%s", pkgs)
-        cmd.extend(pkglist)
-
-        # Allow the output of this to flow outwards (ie not be captured)
-        subp.subp(cmd, capture=False)
